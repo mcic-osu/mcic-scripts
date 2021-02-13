1b7b71ea983ff9a45b908f34b14bc1bc6ca397e1
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2021-02-13 11:38:36 -0500

    Add README for cutadapt.sh with primer file example

--- metabarcoding/cutadapt_example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# An example with primers 515F and 806R for 16S rRNA:
-indir=data/fastq/raw
-outdir=data/fastq/trimmed
-primer_f=GAGTGYCAGCMGCCGCGGTAA
-primer_r=ACGGACTACNVGGGTWTCTAAT
-sbatch cutadapt.sh -i "$indir" -o "$outdir" -f "$primer_f" -r "$primer_r"
