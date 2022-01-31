ed0be2a3300fddede361dca006ffe5935e92a656
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-01-31 17:54:04 -0500

    Remove ps object splitting / taxon agglom from ps_filter

--- metabarcoding/ps_filter.R
+++ metabarcoding/ps_filter.R
@@ -6,10 +6,8 @@
 # SETUP ------------------------------------------------------------------------
 ## Load packages
 if (!"pacman" %in% installed.packages()) install.packages("pacman")
-packages <- c(
-    "BiocManager", "tidyverse", "phyloseq", "decontam",
-    "microbiome", "biomformat"
-)
+packages <- c("BiocManager", "tidyverse", "phyloseq", "decontam",
+              "microbiome", "biomformat")
 pacman::p_load(char = packages)
 
 ## Process command-line arguments
@@ -56,13 +54,6 @@ outfile_biom <- file.path(outdir, paste0(file_id, ".biom"))
 outfile_contam_df <- file.path(outdir_qc, "contam_df.txt")
 outprefix_contamplot <- file.path(outdir_qc, "contam_abund-plot")
 outfile_neg_control <- file.path(outdir_qc, "neg_control_ASVs.txt")
-
-outfile_phylum <- file.path(outdir_glom, paste0(file_id, "_phylum.rds"))
-outfile_class <- file.path(outdir_glom, paste0(file_id, "_class.rds"))
-outfile_order <- file.path(outdir_glom, paste0(file_id, "_order.rds"))
-outfile_family <- file.path(outdir_glom, paste0(file_id, "_family.rds"))
-outfile_genus <- file.path(outdir_glom, paste0(file_id, "_genus.rds"))
-outfile_species <- file.path(outdir_glom, paste0(file_id, "_species.rds"))
 
 ## Process parameters
 if (is.na(batch_column)) batch_column <- NULL
@@ -319,22 +310,6 @@ setdiff(sample_names(ps_target), sample_names(ps))
 
 ## Save RDS file of phyloseq object
 saveRDS(ps, ps_out)
-
-
-# CREATE PHYLOSEQ OBJECTS AGGLOMERATED BY TAXRANK ------------------------------
-ps_phylum <- tax_glom(ps, taxrank = "phylum")
-ps_class <- tax_glom(ps, taxrank = "class")
-ps_order <- tax_glom(ps, taxrank = "order")
-ps_family <- tax_glom(ps, taxrank = "family")
-ps_genus <- tax_glom(ps, taxrank = "genus")
-ps_species <- tax_glom(ps, taxrank = "species")
-
-saveRDS(ps_phylum, outfile_phylum)
-saveRDS(ps_class, outfile_class)
-saveRDS(ps_order, outfile_order)
-saveRDS(ps_family, outfile_family)
-saveRDS(ps_genus, outfile_genus)
-saveRDS(ps_species, outfile_species)
 
 
 # CREATE BIOM FILE -------------------------------------------------------------
