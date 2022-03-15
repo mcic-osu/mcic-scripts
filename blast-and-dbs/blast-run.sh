3197fe729a33e4d9278aa4b8d57493593b0e3796
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-03-15 15:43:22 -0400

    Improve help and usage

--- blast-and-dbs/blast-run.sh
+++ blast-and-dbs/blast-run.sh
@@ -5,50 +5,60 @@
 #SBATCH --cpus-per-task=12
 #SBATCH --output=slurm-blast-%j.out
 
-# SETUP ------------------------------------------------------------------------
-## Load software
-[[ $(which conda) = ~/miniconda3/bin/conda ]] || module load python/3.6-conda5.2
-source activate blast-env
-
-## Bash strict mode
-set -euo pipefail
-
-## Help
+## Help function
 Help() {
-    # Display Help
     echo
     echo "## $0: Run BLAST for an input (query) FASTA file, either locally or remotely."
     echo
-    echo "## Syntax: $0 -i <input-fasta> -o <output> -d <blast-db> [-r] [-h]"
-    echo "## Options:"
-    echo "## -h     Print help."
-    echo "## -i     Input file ('query', a FASTA file) (REQUIRED)"
-    echo "## -o     Output file (REQUIRED)"
-    echo "## -d     Blast DB. If remote, e.g. 'nt' or 'nr'. If local, make sure to specify the dir AND THE DB name, e.g. refdata/blast/nr-db/nr"
-    echo "## -r     Run BLAST remotely (default: run BLAST locally)"
-    echo "## Example: $0 -q my.fasta -o results/blast/blast.out -d refdata/blast/nt-db/nt"
+    echo "## Syntax: $0 -i <input-fasta> -o <output-file> ..."
+    echo
+    echo "## Required options ('STR' means that the option requires a string as an argument):"
+    echo "## -i STR    Input file ('query', a FASTA file)"
+    echo "## -o STR    Output file"
+    echo
+    echo "## Other options:"
+    echo "## -d STR    Blast DB [default: 'nr']"
+    echo "             If remote, e.g. 'nt' or 'nr'."
+    echo "             If local, make sure to specify the dir AND THE DB name, e.g. 'refdata/blast/nr-db/nr'"
+    echo "## -l        Run BLAST locally [default: run BLAST remotely]"
+    echo "## -h        Print this help message and exit"
+    echo
+    echo "## Example: $0 -i data/my.fasta -o results/blast/blast.out -d refdata/blast/nt-db/nt"
     echo "## To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
     echo
 }
 
+# SETUP ------------------------------------------------------------------------
+## Report
+echo -e "\n## Starting script blast-run.sh..."
+date
+echo
+
 ## Option defaults
 query_fa=""      # BLAST input file name (FASTA)
 blast_out=""     # BLAST output file name
-blast_db="nr"
-remote="false"
+blast_db="nr"    # Blast database
+remote="true"    # Whether or not to run BLAST remotely
 
 ## Parse command-line options
-while getopts ':i:o:d:rh' flag; do
+while getopts ':i:o:d:lh' flag; do
     case "${flag}" in
     i) query_fa="$OPTARG" ;;
     o) blast_out="$OPTARG" ;;
     d) blast_db="$OPTARG" ;;
-    r) remote=true ;;
+    l) remote=false ;;
     h) Help && exit 0 ;;
-    \?) echo "## $0: ERROR: Invalid option" >&2 && exit 1 ;;
+    \?) echo "## $0: ERROR: Invalid option -$OPTARG" >&2 && exit 1 ;;
     :) echo "## $0: ERROR: Option -$OPTARG requires an argument." >&2 && exit 1 ;;
     esac
 done
+
+## Load software
+module load python/3.6-conda5.2
+source activate /users/PAS0471/jelmer/miniconda3/envs/blast-env
+
+## Bash strict mode
+set -euo pipefail
 
 ## Process options
 outdir=$(dirname "$blast_out")
@@ -60,13 +70,10 @@ mkdir -p "$outdir"
 [[ ! -f $query_fa ]] && echo "## $0: ERROR: Input file $query_fa does not exist" >&2 && exit 1
 
 ## Report
-echo "## Starting script blast-run..."
-date
-echo "## Command-line args:"
-echo "## Input FASTA file:     $query_fa"
-echo "## BLAST output file:    $blast_out"
-echo "## BLAST database:       $blast_db"
-echo "## Run blast remotely?   $remote"
+echo "## Input FASTA file:       $query_fa"
+echo "## BLAST output file:      $blast_out"
+echo "## BLAST database:         $blast_db"
+echo "## Run blast remotely?     $remote"
 echo -e "--------------------\n"
 
 
@@ -110,3 +117,4 @@ ls -lh "$blast_out"
 
 echo -e "\n## Done with script blast-run.sh"
 date
+echo
