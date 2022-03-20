6d565b382fde4a2b1efa20c49feb312560766690
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-03-20 16:36:16 -0400

    First pass finish of script

--- misc/bbmap.sh
+++ misc/bbmap.sh
@@ -6,11 +6,13 @@
 #SBATCH --mem=32G
 #SBATCH --output=slurm-bbmap-%j.out
 
+## Report
+echo "## Starting script bbmap.sh..."
+date
+
 ## Software
-source ~/.bashrc
-[[ $(which conda) = ~/miniconda3/bin/conda ]] || module load python/3.6-conda5.2
-conda activate bbmap-env
-conda activate --stack samtools-env
+module load python/3.6-conda5.2
+source activate /users/PAS0471/jelmer/miniconda3/envs/bbmap-env  # Also has samtools installed
 
 ## Bash strict mode
 set -euo pipefail
@@ -18,43 +20,42 @@ set -euo pipefail
 ## Command-line args
 R1_in="$1"
 R2_in="$2"
-scaffolds="$3"
-bam_unsorted="$4"
-bam_sorted="$5"
+ref="$3"
+bam_out="$4"
 
 ## Additional variables
-n_cores="$SLURM_CPUS_ON_NODE"                            # Retrieve number of cores
+n_cores="$SLURM_CPUS_ON_NODE"
 
-## Other variables
-outdir_unsorted=$(dirname "$bam_unsorted")
-mkdir -p "$outdir_unsorted"
-outdir_sorted=$(dirname "$bam_sorted")
-mkdir -p "$outdir_sorted"
+## Process variables
+bam_unsorted=${bam_out/.bam/}.unsorted.bam
+outdir=$(dirname "$bam_out")
+mkdir -p "$outdir"
 
 ## Report
-echo "## Starting script bbmap.sh..."
-date
 echo "## Command-line args:"
-echo "## Input FASTQ file - R1: $R1_in"
-echo "## Input FASTQ file - R2: $R2_in"
-echo "## Assembled scaffold/contigs: $scaffolds"
-echo "## Output BAM file - unsorted: $bam_unsorted"
-echo "## Output BAM file - sorted: $bam_sorted"
-echo -e "---------------------------\n\n"
+echo "## Input FASTQ file - R1:          $R1_in"
+echo "## Input FASTQ file - R2:          $R2_in"
+echo "## Reference genome FASTQ:         $ref"
+echo "## Output BAM file:                $bam_out"
+echo -e "---------------------\n"
 
 ## Run BBMAP
-echo "Running BBMap..."
+echo "## Now running BBMap..."
 bbmap.sh \
-    ref="$scaffolds" \
+    ref="$ref" \
     in1="$R1_in" in2="$R2_in" \
     out="$bam_unsorted" \
     k=14 minid=0.9 build=1 \
     threads="$n_cores"
 
 ## Run samtools
-echo -e "\nRunning samtools sort..."
-samtools sort "$bam_unsorted" > "$bam_sorted"
+echo -e "\n## Now running samtools sort..."
+samtools sort "$bam_unsorted" > "$bam_out" && \
+    rm "$bam_unsorted"
 
 ## Report
+echo -e "\n## Output BAM file:"
+ls -lh "$bam_out"
 echo -e "\n## Done with script bbmap.sh"
 date
+echo
