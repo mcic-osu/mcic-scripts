d4af978ac2215f3e52b1fc70661f2e5ae859cf68
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-04-03 17:00:02 -0400

    Add script to download a part of the RefSeq database

---
+++ blast-and-dbs/dl-refseq.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --account=PAS0471
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --output=slurm-download-refseq-%j.out
+
+## Report
+echo -e "\n## Starting script dl-refseq.sh..."
+date
+echo
+
+## Parameters
+refseq_lib=$1     # Options are the dir names here: https://ftp.ncbi.nlm.nih.gov/refseq/release/
+outdir=$2
+
+## NCBI key
+export NCBI_API_KEY=34618c91021ccd7f17429b650a087b585f08
+#? https://ftp.ncbi.nlm.nih.gov/refseq/release/
+#? See also https://github.com/DerrickWood/kraken2/issues/115
+
+## Make outdir
+mkdir -p "$outdir"
+
+## Download
+wget \
+    ftp://ftp.ncbi.nlm.nih.gov/refseq/release/"$refseq_lib"/*genomic.fna.gz \
+    -P "$outdir"
+
+## Report
+echo -e "\n## Listing files in output dir:"
+ls -lh "$outdir"
+echo -e "\n## Done with script dl-refseq.sh..."
+date
+echo
+sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+echo
