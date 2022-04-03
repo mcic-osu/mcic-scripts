9bcdeac8df633de27987bcb92df269dd7796cbdb
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-04-03 15:33:25 -0400

    Switch to using flags, full conda path, etc

--- trans-assembly/busco.sh
+++ trans-assembly/busco.sh
@@ -9,34 +9,74 @@
 #SBATCH --output=slurm-busco-%j.out
 
 
-# SETUP ------------------------------------------------------------------------
-## Load software
-source ~/.bashrc
-[[ $(which conda) = ~/miniconda3/bin/conda ]] || module load python/3.6-conda5.2
-source activate busco-env
+# PARSE COMMAND-LINE ARGS ------------------------------------------------------
+## Help function
+Help() {
+  echo
+  echo "## $0: Run BUSCO to check a transcriptome or genome assembly."
+  echo
+  echo "## Syntax: $0 -i <input-FASTA> -o <output-dir> -d <db-name> ..."
+  echo
+  echo "## Required options:"
+  echo "## -i STRING     Input FASTA file with the assembly"
+  echo "## -o STRING     Output directory"
+  echo "## -d STRING     Busco database name (see https://busco.ezlab.org/list_of_lineages.html)"
+  echo
+  echo "## Other options:"
+  echo "## -t STRING     Assembly type                  [default: 'transcriptome']"
+  echo "## -h            Print this help message and exit"
+  echo
+  echo "## Example: $0 -i results/asssmbly/assembly.fa -o results/BUSCO -d bacteria_odb"
+  echo "## To submit to the OSC queue, preface with 'sbatch': sbatch $0 ..."
+  echo
+}
 
-## Bash strict mode
-set -euo pipefail
+## Default parameter values
+fa_in=""
+outdir=""
+busco_db=""
+type=transcriptome
 
-## Command-line args
-fa_in=$1
-outdir=$2
-busco_db=$3
+## Get parameter values
+while getopts ':i:o:d:t:h' flag; do
+    case "${flag}" in
+    i) fa_in="$OPTARG" ;;
+    o) outdir="$OPTARG" ;;
+    d) busco_db="$OPTARG" ;;
+    t) type="$OPTARG" ;;
+    h) Help && exit 0 ;;
+    \?) echo "## $0: ERROR: Invalid option -$OPTARG" >&2 && exit 1 ;;
+    :) echo "## $0: ERROR: Option -$OPTARG requires an argument." >&2 && exit 1 ;;
+    esac
+done
+
+## Report
+echo "## Starting script busco.sh"
+date
+echo
 
 ## Check input
-[[ "$#" != 3 ]] && echo "## ERROR: Please provide 3 arguments - you provided $#" && exit 1
 [[ ! -f "$fa_in" ]] && echo "## ERROR: Input file $fa_in does not exist" && exit 1
+
+
+# SOFTWARE ---------------------------------------------------------------------
+module load python/3.6-conda5.2
+source activate /users/PAS0471/jelmer/miniconda3/envs/busco-env
+
+
+# OTHER SETUP ------------------------------------------------------------------
+## Bash strict mode
+set -euo pipefail
 
 ## Other paramaters
 n_cores=$SLURM_CPUS_PER_TASK
 
 ## Report
-echo "## Starting script busco.sh"
-date
-echo "## Input FASTA:         $fa_in"
-echo "## Output dir:          $outdir"
-echo "## BUSCO db:            $busco_db"
-echo "## Number of cores:     $n_cores"
+echo "## Input FASTA:           $fa_in"
+echo "## Output dir:            $outdir"
+echo "## BUSCO db:              $busco_db"
+echo "## Assembly type:         $type"
+echo "## Number of cores:       $n_cores"
 echo -e "-------------------------------\n"
 
 ## Create output dir if needed
@@ -45,12 +85,11 @@ mkdir -p "$outdir"
 
 # RUN BUSCO -----------------------------------------------------------------
 echo "## Now running busco..."
-
 busco \
     -i "$fa_in" \
     -o "$outdir" \
     -l "$busco_db" \
-    -m transcriptome \
+    -m "$type" \
     -c "$n_cores" \
     --force
 
@@ -62,6 +101,9 @@ ls -lh "$outdir"
 
 echo -e "\n## Done with script busco.sh"
 date
+echo
+sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+echo
 
 
 # INFO -------------------------------------------------------------------------
