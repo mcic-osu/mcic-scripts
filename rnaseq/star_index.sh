377ef405c0360b231660af03ec3cffd9cc443780
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-04-04 13:00:54 -0400

    Add overhang option

--- rnaseq/star_index.sh
+++ rnaseq/star_index.sh
@@ -24,6 +24,7 @@ Help() {
   echo "## -a STRING      Reference annotation (GFF/GTF) file [default: no GFF/GTF, but this is not recommended]"
   echo "## -s INTEGER     Index size                          [default: automatically determined from genome size]"
   echo "## -r INTEGER     Read length                         [default: '150' (bp)]"
+  echo "## -v INTEGER     Overhang                            [default: read length minus 1]"
   echo "## -h             Print this help message and exit"
   echo
   echo "## Example: $0 -i refdata/my_genome.fa -o refdata/star_index -a refdata/my_genome.gff"
@@ -36,16 +37,18 @@ ref_fa=""
 index_dir=""
 index_size=""
 gff=""
+overhang=""
 read_len=150
 
 ## Parse command-line options
-while getopts ':i:a:o:s:r:h' flag; do
+while getopts ':i:a:o:s:r:v:h' flag; do
   case "${flag}" in
   i) ref_fa="$OPTARG" ;;
   a) gff="$OPTARG" ;;
   o) index_dir="$OPTARG" ;;
   s) index_size="$OPTARG" ;;
   r) read_len="$OPTARG" ;;
+  v) overhang="$OPTARG" ;;
   h) Help && exit 0 ;;
   \?) echo "## $0: ERROR: Invalid option -$OPTARG" >&2 && exit 1 ;;
   :) echo "## $0: ERROR: Option -$OPTARG requires an argument." >&2 && exit 1 ;;
@@ -70,21 +73,15 @@ echo
 [[ ! -f "$ref_fa" ]] && echo "## ERROR: Input FASTA (-i) $ref_fa does not exist" >&2 && exit 1
 [[ "$gff" != "" ]] && [[ ! -f "$gff" ]] && echo "## ERROR: Input file GFF (-a) $gff does not exist" >&2 && exit 1
 
-## Report
-echo "## Input FASTA file:             $ref_fa"
-[[ "$gff" != "" ]] && echo "## Input GFF file:               $gff"
-echo "## Genome index dir (output):    $index_dir"
-echo "## Read length:                  $read_len"
-echo
-
 ## Determine index size
 if [ "$index_size" = "" ]; then
     echo "## Automatically determining index size..."
     genome_size=$(grep -v "^>" "$ref_fa" | wc -c)
     index_size=$(python -c "import math; print(math.floor(math.log($genome_size, 2)/2 -1))")
-    echo "## Genome size: $genome_size / Index size: $index_size"
+    echo "## Genome size:                  $genome_size"
+    echo "## Index size:                   $index_size"
 else
-    echo "## STAR index size:              $index_size"
+    echo "## Index size:                   $index_size"
 fi
 
 ## If a GFF file is provided, build the appropriate argument for STAR
@@ -95,15 +92,21 @@ else
 fi
 
 ## Overhang length should be read length minus 1
-overhang=$(( read_len - 1 ))
+[[ $overhang = "" ]] && overhang=$(( read_len - 1 ))
 
 ## Make output dir if needed
 mkdir -p "$index_dir"
 
-
-# RUN STAR ---------------------------------------------------------------------
+## Report
+echo "## Input FASTA file:             $ref_fa"
+[[ "$gff" != "" ]] && echo "## Input GFF file:               $gff"
+echo "## Genome index dir (output):    $index_dir"
+echo "## Read length:                  $read_len"
+echo "## Overhang:                     $overhang"
 echo -e "-----------------------\n"
 
+
+# RUN STAR ---------------------------------------------------------------------
 ## STAR doesn't accept zipped FASTA files -- unzip if needed
 if [[ $ref_fa = *gz ]]; then
     echo "## Unzipping gzipped FASTA file..."
