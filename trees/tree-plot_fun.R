9b823c8f38b13e0d898a84dcfbd4c8e228393133
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-06-30 12:46:16 -0400

    Flexible plotting with annotation

--- trees/tree-plot_fun.R
+++ trees/tree-plot_fun.R
@@ -1,38 +1,69 @@
 ## Function to plot the tree
-plot_tree <- function(tree, annot, alignment, fig_file,
-                      msa_offset) {
+plot_tree <- function(tree,
+                      alignment = NULL,
+                      annot = NULL, label_cols = NA, color_col = NULL,
+                      fig_file = "tree.png",
+                      plot_msa = FALSE, msa_offset = "auto") {
 
-  ## Plot the tree
   if (!is.null(annot)) {
-    p <- ggtree(tree) %<+% annot +
-      geom_tiplab(aes(label = tree_label, color = is_query)) +
-      scale_color_manual(values = c("black", "red"), guide = "none") +
-      guides(size = "none", color = "none")
+    p <- ggtree(tree) %<+% annot
+    
+    if (is.na(label_cols[1])) p <- p + geom_tiplab(aes_string(label = annot_col))
+    
+    if (!is.na(label_cols[1])) {
+      if(!is.null(color_col)) {
+        p <- p + geom_tiplab(aes_string(label = label_cols[1],
+                                        color = color_col),
+                             align = TRUE)
+        if (color_col == label_cols[1]) p <- p + guides(color = "none")
+      } else {
+        p <- p + geom_tiplab(aes_string(label = label_cols[1]))
+      }
+    }
   } else {
-    p <- ggtree(tree) + geom_tiplab()
+    p <- ggtree(tree) +
+      geom_tiplab()
   }
   
-  p <- p +
-    theme(plot.margin = margin(0.2, 1, 0.2, 0.2, "cm")) +
-    coord_cartesian(clip = "off")
-
-  ## Determine offset for Multiple Sequence Alignment (MSA) visualization
-  if (msa_offset == "textlen") {
-      text_len <- max(nchar(annot$tree_label))
-      tree_dp <- max(node.depth(tree))
-      msa_offset <- (text_len * 0.005) + (tree_dp * 0.02)
-      if (msa_offset > 0.5) msa_offset <- 0.5
+  if (!is.na(label_cols[2])) {
+    p <- p + geom_tiplab(aes_string(label = label_cols[2]),
+                         align = TRUE, linetype = "blank", offset = 3.5)
+  }
+  if (!is.na(label_cols[3])) {
+    p <- p + geom_tiplab(aes_string(label = label_cols[3]),
+                         align = TRUE, linetype = "blank", offset = 5)
   }
 
-  ## Add Multiple Sequence Alignment (MSA) visualization
-  if (msa_offset != "auto") {
-    msaplot(p, alignment, offset = msa_offset, width = 2)
+  p <- p +
+    theme(plot.margin = margin(0.2, 8, 0.2, 0.2, "cm")) +
+    coord_cartesian(clip = "off")
+
+  if (plot_msa == TRUE) {
+    ## Determine offset for Multiple Sequence Alignment (MSA) visualization
+    if (msa_offset == "textlen") {
+        text_len <- max(nchar(annot$tree_label))
+        tree_dp <- max(node.depth(tree))
+        msa_offset <- (text_len * 0.005) + (tree_dp * 0.02)
+        if (msa_offset > 0.5) msa_offset <- 0.5
+    }
+
+    ## Add Multiple Sequence Alignment (MSA) visualization
+    if (msa_offset != "auto") {
+        msaplot(p, alignment, offset = msa_offset, width = 2)
+    } else {
+        msaplot(p, alignment, width = 2)
+    }
+
+    plot_width <- 14
+    plot_height <- 7
+
   } else {
-    msaplot(p, alignment, width = 2)
+    plot_width <- 8
+    plot_height <- 10
   }
   
   ## Save the plot to file
-  ggsave(fig_file, width = 14, height = 7)
+  ggsave(fig_file, width = plot_width, height = plot_height)
 }
 
 ## Function to prep the annotation dataframe associated with the tree
