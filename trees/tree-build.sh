f6ef68f31da07606961872df42c6f8e6b37bab42
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-10-12 17:25:37 -0400

    Make plot optional

--- trees/tree-build.sh
+++ trees/tree-build.sh
@@ -11,30 +11,26 @@
 ## Help function
 Help() {
     echo
-    echo "## $0: Align sequences and build a tree."
+    echo "$0: Align sequences from a FASTA file, build (and optionally plot) a tree."
     echo
-    echo "## Syntax: $0 -i <input-fasta> -o <output-fasta> -t <output-tree> [-p alignment-program]"
+    echo "Syntax: $0 -i <input-fasta> -o <aligned-fasta> ..."
     echo
     echo "## Required options:"
-    echo "## -i STR     Input (unaligned) FASTA file"
-    echo "## -o STR     Output (aligned) FASTA file"
-    echo "## -t STR     Output tree file"
+    echo "    -i FILE       Input (unaligned) FASTA file"
+    echo "    -o FILE       Output (aligned) FASTA file"
+    echo "    -t FILE       Output tree file"
     echo
     echo "## Other options:"
-    echo "## -p STR     Alignment program ('mafft', 'muscle', 'tcoffee', or 'none') [default: 'mafft']"
-    echo "## -f STR     Additional argument(s) for mafft"
-    echo "## -l         Make a plot of the tree [default: no plot]"
-    echo "## -a STR     Annotation df for tree plot"
-    echo "## -h         Print this help message and exit"
+    echo "    -a FILE       Annotation df for tree plot"
+    echo "    -f STRING     Additional argument(s) for mafft"
+    echo "    -h            Print this help message and exit"
+    echo "    -p STRING     Alignment program ('mafft', 'muscle', 'tcoffee', or 'none') [default: 'mafft']"
+    echo "    -l            Make a plot of the tree [default: no plot]"
     echo
-    echo "## Example: $0 -i in.fa -o out.fa -t out.tree -p muscle"
-    echo "## To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
+    echo "Example:      $0 -i in.fa -o out.fa -t out.tree -p muscle"
+    echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
     echo
 }
-
-## Report
-echo "## Starting script tree-build.sh..."
-date
 
 ## Option defaults
 fa_in=""
@@ -61,6 +57,8 @@ while getopts ':i:o:t:p:a:f:lh' flag; do
     esac
 done
 
+
+# SETUP ------------------------------------------------------------------------
 ## Check input
 [[ "$fa_in" = "" ]] && echo "## ERROR: Please specify a FASTA input file with -i " >&2 && exit 1
 [[ "$aln" = "" ]] && echo "## ERROR: Please specify a FASTA output file with -o " >&2 && exit 1
@@ -70,23 +68,22 @@ done
 
 ## Software and scripts
 module load python/3.6-conda5.2
-conda activate /users/PAS0471/jelmer/miniconda3/envs/fasttree-env
+source activate /users/PAS0471/jelmer/miniconda3/envs/fasttree-env
 #? Note: additional Conda environments are loaded below, depending on the alignment program, etc
 
 ## Bash strict mode
 set -euo pipefail
 
 ## Process parameters
-### Output dirs
 outdir_alignment=$(dirname "$aln")
 outdir_fasttree=$(dirname "$tree")
-mkdir -p "$outdir_alignment" "$outdir_fasttree"
-### Tree figure
 tree_fig=${aln%.*}.png
-### Number of cores
 n_cores=$SLURM_CPUS_PER_TASK
 
 ## Report
+echo "## Starting script tree-build.sh..."
+date
+echo
 echo "## Unaligned FASTA file (input):     $fa_in"
 echo "## Aligned FASTA file (output):      $aln"
 echo "## Tree file (output):               $tree"
@@ -96,24 +93,26 @@ echo "## Alignment program:                $align_program"
 [[ "$args_mafft" != "" ]] && echo "## Additional args for mafft:        $args_mafft"
 echo -e "--------------------------\n"
 
+## Make output dir
+mkdir -p "$outdir_alignment" "$outdir_fasttree"
 
 # ALIGN ------------------------------------------------------------------------
 if [ "$align_program" = mafft ]; then
     echo "## Starting alignment with MAFFT..."
-    conda activate --stack mafft-env
+    conda activate mafft-env
     
     mafft --reorder --auto --adjustdirection \
         --leavegappyregion $args_mafft "$fa_in" > "$aln"
 
 elif [ "$align_program" = muscle ]; then
     echo "## Starting alignment with MUSCLE..."
-    conda activate --stack muscle-env
+    conda activate muscle-env
     
     muscle -in "$fa_in" -out "$aln"
 
 elif [ "$align_program" = tcoffee ]; then
     echo "## Starting alignment with t-coffee..."
-    conda activate --stack t_coffee-env
+    conda activate t_coffee-env
     
     aln_prefix=${aln%.*}
     
@@ -132,6 +131,7 @@ sed -i -E -e 's/(^>[^ ]+) .*/\1/' -e 's/^>_R_/>/' "$aln"
 # BUILD TREE -------------------------------------------------------------------
 echo -e "\n---------------------------"
 echo -e "## Starting tree building with FastTree..."
+conda activate /users/PAS0471/jelmer/miniconda3/envs/fasttree-env
 cat "$aln" | FastTree -gamma -nt -gtr -out "$tree"
 
 
@@ -152,14 +152,13 @@ fi
 
 
 # WRAP UP ----------------------------------------------------------------------
-echo -e "\n---------------------------"
+echo -e "\n------------------------------"
 echo -e "\n## Output files:"
 ls -lh "$aln"
 ls -lh "$tree"
-ls -lh "$tree_fig"
+[[ "$make_plot" = true ]] && ls -lh "$tree_fig"
 echo -e "\n## Done with script tree.sh"
 date
 echo
-
-#? Commands follow https://www.biorxiv.org/content/10.1101/2020.11.24.396820v1.full
-#? GitHub repo: https://github.com/Cyoung02/Phylogenetic-Inference-Benchmarking
+sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+echo
