9f73fbbf961790b25ccfe36717ba15f370e977ce
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-10-12 09:18:24 -0400

    Improved reporting and help

--- assembly/nextdenovo.sh
+++ assembly/nextdenovo.sh
@@ -8,24 +8,35 @@
 # PARSE OPTIONS ----------------------------------------------------------------
 ## Help function
 Help() {
-  echo
-  echo "$0: Run Nextdenovo to assemble a genome."
-  echo
-  echo "Syntax: $0 -c <config-file>..."
-  echo
-  echo "Required options:"
-  echo "    -c FILE           Config file for NextDenovo"
-  echo "                      See the example file 'nextdenovo.cfg' in the same dir as this script"
-  echo
-  echo "Other options:"
-  echo "    -a STRING         Other argument(s) to pass to Nextdenovo"
-  echo "    -h                Print this help message and exit"
-  echo
-  echo "Example:              $0 -i data/my.fastq.gz -o results/canu -p my_genome -s 250m"
-  echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
-  echo
-  echo "Nextdenovo documentation: https://nextdenovo.readthedocs.io/en/latest/OPTION.html"
-  echo
+    echo
+    echo "=================================================================================================="
+    echo "$0: Run Nextdenovo to assemble a genome"
+    echo "=================================================================================================="
+    echo
+    echo "SYNTAX:"
+    echo "------------------"
+    echo "  sbatch $0 -c <config-file> ..."
+    echo "  bash $0 -h"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "------------------"
+    echo "    -c FILE           Config file for NextDenovo"
+    echo "                      See the example file 'nextdenovo.cfg' in the same dir as this script."
+    echo "                      This config file will set the input files, output dir, and genome size."
+    echo
+    echo "OTHER OPTIONS:"
+    echo "------------------"
+    echo "    -a STRING         Other argument(s) to pass to Nextdenovo"
+    echo "    -h                Print this help message and exit"
+    echo
+    echo "EXAMPLE COMMAND:"
+    echo "------------------"
+    echo "  sbatch 0 -c nextdenovo.cfg"
+    echo
+    echo "SOFTWARE DOCUMENTATION:"
+    echo "------------------"
+    echo "- https://nextdenovo.readthedocs.io/en/latest/OPTION.html"
+    echo
 }
 
 ## Option defaults
@@ -34,13 +45,13 @@ more_args=""
 
 ## Parse command-line options
 while getopts ':c:a:h' flag; do
-  case "${flag}" in
-    c) config_file="$OPTARG" ;;
-    a) more_args="$OPTARG" ;;
-    h) Help && exit 0 ;;
-    \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
-    :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
-  esac
+    case "${flag}" in
+        c) config_file="$OPTARG" ;;
+        a) more_args="$OPTARG" ;;
+        h) Help && exit 0 ;;
+        \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
+        :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
+    esac
 done
 
 
@@ -52,18 +63,27 @@ source activate /fs/project/PAS0471/jelmer/conda/nextdenovo-env
 ## Bash script settings
 set -euo pipefail
 
+## Check input
+[[ $config_file = "" ]] && echo -e "\n## ERROR: Please specify a config file -c \n" >&2 && exit 1
+[[ ! -f $config_file ]] && echo -e "\n## ERROR: Config file $config_file does not exist! \n" >&2 && exit 1
+
 ## Report
-echo -e "\n## Starting script nextdenovo.sh"
+echo -e "\n=========================================================================="
+echo "## STARTING SCRIPT NEXTDENOVO.SH"
 date
-echo
+echo -e "==========================================================================\n"
 echo "## Config file:                              $config_file"
 [[ $more_args != "" ]] && echo "## Other arguments to pass to NextDenovo:    $more_args"
 echo -e "--------------------\n"
 
+## Show config file
+echo "## Printing the contents of the config file:"
+cat "$config_file"
+echo -e "--------------------\n"
+
 
 # MAIN -------------------------------------------------------------------------
-nextDenovo \
-    "$config_file" $more_args
+nextDenovo "$config_file" $more_args
 
 
 # WRAP UP ----------------------------------------------------------------------
--- assembly/smartdenovo.sh
+++ assembly/smartdenovo.sh
@@ -3,50 +3,116 @@
 #SBATCH --account=PAS0471
 #SBATCH --cpus-per-task=12
 #SBATCH --time=24:00:00
+#SBATCH --job-name=smartdenovo
 #SBATCH --output=slurm-smartdenovo-%j.out
 
+# PARSE OPTIONS ----------------------------------------------------------------
+## Help function
+Help() {
+    echo
+    echo "=================================================================================================="
+    echo "$0: Run SmartDenovo to assemble a genome"
+    echo "=================================================================================================="
+    echo
+    echo "SYNTAX:"
+    echo "------------------"
+    echo "  sbatch $0 -i <FASTQ-file(s)> -o <output-prefix> ..."
+    echo "  bash $0 -h"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "------------------"
+    echo "    -i FILE/STRING    One or more input FASTQ files, quoted and space-separated"
+    echo "                      For example: '-i \"data/fastq/A.fq.gz data/fastq/B.fq.gz\"'"
+    echo "    -o STRING         Output prefix: output dir + genome ID"
+    echo "                      For example: '-o results/smartdenovo/sus_scrofa'"
+    echo
+    echo "OTHER OPTIONS:"
+    echo "------------------"
+    echo "    -a STRING         Other argument(s) to pass to SmartDenovo"
+    echo "    -h                Print this help message and exit"
+    echo
+    echo "EXAMPLE COMMAND:"
+    echo "------------------"
+    echo "  sbatch $0 -i \"data/fastq/A.fq.gz data/fastq/B.fq.gz\" -o results/smartdenovo/sus_scrofa"
+    echo
+    echo "SOFTWARE DOCUMENTATION:"
+    echo "------------------"
+    echo "- https://github.com/ruanjue/smartdenovo "
+    echo
+}
+
+## Option defaults
+infiles=""
+output_prefix=""
+more_args=""
+
+## Parse command-line options
+while getopts 'i:o:a:h' flag; do
+    case "${flag}" in
+        i) infiles="$OPTARG" ;;
+        o) output_prefix="$OPTARG" ;;
+        a) more_args="$OPTARG" ;;
+        h) Help && exit 0 ;;
+        \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
+        :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
+    esac
+done
+
+
+# SETUP ------------------------------------------------------------------------
 ## Load software
-module load python
+module load python/3.6-conda5.2
 source activate /fs/project/PAS0471/jelmer/conda/smartdenovo-env
 
 ## Bash strict settings
 set -euo pipefail
 
-## Process command-line args
-input=$1
-prefix=$2
+## Check input
+[[ $infiles = "" ]] && echo -e "\n## ERROR: Please specify one or more input files with -i \n" >&2 && exit 1
+[[ $output_prefix = "" ]] && echo -e "\n## ERROR: Please specify an output prefix with -o \n" >&2 && exit 1
 
 ## Report
-echo 
-echo  "## Starting script smartdenovo.sh"
+echo -e "\n=========================================================================="
+echo "## STARTING SCRIPT SMARTDENOVO.SH"
 date
+echo -e "==========================================================================\n"
+echo "## Input files:       $infiles"
+echo "## Output prefix:     $output_prefix"
+[[ $more_args != "" ]] && echo "## Other arguments to pass to Smartdenovo:    $more_args"
 echo
-echo "## Input:             $input"
-echo "## Output prefix:     $prefix"
-echo
+echo "## Listing input files:"
+for infile in $infiles; do
+    [[ ! -f $infile ]] && echo -e "\n## ERROR: Input file $infile does not exist! \n" >&2 && exit 1
+    ls -lh "$infile"
+done
+echo -e "--------------------\n"
 
+
+# MAIN -------------------------------------------------------------------------
 ## Create output dir
-outdir=$(dirname "$prefix")
+outdir=$(dirname "$output_prefix")
 mkdir -p "$outdir"
 
 ## Generate a Makefile for smartdenovo to run
 smartdenovo.pl \
-    -p "$prefix" \
+    -p "$output_prefix" \
     -t "$SLURM_CPUS_PER_TASK" \
     -c 1 \
-    "$input" \
-    > "$prefix".mak
+    $infiles \
+    > "$output_prefix".mak
+
+#? "After assembly, the raw unitigs are reported in file prefix.lay.utg and consensus unitigs in prefix.cns"
+#? -c 1 => make consensus
+#! -J min read length -- default = 5,000
 
 ## Run SmartDenovo by running the Makefile
-make -f "$prefix".mak
+make -f "$output_prefix".mak
 
-## Report
-echo
+
+# WRAP UP ----------------------------------------------------------------------
+echo -e "\n-------------------------------"
 echo "## Done with script smartdenovo.sh"
 date
-
-## Docs
-# https://github.com/ruanjue/smartdenovo
-# "After assembly, the raw unitigs are reported in file prefix.lay.utg and consensus unitigs in prefix.cns"
-#> -c 1 => make consensus
-#! -J min read length -- default = 5,000
+echo
+sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+echo
