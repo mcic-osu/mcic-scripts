fe5cad0cd9bb44bdbfd10d91add1dd5441f4e33c
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-11-15 16:20:58 -0500

    Fix, use local

--- annot/deeptmhmm.sh
+++ annot/deeptmhmm.sh
@@ -1,53 +1,48 @@
 #!/bin/bash
 
-#! THIS SCRIPT DOES NOT WORK -- USE SERVER INSTEAD https://dtu.biolib.com/DeepTMHMM/
-
 #SBATCH --account=PAS0471
 #SBATCH --time=6:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
 #SBATCH --job-name=deeptmhmm
 #SBATCH --output=slurm-deeptmhmm-%j.out
 
 # PARSE ARGUMENTS --------------------------------------------------------------
 ## Help function
 Help() {
-  echo
-  echo "$0: Run DeepTMHMM. DOES NOT CURRENTLY WORK!! Use the server instead: https://dtu.biolib.com/DeepTMHMM/"
-  echo
-  echo "Syntax: $0 -i <input-FASTA> -o <output-dir>..."
-  echo
-  echo "Required options:"
-  echo      "-i STRING         Input amino acid FASTA file"
-  echo      "-o STRING         Output directory"
-  echo
-  echo "Other options:"
-  echo      "-a STRING         Other argument(s) to pass to DeepTMHMM"
-  echo      "-h                Print this help message and exit"
-  echo
-  echo "Example:               $0 -i ref/aa.fa -o results/tmhmm"
-  echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
-  echo
-  echo "DeepTMHMM paper: https://www.biorxiv.org/content/10.1101/2022.04.08.487609v1"
-  echo "DeepTMHMM online server: https://dtu.biolib.com/DeepTMHMM"
-  echo
+    echo
+    echo "$0: Run DeepTMHMM."
+    echo
+    echo "Syntax: $0 -i <input-FASTA> -o <output-dir>..."
+    echo
+    echo "Required options:"
+    echo      "-i STRING         Input amino acid FASTA file"
+    echo      "-o STRING         Output directory"
+    echo
+    echo "Other options:"
+    echo      "-h                Print this help message and exit"
+    echo
+    echo "Example:               $0 -i ref/aa.fa -o results/tmhmm"
+    echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
+    echo
+    echo "DeepTMHMM paper: https://www.biorxiv.org/content/10.1101/2022.04.08.487609v1"
+    echo "DeepTMHMM online server: https://dtu.biolib.com/DeepTMHMM"
+    echo
 }
 
 ## Option defaults
 infile=""
 outdir=""
-more_args=""
 
 ## Parse command-line options
-while getopts ':i:o:a:h' flag; do
-  case "${flag}" in
-    i) infile="$OPTARG" ;;
-    o) outdir="$OPTARG" ;;
-    a) more_args="$OPTARG" ;;
-    h) Help && exit 0 ;;
-    \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
-    :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
-  esac
+while getopts ':i:o:h' flag; do
+    case "${flag}" in
+        i) infile="$OPTARG" ;;
+        o) outdir="$OPTARG" ;;
+        h) Help && exit 0 ;;
+        \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
+        :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
+    esac
 done
 
 ## Check input
@@ -59,29 +54,36 @@ done
 # SETUP ------------------------------------------------------------------------
 ## Load software
 module load python/3.6-conda5.2
-source activate /fs/project/PAS0471/jelmer/conda/deeptmhmm
+source activate /fs/project/PAS0471/jelmer/conda/deeptmhmm2
+DEEPTMHMM_DIR=/fs/ess/PAS0471/jelmer/conda/deeptmhmm2/biolib_container/openprotein/
 
 ## Bash strict mode
 set -euo pipefail
 
-## Make output dir
-mkdir -p "$outdir"
+## Make input and output absolute
+[[ ! "$infile" =~ ^/ ]] && infile="$PWD"/"$infile"
+[[ ! "$outdir" =~ ^/ ]] && outdir="$PWD"/"$outdir"
 
 ## Report
 echo
-echo "## Starting script deeptmhmm.sh"
+echo "Starting script deeptmhmm.sh"
 date
-echo
-echo "## Input AA FASTA:                        $infile"
-echo "## Output dir:                            $outdir"
-[[ "$more_args" != "" ]] && echo "## Other arguments to pass to DeepTMHMM:    $more_args"
+echo "Input AA FASTA:    $infile"
+echo "Output dir:        $outdir"
 echo -e "--------------------\n"
 
 
 # MAIN -------------------------------------------------------------------------
+## Make output dir
+mkdir -p "$outdir"
+
 echo "## Now running DeepTMHMM..."
-biolib run DTU/DeepTMHMM \
-    --fasta "$infile" $more_args
+
+cd "$DEEPTMHMM_DIR" || exit
+
+python3 predict.py \
+    --fasta "$infile" \
+    --outdir "$outdir"
 
 
 # WRAP-UP ----------------------------------------------------------------------
