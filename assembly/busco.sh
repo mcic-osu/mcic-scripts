0151def727dd2e1442070f656de67bda79ce9bbd
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-11-28 10:44:56 -0500

    Change to new style

--- assembly/busco.sh
+++ assembly/busco.sh
@@ -24,16 +24,21 @@ Print_help() {
     echo "  sbatch $0 -i <input-FASTA> -o <output-dir> -d <db-name> ..."
     echo
     echo "REQUIRED OPTIONS:"
-    echo "  -i <file>           Input FASTA file with the assembly"
-    echo "  -o <dir>            Output directory"
-    echo "  -d <string>         Busco database name (see https://busco.ezlab.org/list_of_lineages.html)"
+    echo "  -i/--infile     <file>      Input assembly FASTA file"
+    echo "  -o/--outdir     <dir>       Output dir (will be created if needed)"
+    echo "  -d/--db         <string>    Busco database name (see https://busco.ezlab.org/list_of_lineages.html)"
     echo
     echo "OTHER KEY OPTIONS:"
-    echo "  -m <string>         Run mode, i.e. assembly type               [default: 'genome']"
-    echo "                      Valid options: 'genome', 'transcripttome', or 'proteins'"
+    echo "  -m/--mode       <string>    Mode, i.e. assembly type               [default: 'genome']"
+    echo "                              Valid options: 'genome', 'transcripttome', or 'proteins'"
+    echo "  --more-args     <str>       Quoted string with additional argument(s) to pass to Busco"
     echo
     echo "UTILITY OPTIONS:"
-    echo "  -h                Print this help message and exit"
+    echo "  --dryrun                Dry run: don't execute commands, only parse arguments and report"
+    echo "  --debug                 Run the script in debug mode (print all code)"
+    echo "  -h                      Print this help message and exit"
+    echo "  --help                  Print the help for Busco and exit"
+    echo "  -v/--version            Print the version of Busco and exit"
     echo
     echo "EXAMPLE COMMANDS:"
     echo "  sbatch $0 -i results/assembly.fa -o results/busco -d bacteria_odb"
@@ -45,8 +50,8 @@ Print_help() {
 
 ## Load software
 Load_software() {
-    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do conda deactivate; done
     module load miniconda3/4.12.0-py39
+    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do source deactivate; done
     source activate /fs/ess/PAS0471/jelmer/conda/busco
 }
 
@@ -56,74 +61,147 @@ Print_version() {
     busco --version
 }
 
+## Print help for the focal program
+Print_help_program() {
+    Load_software
+    busco --help
+}
+
+## Print SLURM job resource usage info
+Resource_usage() {
+    echo
+    ${e}sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime,MaxVMSize | \
+        grep -Ev "ba|ex"
+    echo
+}
+
+## Print SLURM job requested resources
+Print_resources() {
+    set +u
+    echo "# SLURM job information:"
+    echo "Account (project):    $SLURM_JOB_ACCOUNT"
+    echo "Job ID:               $SLURM_JOB_ID"
+    echo "Job name:             $SLURM_JOB_NAME"
+    echo "Memory (per node):    $SLURM_MEM_PER_NODE"
+    echo "CPUs per task:        $SLURM_CPUS_PER_TASK"
+    [[ "$SLURM_NTASKS" != 1 ]] && echo "Nr of tasks:          $SLURM_NTASKS"
+    [[ -n "$SBATCH_TIMELIMIT" ]] && echo "Time limit:           $SBATCH_TIMELIMIT"
+    echo "======================================================================"
+    echo
+    set -u
+}
+
+## Set the number of threads/CPUs
+Set_threads() {
+    set +u
+    if [[ "$slurm" = true ]]; then
+        if [[ -n "$SLURM_CPUS_PER_TASK" ]]; then
+            threads="$SLURM_CPUS_PER_TASK"
+        elif [[ -n "$SLURM_NTASKS" ]]; then
+            threads="$SLURM_NTASKS"
+        else 
+            echo "WARNING: Can't detect nr of threads, setting to 1"
+            threads=1
+        fi
+    else
+        threads=1
+    fi
+    set -u
+}
+
+## Resource usage information
+Time() {
+    /usr/bin/time -f \
+        '\n# Ran the command:\n%C \n\n# Run stats by /usr/bin/time:\nTime: %E   CPU: %P    Max mem: %M K    Exit status: %x \n' \
+        "$@"
+}   
+
 ## Exit upon error with a message
 Die() {
-    printf "\n$0: ERROR: %s\n" "$1" >&2
-    echo -e "Exiting\n" >&2
+    error_message=${1}
+    error_args=${2-none}
+    
+    echo >&2
+    echo "=====================================================================" >&2
+    printf "$0: ERROR: %s\n" "$error_message" >&2
+    echo -e "\nFor help, run this script with the '-h' option" >&2
+    echo "For example, 'bash mcic-scripts/qc/fastqc.sh -h'" >&2
+    if [[ "$error_args" != "none" ]]; then
+        echo -e "\nArguments passed to the script:" >&2
+        echo "$error_args" >&2
+    fi
+    echo -e "\nEXITING..." >&2
+    echo "=====================================================================" >&2
+    echo >&2
     exit 1
 }
+
 
 # ==============================================================================
 #                          CONSTANTS AND DEFAULTS
 # ==============================================================================
+## Option defaults
 assembly_type=genome
 
 debug=false
-dryrun=false
+dryrun=false && e=""
+slurm=true
+
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
 ## Placeholder defaults
-fa_in=""
+infile=""
 outdir=""
 busco_db=""
+more_args=""
 
 ## Parse command-line args
-while getopts ':i:o:d:m:h' flag; do
-    case "${flag}" in
-        i) fa_in="$OPTARG" ;;
-        o) outdir="$OPTARG" ;;
-        d) busco_db="$OPTARG" ;;
-        m) assembly_type="$OPTARG" ;;
-        h) Print_help; exit 0 ;;
-        \?) Die "Invalid option -$OPTARG" ;;
-        :) Die "Option -$OPTARG requires an argument." ;;
-    esac
-done
+all_args="$*"
 
-## Check input
-[[ "$fa_in" = "" ]] && Die "Please specify an input FASTA file with -i"
-[[ "$outdir" = "" ]] && Die "Please specify an output dir file with -o"
-[[ "$busco_db" = "" ]] && Die "Please specify a Busco database name with -d"
-[[ ! -f "$fa_in" ]] && Die "Input file $fa_in does not exist"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --infile )     shift && infile=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        -d | --db )         shift && busco_db=$1 ;;
+        -m | --mode )       shift && assembly_type=$1 ;;
+        --more-args )       shift && more_args=$1 ;;
+        -v | --version )    Print_version; exit 0 ;;
+        -h )                Print_help; exit 0 ;;
+        --help )            Print_help_program; exit 0;;
+        --dryrun )          dryrun=true && e="echo ";;
+        --debug )           debug=true ;;
+        * )                 Die "Invalid option $1" "$all_args" ;;
+    esac
+    shift
+done
 
 
 # ==============================================================================
 #                          OTHER SETUP
 # ==============================================================================
+## In debugging mode, print all commands
 [[ "$debug" = true ]] && set -o xtrace
 
-## Software
-Load_software
+## Check if this is a SLURM job
+[[ -z "$SLURM_JOB_ID" ]] && slurm=false
 
-## Bash strict mode
-set -euo pipefail
-
-## Get number of threads
-if [[ "$dryrun" = false ]]; then
-    if [[ -z "$SLURM_CPUS_PER_TASK" ]]; then
-        n_threads="$SLURM_NTASKS"
-    else
-        n_threads="$SLURM_CPUS_PER_TASK"
-    fi
-fi
+## Load software and set nr of threads
+[[ "$dryrun" = false ]] && Load_software
+Set_threads
 
 ## If needed, make input path absolute because we have to move into the outdir
-[[ ! $fa_in =~ ^/ ]] && fa_in="$PWD"/"$fa_in"
+[[ ! $infile =~ ^/ ]] && infile="$PWD"/"$infile"
 
 ## Get a sample/assembly ID from the filename
-fileID=$(basename "$fa_in" | sed -E 's/.fn?as?t?a?//')
+fileID=$(basename "$infile" | sed -E 's/.fn?as?t?a?//')
+
+## Check input
+[[ "$infile" = "" ]] && Die "Please specify an input FASTA file with -i"
+[[ "$outdir" = "" ]] && Die "Please specify an output dir file with -o"
+[[ "$busco_db" = "" ]] && Die "Please specify a Busco database name with -d"
+[[ ! -f "$infile" ]] && Die "Input file $infile does not exist"
 
 ## Report
 echo
@@ -131,37 +209,49 @@ echo "==========================================================================
 echo "               STARTING SCRIPT BUSCO.SH"
 date
 echo "=========================================================================="
-echo "Input FASTA:               $fa_in"
-echo "Output dir:                $outdir"
-echo "BUSCO db:                  $busco_db"
-echo "Mode (assembly type):      $assembly_type"
+echo "All arguments to this script:     $all_args"
+echo "Input FASTA:                      $infile"
+echo "Output dir:                       $outdir"
+echo "BUSCO db:                         $busco_db"
+echo "Mode (assembly type):             $assembly_type"
+[[ $more_args != "" ]] && echo "Other arguments for Busco:        $more_args"
 echo
-echo "Number of cores:           $n_threads"
-echo "Assembly ID (inferred):    $fileID"
+echo "Number of cores:                  $threads"
+echo "Assembly ID (inferred):           $fileID"
+echo "Listing the input file(s):"
+ls -lh "$infile"
+[[ $dryrun = true ]] && echo -e "\nTHIS IS A DRY-RUN"
 echo "=========================================================================="
-echo
+
+## Print reserved resources
+[[ "$slurm" = true ]] && Print_resources
 
 
 # ==============================================================================
 #                               RUN
 # ==============================================================================
-## Create output dir if needed
-mkdir -p "$outdir"/logs
+if [[ "$dryrun" != false ]]; then
+    ## Create output dir if needed
+    mkdir -p "$outdir"/logs
 
-## Move into output dir
-cd "$outdir" || exit 1
+    ## Move into output dir
+    cd "$outdir" || exit 1
 
-## Run BUSCO
-echo "## Now running BUSCO..."
-[[ "$debug" = false ]] && set -o xtrace
-busco \
-    -i "$fa_in" \
-    -o "$fileID" \
-    -l "$busco_db" \
-    -m "$assembly_type" \
-    -c "$n_threads" \
-    --force
-[[ "$debug" = false ]] && set +o xtrace
+    ## Run BUSCO
+    echo "## Now running BUSCO..."
+    [[ "$debug" = false ]] && set -o xtrace
+
+    ${e}Time busco \
+        -i "$infile" \
+        -o "$fileID" \
+        -l "$busco_db" \
+        -m "$assembly_type" \
+        -c "$threads" \
+        --force
+    
+    [[ "$debug" = false ]] && set +o xtrace
+
+fi
 
 
 # ==============================================================================
@@ -170,13 +260,11 @@ busco \
 echo
 echo "========================================================================="
 if [[ "$dryrun" = false ]]; then
-    echo "## BUSCO version used:"
-    Print_version | tee logs/version.txt
-    echo -e "\n## Listing files in the output dir:"
-    ls -lh "$outdir"
-    echo
-    sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+    echo "# Version used:"
+    Print_version | tee "$outdir"/logs/version.txt
+    echo -e "\n# Listing files in the output dir:"
+    ls -lhd "$PWD"/"$outdir"/*
+    [[ "$slurm" = true ]] && Resource_usage
 fi
-echo
-echo "## Done with script"
+echo "# Done with script"
 date
--- assembly_trans/rnaquast.sh
+++ assembly_trans/rnaquast.sh
@@ -2,102 +2,247 @@
 
 #SBATCH --account=PAS0471
 #SBATCH --time=12:00:00
-#SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=30G
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --job-name=rnaquast
 #SBATCH --output=slurm-rnaquast-%j.out
 
-
-# PARSE ARGUMENTS --------------------------------------------------------------
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
 ## Help function
-Help() {
-  echo
-  echo "$0: Run rnaQUAST to evaluate transcriptome assembly quality."
-  echo
-  echo "Syntax: $0 -i <input-FASTA> -o <output-dir> ..."
-  echo
-  echo "Required options:"
-  echo "-i STRING         One or more input FASTA files (transcriptome assemblies)"
-  echo "                     When providing more than 1 assembly, use a space-separated list"
-  echo "-o STRING         Output dir"
-  echo
-  echo "Other options:"
-  echo "-x STRING         Extension of assembly files          [default: fasta]"
-  echo "-a STRING         Other argument(s) to pass to CheckM"
-  echo "-h                Print this help message and exit"
-  echo
-  echo "Example: $0 -i results/assembly -o results/checkm -x fa"
-  echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
-  echo
-  echo "rnaQUAST documentation: https://github.com/ablab/rnaquast"
-  echo
+Print_help() {
+    echo
+    echo "======================================================================"
+    echo "                            $0"
+    echo "        Run rnaQUAST to evaluate transcriptome assembly quality"
+    echo "======================================================================"
+    echo
+    echo "USAGE:"
+    echo "  sbatch $0 -i <input file> -o <output dir> [...]"
+    echo "  bash $0 -h"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--assemblies <file/str>  One or more assembly FASTA files."
+    echo "                          When providing multiple assemblies, pass as a quoted string, separating filenames by spaces"
+    echo "  -o/--outdir     <dir>   Output dir (will be created if needed)"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --strandedness  <str>   Either 'stranded'/'reverse'/'forward' (all treated the same) or 'unstranded' [default: 'stranded']"
+    echo "  --more-args     <str>   Quoted string with additional argument(s) to pass to rnaQUAST"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  --dryrun                Dry run: don't execute commands, only parse arguments and report"
+    echo "  --debug                 Run the script in debug mode (print all code)"
+    echo "  -h                      Print this help message and exit"
+    echo "  --help                  Print the help for rnaQUAST and exit"
+    echo
+    echo "EXAMPLE COMMANDS:"
+    echo "  sbatch $0 -i 'results/assembly1.fasta results/assembly2.fasta' -o results/rnaquast"
+    echo
+    echo "SOFTWARE DOCUMENTATION:"
+    echo "  - Docs: https://github.com/ablab/rnaquast"
+    echo
 }
 
+## Load software
+Load_software() {
+    module load miniconda3/4.12.0-py39
+    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do source deactivate; done
+    source activate /fs/ess/PAS0471/jelmer/conda/rnaquast-2.2.1
+}
+
+## Print help for the focal program
+Print_help_program() {
+    Load_software
+    rnaQUAST.py --help
+}
+
+## Print SLURM job resource usage info
+Resource_usage() {
+    echo
+    ${e}sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime,MaxVMSize | \
+        grep -Ev "ba|ex"
+    echo
+}
+
+## Print SLURM job requested resources
+Print_resources() {
+    set +u
+    echo "# SLURM job information:"
+    echo "Account (project):    $SLURM_JOB_ACCOUNT"
+    echo "Job ID:               $SLURM_JOB_ID"
+    echo "Job name:             $SLURM_JOB_NAME"
+    echo "Memory (per node):    $SLURM_MEM_PER_NODE"
+    echo "CPUs per task:        $SLURM_CPUS_PER_TASK"
+    [[ "$SLURM_NTASKS" != 1 ]] && echo "Nr of tasks:          $SLURM_NTASKS"
+    [[ -n "$SBATCH_TIMELIMIT" ]] && echo "Time limit:           $SBATCH_TIMELIMIT"
+    echo "======================================================================"
+    echo
+    set -u
+}
+
+## Set the number of threads/CPUs
+Set_threads() {
+    set +u
+    if [[ "$slurm" = true ]]; then
+        if [[ -n "$SLURM_CPUS_PER_TASK" ]]; then
+            threads="$SLURM_CPUS_PER_TASK"
+        elif [[ -n "$SLURM_NTASKS" ]]; then
+            threads="$SLURM_NTASKS"
+        else 
+            echo "WARNING: Can't detect nr of threads, setting to 1"
+            threads=1
+        fi
+    else
+        threads=1
+    fi
+    set -u
+}
+
+## Resource usage information
+Time() {
+    /usr/bin/time -f \
+        '\n# Ran the command:\n%C \n\n# Run stats by /usr/bin/time:\nTime: %E   CPU: %P    Max mem: %M K    Exit status: %x \n' \
+        "$@"
+}   
+
+## Exit upon error with a message
+Die() {
+    error_message=${1}
+    error_args=${2-none}
+    
+    echo >&2
+    echo "=====================================================================" >&2
+    printf "$0: ERROR: %s\n" "$error_message" >&2
+    echo -e "\nFor help, run this script with the '-h' option" >&2
+    echo "For example, 'bash mcic-scripts/qc/fastqc.sh -h'" >&2
+    if [[ "$error_args" != "none" ]]; then
+        echo -e "\nArguments passed to the script:" >&2
+        echo "$error_args" >&2
+    fi
+    echo -e "\nEXITING..." >&2
+    echo "=====================================================================" >&2
+    echo >&2
+    exit 1
+}
+
+
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
 ## Option defaults
+strandedness=reverse
+
+debug=false
+dryrun=false && e=""
+slurm=true
+
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+## Placeholder defaults
 infiles=""
 outdir=""
 more_args=""
 
-## Parse command-line options
-while getopts ':i:o:a:h' flag; do
-  case "${flag}" in
-    i) infiles="$OPTARG" ;;
-    o) outdir="$OPTARG" ;;
-    a) more_args="$OPTARG" ;;
-    h) Help && exit 0 ;;
-    \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
-    :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
-  esac
+## Parse command-line args
+all_args="$*"
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --assemblies ) shift && infiles=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        --strandedness )    shift && strandedness=$1 ;;
+        --more-args )       shift && more_args=$1 ;;
+        -h )                Print_help; exit 0 ;;
+        --help )            Print_help_program; exit 0;;
+        --dryrun )          dryrun=true && e="echo ";;
+        --debug )           debug=true ;;
+        * )                 Die "Invalid option $1" "$all_args" ;;
+    esac
+    shift
 done
 
-## Other parameters
-n_cores=$SLURM_CPUS_PER_TASK
 
-## Check input
-[[ "$infiles" = "" ]] && echo "## ERROR: Please specify one or more input files with -i" >&2 && exit 1
-[[ "$outdir" = "" ]] && echo "## ERROR: Please specify an output dir with -o" >&2 && exit 1
+# ==============================================================================
+#                          OTHER SETUP
+# ==============================================================================
+## In debugging mode, print all commands
+[[ "$debug" = true ]] && set -o xtrace
 
+## Check if this is a SLURM job
+[[ -z "$SLURM_JOB_ID" ]] && slurm=false
 
-# SETUP ------------------------------------------------------------------------
-## Load software
-module load python/3.6-conda5.2
-source activate /users/PAS0471/jelmer/miniconda3/envs/rnaquast-env
+## Load software and set nr of threads
+[[ "$dryrun" = false ]] && Load_software
+Set_threads
 
-## Bash strict mode
+## Bash script settings
 set -euo pipefail
 
+## Library type
+if [[ "$strandedness" = "reverse" || "$strandedness" = "forward" || "$strandedness" = "stranded" ]]; then
+    strand_arg="--strand_specific"
+else
+    strand_arg=""
+fi
+
+## Check input
+[[ "$infiles" = "" ]] && Die "Please specify one or more assemblies -i/--assemblies" "$all_args"
+[[ "$outdir" = "" ]] && Die "Please specify an output dir with -o/--outdir" "$all_args"
+
 ## Report
-echo "## Starting script rnaquast.sh"
+echo
+echo "=========================================================================="
+echo "                    STARTING SCRIPT RNAQUAST.SH"
 date
-echo "## Input FASTA file(s):    $infiles"
-echo "## Output dir:             $outdir"
-echo "## Number of cores:        $n_cores"
-echo -e "------------------------\n"
+echo "=========================================================================="
+echo "All arguments to this script:     $all_args"
+echo "Input assembly FASTA file(s):     $infiles"
+echo "Output dir:                       $outdir"
+echo "Strandedness / strand argument:   $strandedness / $strand_arg"
+[[ $more_args != "" ]] && echo "Other arguments for rnaQUAST:     $more_args"
+echo "Number of threads/cores:          $threads"
+echo
+[[ $dryrun = true ]] && echo -e "\nTHIS IS A DRY-RUN"
+echo "=========================================================================="
 
-## Create output dir if needed
-mkdir -p "$outdir"
+## Print reserved resources
+[[ "$slurm" = true ]] && Print_resources
 
 
-# RUN RNAQUAST -----------------------------------------------------------------
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+## Create the output directory
+${e}mkdir -p "$outdir"/logs
+
 echo "## Now running rnaQUAST..."
-rnaQUAST.py \
+${e}Time rnaQUAST.py \
     --transcripts $infiles \
     --output_dir "$outdir" \
-    --threads "$n_cores" \
+    --threads "$threads" \
     --strand_specific \
     --gene_mark \
+    $strand_arg \
     $more_args
 
 #? Not running BUSCO with rnaQUAST because it failed
 
 
-# WRAP-UP ----------------------------------------------------------------------
-echo -e "\n-------------------------------"
-echo "## Listing files in the output dir:"
-ls -lh "$outdir"
-echo -e "\n## Done with script rnaquast.sh"
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
+echo
+echo "========================================================================="
+if [[ "$dryrun" = false ]]; then
+    echo -e "\n# Listing files in the output dir:"
+    ls -lhd "$PWD"/"$outdir"/*
+    [[ "$slurm" = true ]] && Resource_usage
+fi
+echo "# Done with script"
 date
-echo
-sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
-echo
