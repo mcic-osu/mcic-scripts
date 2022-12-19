03da44a6fa816b71cbbe55f3ed4ac75ae949184c
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-12-18 21:15:55 -0500

    Dont print conda deactivate warning

--- qc/multiqc.sh
+++ qc/multiqc.sh
@@ -55,7 +55,7 @@ Print_help() {
 ## Load software
 Load_software() {
     module load miniconda3/4.12.0-py39
-    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do source deactivate; done
+    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do source deactivate 2>/dev/null; done
     source activate /fs/project/PAS0471/jelmer/conda/multiqc
 }
 
