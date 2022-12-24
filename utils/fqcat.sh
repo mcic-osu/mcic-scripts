44e06959973edeeed29d628c3c6b6a81e2a86d3b
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2022-12-24 13:48:08 -0500

    No longer hardcode PASS subdir

--- utils/fqcat.sh
+++ utils/fqcat.sh
@@ -12,7 +12,7 @@
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
-## Help function
+# Help function
 Print_help() {
     echo
     echo "======================================================================"
@@ -28,6 +28,11 @@ Print_help() {
     echo "  -i/--indir     <dir>    Input file"
     echo "  -o/--outfile   <file>   Output file (its dir will be created if needed)"
     echo
+    echo "OTHER KEY OPTIONS"
+    echo "  --subdir       <dir>    Subdir which may or may not be one layer removed from the indir specified by -i/--indir"
+    echo "                          This can be useful if FASTQ files are inside sample-specific folders"
+    echo "  --extension    <str>    Input file extension                        [default: 'fastq.gz']"
+    echo
     echo "UTILITY OPTIONS:"
     echo "  --dryrun                Dry run: don't execute commands, only parse arguments and report"
     echo "  --debug                 Run the script in debug mode (print all code)"
@@ -35,13 +40,13 @@ Print_help() {
     echo
 }
 
-## Print SLURM job resource usage info
+# Print SLURM job resource usage info
 Resource_usage() {
     ${e}sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime,MaxVMSize | \
         grep -Ev "ba|ex"
 }
 
-## Print SLURM job requested resources
+# Print SLURM job requested resources
 Print_resources() {
     set +u
     echo "# SLURM job information:"
@@ -57,14 +62,14 @@ Print_resources() {
     set -u
 }
 
-## Recource usage information
+# Recource usage information
 Time() {
     /usr/bin/time -f \
         '\n# Ran the command:\n%C \n\n# Run stats by /usr/bin/time:\nTime: %E   CPU: %P    Max mem: %M K    Avg Mem: %t K    Exit status: %x \n' \
         "$@"
 }   
 
-## Exit upon error with a message
+# Exit upon error with a message
 Die() {
     error_message=${1}
     error_args=${2-none}
@@ -88,7 +93,9 @@ Die() {
 # ==============================================================================
 #                          CONSTANTS AND DEFAULTS
 # ==============================================================================
-## Option defaults
+# Option defaults
+extension="fastq.gz"
+
 debug=false
 dryrun=false && e=""
 slurm=true
@@ -97,17 +104,20 @@ slurm=true
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
-## Placeholder defaults
+# Placeholder defaults
 indir=""
 outfile=""
+subdir=""
 
-## Parse command-line args
+# Parse command-line args
 all_args="$*"
 
 while [ "$1" != "" ]; do
     case "$1" in
         -i | --indir )          shift && indir=$1 ;;
         -o | --outfile )        shift && outfile=$1 ;;
+        --subdir )              shift && subdir=$1 ;;
+        --extension )           shift && extension=$1 ;;
         -h )                    Print_help; exit 0;;
         --dryrun )              dryrun=true && e="echo ";;
         --debug )               debug=true ;;
@@ -120,24 +130,24 @@ done
 # ==============================================================================
 #                          OTHER SETUP
 # ==============================================================================
-## In debugging mode, print all commands
+# In debugging mode, print all commands
 [[ "$debug" = true ]] && set -o xtrace
 
-## Check if this is a SLURM job
+# Check if this is a SLURM job
 [[ -z "$SLURM_JOB_ID" ]] && slurm=false
 
-## Bash script settings
+# Bash script settings
 set -euo pipefail
 
-## Check input
+# Check input
 [[ $indir = "" ]] && Die "Please specify an input dir with -i" "$all_args"
 [[ $outfile = "" ]] && Die "Please specify an output file with -o" "$all_args"
-[[ ! -d $indir ]] && Die "Input file $indir does not exist"
+[[ ! -d $indir ]] && Die "Input dir $indir does not exist"
 
-## Determine outdir
+# Determine outdir
 outdir=$(dirname "$outfile")
 
-## Report
+# Report
 echo
 echo "=========================================================================="
 echo "                    STARTING SCRIPT CONCAT_FQ.SH"
@@ -145,26 +155,36 @@ date
 echo "=========================================================================="
 echo "All arguments to this script:     $all_args"
 echo "Input dir:                        $indir"
+echo "File extension:                   $extension"
+[[ $subdir != "" ]] && echo "Input subdir:                     $subdir"
 echo "Output file:                      $outfile"
 [[ $dryrun = true ]] && echo -e "\nTHIS IS A DRY-RUN"
 echo "=========================================================================="
 
-## Print reserved resources
+# Print reserved resources
 [[ "$slurm" = true ]] && Print_resources
 
 # ==============================================================================
 #                               RUN
 # ==============================================================================
-## Create the output directory
+# Create the output directory
 ${e}mkdir -p "$outdir"/logs
 
-## Run
-mapfile -t fq_files < <(find "$indir"/*/pass -name "*fastq.gz" | sort)
-echo -e "\n# Number of FASTQ files found: ${#fq_files[@]}"
+# Find the FASTQ files
+if [[ $subdir != "" ]]; then
+    mapfile -t fq_files < <(find "$indir"/*/"$subdir" -name "*$extension" | sort)
+else
+    mapfile -t fq_files < <(find "$indir" -name "*$extension" | sort)
+fi
 
+echo -e "\n# Number of FASTQ files found: ${#fq_files[@]}"
+[[ ${#fq_files[@]} -eq 0 ]] && Die "No FASTQ files were found..."
+
+# Concatenate the FASTQ files
 echo -e "\n# Now concatenating the FASTQ files..."
 ${e}Time cat "${fq_files[@]}" > "$outfile"
 
+# Count the number of sequences in the output file
 if [[ "$dryrun" = false ]]; then
     echo -e "\n# Now counting the number of sequences..."
     nseqs=$(zcat "$outfile" | awk '{l++}END{print l/4}')
