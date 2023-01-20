29ab2e2ac3c61b5533e7d41510d59ef3dd6e83eb
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-01-20 15:11:40 -0500

    Use outfile instead of outdir

--- nanopore/medaka.sh
+++ nanopore/medaka.sh
@@ -12,7 +12,7 @@
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
-## Help function
+# Help function
 Print_help() {
     echo
     echo "======================================================================"
@@ -21,21 +21,21 @@ Print_help() {
     echo "======================================================================"
     echo
     echo "USAGE:"
-    echo "  sbatch $0 -i <input reads> -r <input assembly> -o <output dir> -m <model> [...]"
+    echo "  sbatch $0 --reads <FASTQ> --assembly <FASTA> --model <Medaka-model> -o <output-file> [...]"
     echo "  bash $0 -h"
     echo
     echo "REQUIRED OPTIONS:"
-    echo "  -i/--reads      <file>  Input reads: FASTQ file (reads used for correction)"
-    echo "  -r/--assembly   <file>  Input assembly: FASTA file (to be corrected)"
-    echo "  -o/--outdir     <dir>   Output dir (will be created if needed)"
-    echo "  -m/--model      <str>   Medaka model, see the Medaka docs at https://github.com/nanoporetech/medaka#models"
+    echo "  -o/--outfile    <file>  Output assembly FASTA (dir will be created if needed)"
+    echo "  --reads         <file>  Input reads: FASTQ file (reads used for correction)"
+    echo "  --assembly      <file>  Input assembly: FASTA file (to be corrected)"
+    echo "  --model         <str>   Medaka model, see the Medaka docs at https://github.com/nanoporetech/medaka#models"
     echo "                          Get a full list of possible models by running:"
     echo "                              module load miniconda3/4.12.0-py39"
     echo "                              source activate /fs/ess/PAS0471/jelmer/conda/medaka-1.7.2"
     echo "                              medaka tools list_models"
     echo
     echo "OTHER KEY OPTIONS:"
-    echo "  --more_args  <str>      Quoted string with additional argument(s) to pass to Medaka"
+    echo "  --more_args     <str>   Quoted string with additional argument(s) to pass to Medaka"
     echo
     echo "UTILITY OPTIONS:"
     echo "  --dryrun                Dry run: don't execute commands, only parse arguments and report"
@@ -51,14 +51,14 @@ Print_help() {
     echo
 }
 
-## Load software
+# Load software
 Load_software() {
-    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do conda deactivate; done
+    [[ -n "$CONDA_SHLVL" ]] && for i in $(seq "${CONDA_SHLVL}"); do conda deactivate 2>/dev/null; done
     module load miniconda3/4.12.0-py39
     source activate /fs/ess/PAS0471/jelmer/conda/medaka-1.7.2
 }
 
-## Print version
+# Print version
 Print_version() {
     Load_software
     medaka_consensus -h &> medaka_help.txt
@@ -66,20 +66,19 @@ Print_version() {
     rm medaka_help.txt
 }
 
-## Print help for the focal program
+# Print help for the focal program
 Print_help_program() {
     Load_software
     medaka_consensus -h
 }
 
 
-## Print SLURM job resource usage info
+# Print SLURM job resource usage info
 Resource_usage() {
-    ${e}sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime,MaxVMSize | \
-        grep -Ev "ba|ex"
+    sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime | grep -Ev "ba|ex"
 }
 
-## Print SLURM job requested resources
+# Print SLURM job requested resources
 Print_resources() {
     set +u
     echo "# SLURM job information:"
@@ -95,7 +94,7 @@ Print_resources() {
     set -u
 }
 
-## Set the number of threads/CPUs
+# Set the number of threads/CPUs
 Set_threads() {
     set +u
     if [[ "$slurm" = true ]]; then
@@ -113,14 +112,14 @@ Set_threads() {
     set -u
 }
 
-## Resource usage information
+# Resource usage information
 Time() {
     /usr/bin/time -f \
         '\n# Ran the command:\n%C \n\n# Run stats by /usr/bin/time:\nTime: %E   CPU: %P    Max mem: %M K    Avg Mem: %t K    Exit status: %x \n' \
         "$@"
 }   
 
-## Exit upon error with a message
+# Exit upon error with a message
 Die() {
     error_message=${1}
     error_args=${2-none}
@@ -144,34 +143,36 @@ Die() {
 # ==============================================================================
 #                          CONSTANTS AND DEFAULTS
 # ==============================================================================
-## Option defaults
+# Option defaults
 debug=false
-dryrun=false && e=""
+dryrun=false
 slurm=true
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
-## Placeholder defaults
+# Placeholder defaults
+outfile=""
 reads=""
 assembly=""
+model=""
 more_args=""
 
-## Parse command-line args
+# Parse command-line args
 all_args="$*"
 
 while [ "$1" != "" ]; do
     case "$1" in
-        -i | --reads )          shift && reads=$1 ;;
-        -r | --assembly )       shift && assembly=$1 ;;
-        -o | --outdir )         shift && outdir=$1 ;;
-        -m | --model )          shift && model=$1 ;;
-        --more_args )           shift && more_args=$1 ;;
-        -h )                    Print_help; exit 0;;
-        --help )                Print_help_program; exit 0;;
-        --debug )               debug=true ;;
-        --dryrun )              dryrun=true ;;
-        * )                     Die "Invalid option $1" "$all_args" ;;
+        -o | --outfile )   shift && outfile=$1 ;;
+        --reads )          shift && reads=$1 ;;
+        --assembly )       shift && assembly=$1 ;;
+        --model )          shift && model=$1 ;;
+        --more_args )      shift && more_args=$1 ;;
+        -h )               Print_help; exit 0;;
+        --help )           Print_help_program; exit 0;;
+        --debug )          debug=true ;;
+        --dryrun )         dryrun=true ;;
+        * )                Die "Invalid option $1" "$all_args" ;;
     esac
     shift
 done
@@ -180,27 +181,31 @@ done
 # ==============================================================================
 #                          OTHER SETUP
 # ==============================================================================
-## In debugging mode, print all commands
+# In debugging mode, print all commands
 [[ "$debug" = true ]] && set -o xtrace
 
-## Check if this is a SLURM job
+# Check if this is a SLURM job
 [[ -z "$SLURM_JOB_ID" ]] && slurm=false
 
-## Load software and set nr of threads
+# Load software and set nr of threads
 [[ "$dryrun" = false ]] && Load_software
 Set_threads
 
-## Bash script settings
+# Bash script settings
 set -euo pipefail
 
-## Check input
-[[ $reads = "" ]] && Die "Please specify a file with input reads with -i" "$all_args"
-[[ $assembly = "" ]] && Die "Please specify an input assembly with -r" "$all_args"
-[[ $outdir = "" ]] && Die "Please specify an output dir with -o" "$all_args"
+# Check input
+[[ $reads = "" ]] && Die "Please specify a file with input reads with --reads" "$all_args"
+[[ $assembly = "" ]] && Die "Please specify an input assembly with --assembly" "$all_args"
+[[ $outfile = "" ]] && Die "Please specify an output file with -o" "$all_args"
+[[ $model = "" ]] && Die "Please specify a Medaka model with --model" "$all_args"
 [[ ! -f $reads ]] && Die "Input FASTQ file $reads does not exist"
 [[ ! -f $assembly ]] && Die "Input assembly file $assembly does not exist"
 
-## Report
+# Get output dir
+outdir=$(dirname "$outfile")
+
+# Report
 echo
 echo "=========================================================================="
 echo "               STARTING SCRIPT MEDAKA.SH"
@@ -208,15 +213,15 @@ date
 echo "=========================================================================="
 echo "Input reads (FASTQ) file:         $reads"
 echo "Input assembly (FASTA) file:      $assembly"
-echo "Output dir:                       $outdir"
+echo "Medaka model:                     $model"
+echo "Output assembly file:             $outfile"
 [[ $more_args != "" ]] && echo "Other arguments for Medaka:    $more_args"
-echo
-echo "Listing input files:"
+echo "# Listing the input files:"
 ls -lh "$reads" "$assembly" 
 [[ $dryrun = true ]] && echo -e "\nTHIS IS A DRY-RUN"
 echo "=========================================================================="
 
-## Print reserved resources
+# Print reserved resources
 [[ "$slurm" = true ]] && Print_resources
 
 
@@ -224,11 +229,12 @@ echo "==========================================================================
 #                               RUN
 # ==============================================================================
 if [[ "$dryrun" = false ]]; then
-    ## Create the output directory
-    mkdir -p "$outdir"/logs
+    # Create the output directory
+    echo -e "\n# Creating the output directories..."
+    mkdir -pv "$outdir"/logs
 
-    ## Run
-    echo -e "\n## Now running Medaka..."
+    # Run
+    echo -e "\n# Running Medaka..."
     Time medaka_consensus \
         -i "$reads" \
         -d "$assembly" \
@@ -236,6 +242,8 @@ if [[ "$dryrun" = false ]]; then
         -t "$threads" \
         -m "$model"
 
+    echo -e "\n# Renaming the output file"
+    mv -v "$outdir"/consensus.fasta "$outfile"
 fi
 
 # ==============================================================================
