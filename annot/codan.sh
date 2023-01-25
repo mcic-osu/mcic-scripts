fee22a3b4ef2b2ff0927dd659f9e3d72c70937ad
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-01-25 09:56:35 -0500

    More time and memory

--- annot/codan.sh
+++ annot/codan.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 #SBATCH --account=PAS0471
-#SBATCH --time=3:00:00
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=32G
+#SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=48G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=codan
