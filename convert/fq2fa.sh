195cec6c34a05fe1deb70d4c90c54724701da164
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-02-26 20:47:57 -0500

    Fix using posistional args

--- convert/fq2fa.sh
+++ convert/fq2fa.sh
@@ -5,30 +5,29 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --output=slurm-fq2fa-%j.out
 
-## Report
-echo "## Starting script fq2fa.sh"
+# Report
+echo "# Starting script fq2fa.sh"
 date
-echo
 
-## Software
+# Software
 module load python/3.6-conda5.2
 source activate /fs/ess/PAS0471/jelmer/conda/seqtk
 
-## Strict settings
+# Bash strict settings
 set -euo pipefail
 
-## Command-line args
+# Command-line args
 fq_in=$1
 fa_out=$2
 
-## Report
-echo "## Input FASTQ file:       $fq_in"
-echo "## Output FASTA file:      $fa_out"
+# Report
+echo "# Input FASTQ file:       $fq_in"
+echo "# Output FASTA file:      $fa_out"
 
-## Convert FASTQ to FASTA
+# Convert FASTQ to FASTA
 seqtk seq -a "$fq_in" > "$fa_out"
 
-## Report
-echo "## Done with script fq2fa.sh"
+# Report
+echo "# Done with script fq2fa.sh"
 date
 echo
