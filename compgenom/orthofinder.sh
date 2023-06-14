4552f621d498d3dc4442dbbd1ab739634ba2a048
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-06-14 11:42:32 -0400

    Defaults up top

--- compgenom/orthofinder.sh
+++ compgenom/orthofinder.sh
@@ -1,94 +1,251 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 #SBATCH --account=PAS0471
-#SBATCH --time=1:00:00
+#SBATCH --time=6:00:00
 #SBATCH --cpus-per-task=20
+#SBATCH --mem=80G
+#SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=orthofinder
 #SBATCH --output=slurm-orthofinder-%j.out
 
+# Run OrthoFinder to find orthologs between genomes
 
-# PARSE ARGUMENTS --------------------------------------------------------------
-## Help function
-Help() {
-  echo
-  echo "$0: Run OrthoFinder to find orthologs between genomes."
-  echo
-  echo "Syntax: $0 -i <input-dir> -o <output-dir> ..."
-  echo
-  echo "Required options:"
-  echo "    -i DIR            Input dir with protein FASTA files, one per genome"
-  echo "    -o DIR            Output dir"
-  echo "                      WARNING: If output dir exists, it will be emptied prior to running OrthoFinder"
-  echo
-  echo "Other options:"
-  echo "    -a STRING         Other argument(s) to pass to OrthoFinder"
-  echo "    -h                Print this help message and exit"
-  echo
-  echo "Example:              $0 -i data/genomes -o results/orthofinder"
-  echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
-  echo
-  echo "OrthoFinder documentation: https://github.com/davidemms/OrthoFinder and https://davidemms.github.io/"
-  echo "OrthoFinder paper: https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1832-y"
-  echo
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants
+readonly SCRIPT_NAME=orthofinder.sh
+readonly SCRIPT_VERSION="1.0"
+readonly SCRIPT_AUTHOR="Jelmer Poelstra"
+readonly SCRIPT_URL=https://github.com/mcic-osu/mcic-scripts
+readonly MODULE=miniconda3/4.12.0-py39
+readonly CONDA_ENV=/fs/project/PAS0471/jelmer/conda/orthofinder
+readonly TOOL_BINARY=orthofinder
+readonly TOOL_NAME=Orthofinder
+readonly TOOL_DOCS=https://github.com/davidemms/OrthoFinder
+readonly TOOL_PAPER=https://genomebiology.biomedcentral.com/articles/10.1186/s13059-019-1832-y
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+# Help function
+script_help() {
+    echo
+    echo "        $0 (v. $SCRIPT_VERSION): Run $TOOL_NAME"
+    echo "        =============================================="
+    echo "DESCRIPTION:"
+    echo "  Run OrthoFinder to find orthologs between genomes"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage:"
+    echo "      sbatch $0 -i TODO -o results/TODO" #TODO
+    echo "  - To run the script using a different OSC project than PAS0471:"
+    echo "      sbatch -A PAS0001 $0 [...]"
+    echo "  - To just print the help message for this script or for Orthofinder"
+    echo "      bash $0 -h"
+    echo "      bash $0 --help"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--indir      <dir>   Input dir with protein FASTA files, one per genome"
+    echo "  -o/--outdir     <dir>   Output dir (NOTE: This dir should not yet exist)"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --more_args     <str>   Quoted string with more argument(s) for $TOOL_NAME"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  -h                      Print this help message and exit"
+    echo "  --help                  Print the help for $TOOL_NAME and exit"
+    echo "  -v                      Print the version of this script and exit"
+    echo "  -v/--version            Print the version of $TOOL_NAME and exit"
+    echo
+    echo "TOOL DOCUMENTATION:"
+    echo "  - Docs: $TOOL_DOCS"
+    echo "  - Paper: $TOOL_PAPER"
+    echo
 }
 
-## Option defaults
-indir=""
-outdir=""
-more_args=""
+# Load software
+load_tool_conda() {
+    set +u
+    module load "$MODULE" # Load the OSC Conda module
+    # Deactivate any active Conda environments:
+    if [[ -n "$CONDA_SHLVL" ]]; then
+        for i in $(seq "${CONDA_SHLVL}"); do source deactivate 2>/dev/null; done
+    fi
+    source activate "$CONDA_ENV" # Activate the focal environment
+    set -u
+}
 
-## Parse command-line options
-while getopts ':i:o:a:h' flag; do
-  case "${flag}" in
-    i) indir="$OPTARG" ;;
-    o) outdir="$OPTARG" ;;
-    a) more_args="$OPTARG" ;;
-    h) Help && exit 0 ;;
-    \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
-    :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
-  esac
+# Exit upon error with a message
+die() {
+    local error_message=${1}
+    local error_args=${2-none}
+    log_time "$0: ERROR: $error_message" >&2
+    log_time "For help, run this script with the '-h' option" >&2
+    if [[ "$error_args" != "none" ]]; then
+        log_time "Arguments passed to the script:" >&2
+        echo "$error_args" >&2
+    fi
+    log_time "EXITING..." >&2
+    exit 1
+}
+
+# Log messages that include the time
+log_time() { echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]" ${1-""}; }
+
+# Print the script version
+script_version() {
+    echo "Run using $SCRIPT_NAME by $SCRIPT_AUTHOR, version $SCRIPT_VERSION ($SCRIPT_URL)"
+}
+
+# Print the tool's version
+tool_version() {
+    set +e
+    load_tool_conda
+    "$TOOL_BINARY" --help | sed -n 2p
+    set -e
+}
+
+# Print the tool's help
+tool_help() {
+    load_tool_conda
+    "$TOOL_BINARY" --help
+}
+
+# Print SLURM job resource usage info
+resource_usage() {
+    sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime | grep -Ev "ba|ex"
+}
+
+# Print SLURM job requested resources
+slurm_resources() {
+    set +u
+    log_time "SLURM job information:"
+    echo "Account (project):                        $SLURM_JOB_ACCOUNT"
+    echo "Job ID:                                   $SLURM_JOB_ID"
+    echo "Job name:                                 $SLURM_JOB_NAME"
+    echo "Memory (MB per node):                     $SLURM_MEM_PER_NODE"
+    echo "CPUs (per task):                          $SLURM_CPUS_PER_TASK"
+    echo "Time limit:                               $SLURM_TIMELIMIT"
+    echo -e "=================================================================\n"
+    set -u
+}
+
+# Set the number of threads/CPUs
+set_threads() {
+    set +u
+    if [[ "$is_slurm" == true ]]; then
+        if [[ -n "$SLURM_CPUS_PER_TASK" ]]; then
+            readonly threads="$SLURM_CPUS_PER_TASK"
+        elif [[ -n "$SLURM_NTASKS" ]]; then
+            readonly threads="$SLURM_NTASKS"
+        else 
+            log_time "WARNING: Can't detect nr of threads, setting to 1"
+            readonly threads=1
+        fi
+    else
+        readonly threads=1
+    fi
+    set -u
+}
+
+# Resource usage information for any process
+runstats() {
+    /usr/bin/time -f \
+        "\n# Ran the command: \n%C
+        \n# Run stats by /usr/bin/time:
+        Time: %E   CPU: %P    Max mem: %M K    Exit status: %x \n" \
+        "$@"
+}
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+indir=
+outdir=
+more_args=
+
+# Parse command-line args
+all_args="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --indir )      shift && readonly indir=$1 ;;
+        -o | --outdir )     shift && readonly outdir=$1 ;;
+        --more_args )       shift && readonly more_args=$1 ;;
+        -v )                script_version; exit 0 ;;
+        -h )                script_help; exit 0 ;;
+        --version )         tool_version; exit 0 ;;
+        --help )            tool_help; exit 0;;
+        * )                 die "Invalid option $1" "$all_args" ;;
+    esac
+    shift
 done
 
+# Check input
+[[ -z "$indir" ]] && die "No input file specified, do so with -i/--indir" "$all_args"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_args"
+[[ ! -d "$indir" ]] && die "Input dir $indir does not exist"
 
-# SETUP ------------------------------------------------------------------------
-## Check input
-[[ ! -d "$indir" ]] && echo "## ERROR: Input dir (-i) $indir does not exist" >&2 && exit 1
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Check if this is a SLURM job
+if [[ -z "$SLURM_JOB_ID" ]]; then is_slurm=false; else is_slurm=true; fi
 
-## Load software
-module load python/3.6-conda5.2
-source activate /fs/project/PAS0471/jelmer/conda/orthofinder-2.5.4
-
-## Bash strict mode
+# Strict bash settings
 set -euo pipefail
 
-## Remove output dir
-rm -r "$outdir"
+# Load software and set nr of threads
+load_tool_conda
+set_threads
 
-## Report
-echo
-echo "## Starting script orthofinder.sh"
-date
-echo
-echo "## Input dir:                    $indir"
-echo "## Output dir:                   $outdir"
-[[ $more_args != "" ]] && echo "## Other arguments for OrthoFinder:    $more_args"
-echo -e "--------------------\n"
+# ==============================================================================
+#              DEFINE OUTPUTS AND DERIVED INPUTS, BUILD ARGS
+# ==============================================================================
+# Define outputs based on script parameters
+readonly version_file="$outdir"/logs/version.txt
+readonly log_dir="$outdir"/logs
 
-# RUN --------------------------------------------------------------------------
-echo "## Starting Orthofinder run..."
-orthofinder \
+# ==============================================================================
+#                               REPORT
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All arguments to this script:             $all_args"
+echo "Input dir:                                $indir"
+echo "Output dir:                               $outdir"
+[[ -n $more_args ]] && echo "Other arguments for $TOOL_NAME:   $more_args"
+echo "Number of threads/cores:                  $threads"
+echo
+log_time "Listing the input file(s):"
+ls -lh "$indir"
+[[ "$is_slurm" = true ]] && slurm_resources
+
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+# Run the tool
+log_time "Running $TOOL_NAME..."
+runstats $TOOL_BINARY \
     -f "$indir" \
     -o "$outdir" \
-    -t "$SLURM_CPUS_PER_TASK" \
-    -a "$SLURM_CPUS_PER_TASK" $more_args
+    -t "$threads" \
+    -a "$threads" \
+    $more_args
 
+# Create the log directory
+log_time "Creating the log directory..."
+mkdir -pv "$log_dir"
 
-# WRAP-UP ----------------------------------------------------------------------
-echo -e "\n-------------------------------"
-echo "## Listing files in the output dir:"
-ls -lh "$outdir"
-echo -e "\n## Done with script orthofinder.sh"
-date
-echo
-sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
+printf "\n======================================================================"
+log_time "Versions used:"
+tool_version | tee "$version_file"
+script_version | tee -a "$version_file" 
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+[[ "$is_slurm" = true ]] && echo && resource_usage
+log_time "Done with script $SCRIPT_NAME"
 echo
