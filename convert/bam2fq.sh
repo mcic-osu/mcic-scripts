9bf31c008ac5a8e7ea4e42aba3e69bb228428b61
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-06-14 11:41:26 -0400

    Use samtools instead of bedtools

--- convert/bam2fq.sh
+++ convert/bam2fq.sh
@@ -1,49 +1,286 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 #SBATCH --account=PAS0471
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
+#SBATCH --job-name=bam2fq
 #SBATCH --output=slurm-bam2fq-%j.out
 
-## Report
-echo -e "\n## Starting script bam2fq.sh..."
-date
+# Convert a BAM file with paired-end reads into two FASTQ files using
+# 'samtools collate' followed by 'samtools fastq'
+
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants
+readonly SCRIPT_NAME=bam2fq.sh
+readonly SCRIPT_VERSION="1.0"
+readonly SCRIPT_AUTHOR="Jelmer Poelstra"
+readonly SCRIPT_URL=https://github.com/mcic-osu/mcic-scripts
+readonly MODULE=miniconda3/4.12.0-py39
+readonly CONDA_ENV=/fs/project/PAS0471/jelmer/conda/samtools
+readonly TOOL_BINARY=samtools
+readonly TOOL_NAME=samtools
+readonly TOOL_DOCS=http://www.htslib.org/doc/samtools-fasta.html
+readonly TOOL_PAPER=https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2723002/
+
+# Option defaults
+gzip_output=true
+fq_pair_suffix="_R"      # Will generate _R1.fastq.gz, _R2.fastq.gz
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+# Help function
+script_help() {
+    echo
+    echo "        $0 (v. $SCRIPT_VERSION): Run $TOOL_NAME"
+    echo "        =============================================="
+    echo "DESCRIPTION:"
+    echo "  Convert a BAM file with paired-end reads into two FASTQ files using"
+    echo "  'samtools collate' followed by 'samtools fastq'"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage:"
+    echo "      sbatch $0 -i results/sampleA.bam -o results/converted_fastq/"
+    echo "  - To run the script using a different OSC project than PAS0471:"
+    echo "      sbatch -A PAS0001 $0 [...]"
+    echo "  - To just print the help message for this script (-h) or for $TOOL_NAME (--help)"
+    echo "      bash $0 -h"
+    echo "      bash $0 --help"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--infile     <file>  Input file"
+    echo "  -o/--outdir     <dir>   Output dir (will be created if needed)"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --bam_suffix_rm <str>   Additional suffix in the BAM files to be removed for the output FASTQ file"
+    echo "  --fq_pair_suffix <str>  FASTQ pair suffix, e.g '_' will give '_1' and '_2' [default: '_R']"
+    echo "  --no_zip                Don't gzip the output FASTQ files           [default: gzip them]"
+    echo "  --more_args     <str>   Quoted string with more argument(s) for $TOOL_NAME fastq"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  -h                      Print this help message and exit"
+    echo "  --help                  Print the help for $TOOL_NAME and exit"
+    echo "  -v                      Print the version of this script and exit"
+    echo "  -v/--version            Print the version of $TOOL_NAME and exit"
+    echo
+    echo "OUTPUT:"
+    echo "  - Two FASTQ files: R1 and R2" 
+    echo
+    echo "TOOL DOCUMENTATION:"
+    echo "  - Docs: $TOOL_DOCS"
+    echo "  - Paper: $TOOL_PAPER"
+    echo
+}
+
+# Load software
+load_tool_conda() {
+    set +u
+    module load "$MODULE" # Load the OSC Conda module
+    # Deactivate any active Conda environments:
+    if [[ -n "$CONDA_SHLVL" ]]; then
+        for i in $(seq "${CONDA_SHLVL}"); do source deactivate 2>/dev/null; done
+    fi
+    source activate "$CONDA_ENV" # Activate the focal environment
+    set -u
+}
+
+# Exit upon error with a message
+die() {
+    local error_message=${1}
+    local error_args=${2-none}
+    log_time "$0: ERROR: $error_message" >&2
+    log_time "For help, run this script with the '-h' option" >&2
+    if [[ "$error_args" != "none" ]]; then
+        log_time "Arguments passed to the script:" >&2
+        echo "$error_args" >&2
+    fi
+    log_time "EXITING..." >&2
+    exit 1
+}
+
+# Log messages that include the time
+log_time() { echo -e "\n[$(date +'%Y-%m-%d %H:%M:%S')]" ${1-""}; }
+
+# Print the script version
+script_version() {
+    echo "Run using $SCRIPT_NAME by $SCRIPT_AUTHOR, version $SCRIPT_VERSION ($SCRIPT_URL)"
+}
+
+# Print the tool's version
+tool_version() {
+    set +e
+    load_tool_conda
+    $TOOL_BINARY --version
+    set -e
+}
+
+# Print the tool's help
+tool_help() {
+    load_tool_conda
+    $TOOL_BINARY --help
+}
+
+# Print SLURM job resource usage info
+resource_usage() {
+    sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%60,Elapsed,CPUTime | grep -Ev "ba|ex"
+}
+
+# Print SLURM job requested resources
+slurm_resources() {
+    set +u
+    log_time "SLURM job information:"
+    echo "Account (project):                        $SLURM_JOB_ACCOUNT"
+    echo "Job ID:                                   $SLURM_JOB_ID"
+    echo "Job name:                                 $SLURM_JOB_NAME"
+    echo "Memory (MB per node):                     $SLURM_MEM_PER_NODE"
+    echo "CPUs (per task):                          $SLURM_CPUS_PER_TASK"
+    echo "Time limit:                               $SLURM_TIMELIMIT"
+    echo -e "=================================================================\n"
+    set -u
+}
+
+# Set the number of threads/CPUs
+set_threads() {
+    set +u
+    if [[ "$is_slurm" == true ]]; then
+        if [[ -n "$SLURM_CPUS_PER_TASK" ]]; then
+            readonly threads="$SLURM_CPUS_PER_TASK"
+        elif [[ -n "$SLURM_NTASKS" ]]; then
+            readonly threads="$SLURM_NTASKS"
+        else 
+            log_time "WARNING: Can't detect nr of threads, setting to 1"
+            readonly threads=1
+        fi
+    else
+        readonly threads=1
+    fi
+    set -u
+}
+
+# Resource usage information for any process
+runstats() {
+    /usr/bin/time -f \
+        "\n# Ran the command: \n%C
+        \n# Run stats by /usr/bin/time:
+        Time: %E   CPU: %P    Max mem: %M K    Exit status: %x \n" \
+        "$@"
+}
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+infile=
+outdir=
+bam_suffix_rm=
+more_args=
+
+# Parse command-line args
+all_args="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --infile )     shift && readonly infile=$1 ;;
+        -o | --outdir )     shift && readonly outdir=$1 ;;
+        --no_zip )   gzip_output=false ;;
+        --bam_suffix_rm )   shift && bam_suffix_rm=$1 ;;
+        --fq_pair_suffix )  shift && fq_pair_suffix=$1 ;;
+        --more_args )       shift && readonly more_args=$1 ;;
+        -v )                script_version; exit 0 ;;
+        -h )                script_help; exit 0 ;;
+        --version )         tool_version; exit 0 ;;
+        --help )            tool_help; exit 0;;
+        * )                 die "Invalid option $1" "$all_args" ;;
+    esac
+    shift
+done
+
+# Check input
+[[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_args"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_args"
+[[ ! -f "$infile" ]] && die "Input file $infile does not exist"
+
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Check if this is a SLURM job
+if [[ -z "$SLURM_JOB_ID" ]]; then is_slurm=false; else is_slurm=true; fi
+
+# Strict bash settings
+set -euo pipefail
+
+# Load software and set nr of threads
+load_tool_conda
+set_threads
+
+# ==============================================================================
+#              DEFINE OUTPUTS AND DERIVED INPUTS, BUILD ARGS
+# ==============================================================================
+# Define outputs based on script parameters
+readonly version_file="$outdir"/logs/version.txt
+readonly log_dir="$outdir"/logs
+
+# Output files
+sampleID=$(basename "$infile" "$bam_suffix_rm".bam)
+fq_R1=$outdir/"$sampleID""$fq_pair_suffix"1.fastq
+fq_R2=$outdir/"$sampleID""$fq_pair_suffix"2.fastq
+
+# ==============================================================================
+#                               REPORT
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All arguments to this script:             $all_args"
+echo "Input file:                               $infile"
+echo "Output dir:                               $outdir"
+echo "Output R1 FASTQ file:                     $fq_R1"
+echo "Output R2 FASTQ file:                     $fq_R2"
+[[ -n $more_args ]] && echo "Other arguments for $TOOL_NAME:   $more_args"
+echo "Number of threads/cores:                  $threads"
 echo
+log_time "Listing the input file(s):"
+ls -lh "$infile"
+[[ "$is_slurm" = true ]] && slurm_resources
 
-## Process args
-bam=$1
-outdir=$2
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+# Create the output directories
+log_time "Creating the output directories..."
+mkdir -pv "$log_dir"
 
-## Load software
-module load python/3.6-conda5.2
-source activate /fs/project/PAS0471/jelmer/conda/bedtools-env
+# Run the tool
+log_time "Running $TOOL_NAME..."
+$TOOL_BINARY collate \
+    -@ "$threads" \
+    -u \
+    -O "$infile" |
+    $TOOL_BINARY fastq \
+        -1 "$fq_R1" -2 "$fq_R2" \
+        -0 /dev/null -s /dev/null \
+        -n \
+        $more_args
 
-## Output files
-mkdir -p "$outdir"
-sampleID=$(basename "$bam" .bam)
-fq_R1=$outdir/"$sampleID"_R1.fastq
-fq_R2=$outdir/"$sampleID"_R2.fastq
+#? http://www.htslib.org/doc/samtools-collate.html
+#? http://www.htslib.org/doc/samtools-fasta.html
 
-## Report
-echo "## Input BAM file:      $bam"
-echo "## Output R1 FASTQ:     $fq_R1"
-echo "## Output R2 FASTQ:     $fq_R2"
-echo
+# Gzip FASTQ files
+if [[ "$gzip_output" == true ]]; then
+    echo -e "\nGzipping the FASTQ files..."
+    gzip -vf "$fq_R1"
+    gzip -vf "$fq_R2"
+fi
 
-# MAIN -------------------------------------------------------------------------
-## Convert BAM to FASTQ
-bedtools bamtofastq -i "$bam" -fq "$fq_R1" -fq2 "$fq_R2"
-
-## Gzip FASTQ files
-gzip "$fq_R1"
-gzip "$fq_R2"
-
-
-# WRAP UP ----------------------------------------------------------------------
-## Report
-echo -e "\n## Output files:"
-ls -lh "$fq_R1".gz "$fq_R2".gz
-echo -e "\n## Done with script bam2fq.sh"
-date
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
+printf "\n======================================================================"
+log_time "Versions used:"
+tool_version | tee "$version_file"
+script_version | tee -a "$version_file" 
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+[[ "$is_slurm" = true ]] && echo && resource_usage
+log_time "Done with script $SCRIPT_NAME"
 echo
