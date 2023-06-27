88a7f89e63ed3e80ec89867c928b46541cfc0826
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-06-27 08:47:30 -0400

    Format update

--- map/samtools-view.sh
+++ map/samtools-view.sh
@@ -1,47 +1,182 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 #SBATCH --account=PAS0471
 #SBATCH --time=1:00:00
-#SBATCH --mem=10G
-#SBATCH --job-name=samtools-view
-#SBATCH --output=slurm-samtools-view-%j.out
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --mail-type=FAIL
+#SBATCH --job-name=samtools_view
+#SBATCH --output=slurm-samtools_view-%j.out
 
-## Report
-echo -e "\n## Starting script samtools-view.sh..."
-date
-echo
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants
+readonly DESCRIPTION="Run samtools view to filter a BAM file"
+readonly MODULE=miniconda3/4.12.0-py39
+readonly CONDA=/fs/ess/PAS0471/jelmer/conda/samtools
+readonly SCRIPT_VERSION="1.0"
+readonly SCRIPT_AUTHOR="Jelmer Poelstra"
+readonly SCRIPT_URL=https://github.com/mcic-osu/mcic-scripts
+readonly TOOL_BINARY="samtools view"
+readonly TOOL_NAME="Samtools"
+readonly TOOL_DOCS=https://www.htslib.org/doc/samtools.html
+readonly VERSION_COMMAND=
+readonly HELP_COMMAND=
 
-## Process args
-bam_in=$1
-outdir=$2
-samtools_args=$3
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+# Help function
+script_help() {
+    echo
+    echo "        $0 (v. $SCRIPT_VERSION): Run $TOOL_NAME"
+    echo "        =============================================="
+    echo "DESCRIPTION:"
+    echo "  $DESCRIPTION"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage (always submit your scripts to SLURM with 'sbatch'):"
+    echo "      sbatch $0 -i TODO -o results/TODO" #TODO
+    echo "  - To just print the help message for this script (-h) or for $TOOL_NAME (--help):"
+    echo "      bash $0 -h"
+    echo "      bash $0 --help"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--infile     <file>  Input BAM file"
+    echo "  -o/--outfile    <file>  Output BAM file (dir will be created if needed)"
+    echo "  --args          <str>   Samtools view arguments"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  -h                      Print this help message and exit"
+    echo "  --help                  Print the help for $TOOL_NAME and exit"
+    echo "  -v                      Print the version of this script and exit"
+    echo "  --version               Print the version of $TOOL_NAME and exit"
+    echo
+    echo "TOOL DOCUMENTATION:"
+    echo "  - Docs: $TOOL_DOCS"
+    echo
+}
 
-## Load software
-module load python/3.6-conda5.2
-source activate /fs/ess/PAS0471/jelmer/conda/samtools
+# Function to source the script with Bash functions
+source_function_script() {
+    local is_slurm=$1
 
-## Define output file
-bam_out="$outdir"/$(basename "$bam_in")
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$is_slurm" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script=$(realpath "$script_dir"/../dev/bash_functions.sh)
+    
+    if [[ ! -f "$function_script" ]]; then
+        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
+        git clone https://github.com/mcic-osu/mcic-scripts.git
+        function_script=mcic-scripts/dev/bash_functions.sh
+    fi
+    # shellcheck source=/dev/null
+    source "$function_script"
+}
 
-## Report
-echo "## Input BAM file:                $bam_in"
-echo "## Output BAM file:               $bam_out"
-echo "## Other samtools arguments:      $samtools_args"
-echo
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP I
+# ==============================================================================
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script $IS_SLURM
 
-## Make output dir
-mkdir -p "$outdir"
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+infile=
+outdir=
+args=
 
+# Parse command-line args
+all_args="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --infile )     shift && readonly infile=$1 ;;
+        -o | --outfile )    shift && readonly outfile=$1 ;;
+        --args )            shift && readonly args=$1 ;;
+        -v )                script_version; exit 0 ;;
+        -h )                script_help; exit 0 ;;
+        --version )         load_env "$MODULE" "$CONDA"
+                            tool_version "$VERSION_COMMAND" && exit 0 ;;
+        --help )            load_env "$MODULE" "$CONDA"
+                            tool_help "$HELP_COMMAND" && exit 0;;
+        * )                 die "Invalid option $1" "$all_args" ;;
+    esac
+    shift
+done
 
-# MAIN -------------------------------------------------------------------------
-echo "## Now running samtools..."
-samtools view "$bam_in" -b $samtools_args > "$bam_out"
+# Check arguments
+[[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_args"
+[[ -z "$outfile" ]] && die "No output file specified, do so with -o/--outfile" "$all_args"
+[[ ! -f "$infile" ]] && die "Input file $infile does not exist"
 
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP II
+# ==============================================================================
+# Strict bash settings
+set -euo pipefail
 
-# WRAP UP ----------------------------------------------------------------------
-## Report
-echo -e "\n## Output file:"
-ls -lh "$bam_out"
-echo -e "\n## Done with script samtools-view.sh"
-date
-echo
+# Logging files and dirs
+outdir=$(dirname "$outfile")
+
+readonly LOG_DIR="$outdir"/logs
+readonly VERSION_FILE="$LOG_DIR"/version.txt
+readonly CONDA_YML="$LOG_DIR"/conda_env.yml
+readonly ENV_FILE="$LOG_DIR"/env.txt
+mkdir -p "$LOG_DIR"
+
+# Load software and set nr of threads
+load_env "$MODULE" "$CONDA" "$CONDA_YML"
+set_threads "$IS_SLURM"
+
+# ==============================================================================
+#                               REPORT
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All arguments to this script:             $all_args"
+echo "Input file:                               $infile"
+echo "Output file:                              $outfile"
+echo "Samtools view arguments:                  $args"
+log_time "Listing the input file(s):"
+ls -lh "$infile"
+[[ "$IS_SLURM" = true ]] && slurm_resources
+
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+# Run the tool
+log_time "Running $TOOL_NAME..."
+runstats $TOOL_BINARY \
+    --threads "$threads" \
+    -b \
+    --write-index \
+    $args \
+    -o "$outfile" \
+    "$infile"
+
+# List the output
+log_time "Number of entries in the input file: $(samtools view -c "$infile")"
+log_time "Number of entries in the output file: $(samtools view -c "$outfile")"
+log_time "Listing the output file:"
+ls -lh "$outfile"
+
+# ==============================================================================
+#                               WRAP UP
+# ==============================================================================
+printf "\n======================================================================"
+log_time "Versions used:"
+tool_version "$VERSION_COMMAND" | tee "$VERSION_FILE"
+script_version "$SCRIPT_NAME" "$SCRIPT_AUTHOR" "$SCRIPT_VERSION" "$SCRIPT_URL" | tee -a "$VERSION_FILE" 
+env | sort > "$ENV_FILE"
+[[ "$IS_SLURM" = true ]] && resource_usage
+log_time "Done with script $SCRIPT_NAME\n"
