c83ea7b0fa116e41f0ec15d03045d4440b61b3e5
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-08-24 16:46:20 -0400

    Add root option

--- trees/ggtree.R
+++ trees/ggtree.R
@@ -36,9 +36,30 @@ parser$add_argument("-o", "--figure",
                     type = "character", required = FALSE, default = NULL,
                     help = "Output figure file in '.png' format
                             [default: same dir and name as tree file, but .png extension]")
+parser$add_argument("--root",
+                    type = "character", default = NULL,
+                    help = "ID of sample that should be the root of the tree: tree will be rerooted")
+parser$add_argument("--annot",
+                    type = "character", default = NULL,
+                    help = "Input annotation/metadata file")
+parser$add_argument("--layout",
+                    type = "character", default = "rectangular",
+                    help = "Tree layout")
+parser$add_argument("--color_column",
+                    type = "character", default = NULL,
+                    help = "Name of annotation file column to color tip labels by")
+parser$add_argument("--tiplab_column",
+                    type = "character", default = NULL,
+                    help = "Name of annotation file column to use as tip labels (instead of labels in the tree file)")
+
 args <- parser$parse_args()
 tree_file <- args$tree
 figure_file <- args$figure
+annot_file <- args$annot
+layout <- args$layout
+color_column <- args$color_column
+tiplab_column <- args$tiplab_column
+root <- args$root
 
 # Define the output file name, if needed
 if (is.null(figure_file)) {
@@ -50,32 +71,77 @@ if (is.null(figure_file)) {
 
 # Report
 message("\n# Starting script ggtree.R")
-message("# Input tree file:          ", tree_file)
-message("# Output figure file:       ", figure_file, "\n")
+message("# Input tree file:                         ", tree_file)
+message("# Output figure file:                      ", figure_file)
+if (!is.null(annot_file)) message("# Annotation/metadata file:                ", annot_file)
+if (!is.null(tiplab_column)) message("# Metadata column for tip labels:  ",      tiplab_column)
+if (!is.null(color_column)) message("# Metadata column for colors:              ", color_column)
+if (!is.null(root)) message("# ID of sample that should be the root:    ", root)
+message()
 
 
-# PLOT THE TREE ----------------------------------------------------------------
+# PLOT PREP --------------------------------------------------------------------
 # Read the tree file and prep the tree
 tree <- read.tree(tree_file)
 nseqs <- length(tree$tip.label)
-total_edge_len <- sum(tree$edge.length)
 
-#tree <- ape::root(tree, outgroup = "AG21-0056") #TODO - Add 'root' option
+# Read the annotation
+if (!is.null(annot_file)) {
+  annot <- read.delim(annot_file)
+  if (!is.null(tiplab_column)) {
+    annot$tiplab <- annot[[tiplab_column]]
+    tiplab_column <- "tiplab"
+  }
+  message("\n# Showing the first few lines of the annotation dataframe:")
+  print(head(annot))
+  cat("\n")
+
+  # Check that IDs for annotation & tree match
+  message("\n# Showing the tree's tip labels:")
+  print(tree$tip.label)
+
+  message("\n# Are all tip labels in the annotation df?")
+  print(all(tree$tip.label %in% annot[[1]]))
+}
+
+# Tiplab size
+if (nseqs < 50) LABEL_SIZE <- 2.5 else LABEL_SIZE <- 2
+
+# Add root option
+if (! is.null(root)) {
+  message("\n# Rerooting the tree, using ", root, " as the root...")
+  tree <- ape::root(tree, outgroup = root) 
+}
+
+
+# PLOT THE TREE ----------------------------------------------------------------
+# Base tree
+p <- ggtree(tree, layout = layout)
+
+# Add annotation dataframe if provided
+if (!is.null(annot_file)) p <- p %<+% annot
+
+# Tip labels
+if (!is.null(tiplab_column)) {
+  p <- p + geom_tiplab(aes_string(color = color_column, label = tiplab_column),
+                       align = TRUE, linesize = 0, size = LABEL_SIZE)
+} else {
+  p <- p + geom_tiplab(aes_string(color = color_column),
+                       align = TRUE, linesize = 0, size = LABEL_SIZE)
+}
 
 # Make the plot
-p <- ggtree(tree, layout = "rectangular") +
-        geom_tiplab(size = 2.5, color = "grey50") +
-        geom_treescale(x = 0, y = nseqs - 1, color = "grey50") +
-        geom_rootedge(rootedge = sum(tree$edge.length) / 50) +
-        theme(plot.margin = margin(0.2, 2, 0.2, 0.2, "cm")) +
-        coord_cartesian(clip = "off")
+p <- p +
+  geom_rootedge(rootedge = sum(tree$edge.length) / 50) +
+  theme(plot.margin = margin(0.2, 2, 0.2, 0.2, "cm"))
+if(layout == "rectangular") p <- p + coord_cartesian(clip = "off")
 
 # Save the plot to file
 ggsave(figure_file, p, width = 6, height = 6, dpi = "retina")
 
 
 # WRAP UP ----------------------------------------------------------------------
-message("# Listing the output file:")
+message("\n# Listing the output file:")
 system(paste("ls -lh", figure_file))
 message("\n# Done with script ggtree.R")
 Sys.time()
