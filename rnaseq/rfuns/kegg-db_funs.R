19eeb87460dff6525f3284c42b05707e932f9f87
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2023-12-12 12:14:40 -0500

    Rename functions ... needs finishing

--- rnaseq/rfuns/kegg-db_funs.R
+++ rnaseq/rfuns/kegg-db_funs.R
@@ -4,25 +4,8 @@ if (! "BiocManager" %in% installed.packages()) install.packages("BiocManager")
 if (! "KEGGREST" %in% installed.packages()) BiocManager::install("KEGGREST")
 
 # KEGG DATABASE FUNCTIONS ------------------------------------------------------
-# Function to get the description (technically: 'Name') of a KEGG pathway,
-# given its pathway ID ('ko' or 'map' IDs).
-# Needs tryCatch because some IDs fail (example of a failing pathway ID: "ko01130"),
-# and the function needs to keep running.
-# Use like so: `pw_descrip <- map_dfr(.x = kegg_ids, .f = kegg_descrip)`
-get_kegg_descrip <- function(kegg_pathway) {
-  message(kegg_pathway)
-  tryCatch( {
-    description <- keggGet(kegg_pathway)[[1]]$NAME
-    return(data.frame(kegg_pathway, description))
-  }, error = function(cond) {
-    message("keggGet failure")
-    return(NULL)
-  }
-  )
-}
-
 # Get the genes belonging to a certain KEGG pathway
-get_pw_genes <- function(pathway_id) {
+p2genes <- function(pathway_id) {
   print(pathway_id)
   
   pw <- keggGet(pathway_id)
@@ -35,7 +18,7 @@ get_pw_genes <- function(pathway_id) {
 }
 
 # Get the genes belonging to a certain KEGG pathway
-get_pw_kos <- function(pathway_id) {
+pw2kos <- function(pathway_id) {
   cat(pathway_id, "... ")
   
   pw <- keggGet(pathway_id)
@@ -59,9 +42,19 @@ get_pw_kos <- function(pathway_id) {
   return(ko_df)
 }
 
+# Get the KO numbers associated with a module from the KEGG database
+mod2kos <- function(idx, modules) {
+  module <- names(modules)[idx]
+  mod_vec <- keggGet(module)[[1]]$ORTHOLOGY
+  mod_df <- data.frame(KO_id = names(mod_vec), KO_descrip = mod_vec,
+                       module = module, module_descrip = modules[idx],
+                       row.names = NULL)
+  return(mod_df)
+}
+
 # Function to get a KEGG pathway (ko-ID) associated with a KEGG K-term
 # (Example K_term: "K13449")
-get_pathway <- function(K_term, outdir) {
+ko2pw <- function(K_term, outdir) {
   cat("K_term:", K_term, " ")
   
   tryCatch(
@@ -89,21 +82,27 @@ get_pathway <- function(K_term, outdir) {
   )
 }
 
-# Get the KO numbers associated with a module,
-# straight from the KEGG database
-get_kos_in_mod <- function(idx, modules) {
-  module <- names(modules)[idx]
-  mod_vec <- keggGet(module)[[1]]$ORTHOLOGY
-  mod_df <- data.frame(KO_id = names(mod_vec), KO_descrip = mod_vec,
-                       module = module, module_descrip = modules[idx],
-                       row.names = NULL)
-  return(mod_df)
-}
-
 # Get NCBI ID for a gene ID
 get_NCBI_id <- function(geneID) {
   geneID_NCBI <- entrez_search(db = "gene", term = geneID)$ids
   message(geneID, " - ", geneID_NCBI)
   if (is_empty(geneID_NCBI)) geneID_NCBI <- NA
   return(data.frame(geneID, geneID_NCBI))
+}
+
+# Function to get the description (technically: 'Name') of a KEGG pathway,
+# given its pathway ID ('ko' or 'map' IDs).
+# Needs tryCatch because some IDs fail (example of a failing pathway ID: "ko01130"),
+# and the function needs to keep running.
+# Use like so: `pw_descrip <- map_dfr(.x = kegg_ids, .f = kegg_descrip)`
+get_kegg_descrip <- function(kegg_pathway) {
+  message(kegg_pathway)
+  tryCatch( {
+    description <- keggGet(kegg_pathway)[[1]]$NAME
+    return(data.frame(kegg_pathway, description))
+  }, error = function(cond) {
+    message("keggGet failure")
+    return(NULL)
+  }
+  )
 }
