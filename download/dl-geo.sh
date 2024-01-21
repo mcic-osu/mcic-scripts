9e282cf3457d50b7ec08cbdd6c80bb94b1e294b5
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2024-01-20 20:18:22 -0500

    Add script to download GEO data

---
+++ download/dl-geo.sh
@@ -0,0 +1,209 @@
+#!/usr/bin/env bash
+#SBATCH --account=PAS0471
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --mail-type=FAIL
+#SBATCH --job-name=geofetch
+#SBATCH --output=slurm-geofetch-%j.out
+
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants - generic
+DESCRIPTION="Download GEO (Gene Expression Omnibus) data using geofetch"
+SCRIPT_VERSION="2024-01-20"
+SCRIPT_AUTHOR="Jelmer Poelstra"
+REPO_URL=https://github.com/mcic-osu/mcic-scripts
+FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions2.sh
+TOOL_BINARY=geofetch
+TOOL_NAME=geofetch
+TOOL_DOCS=https://geofetch.databio.org/en/latest
+VERSION_COMMAND="$TOOL_BINARY --version"
+
+# Defaults - generics
+env=conda                           # Use a 'conda' env or a Singularity 'container'
+conda_path=/fs/ess/PAS0471/jelmer/conda/geofetch
+container_path=
+container_url=
+dl_container=false
+container_dir="$HOME/containers"
+strict_bash=true
+version_only=false                 # When true, just print tool & script version info and exit
+
+# Defaults - tool parameters
+skip_fastq=false
+skip_counts=false
+skip_meta=false
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+script_help() {
+    echo -e "\n                          $0"
+    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
+    echo "        =============================================================="
+    echo "DESCRIPTION:"
+    echo "  $DESCRIPTION"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage:"
+    echo "      sbatch $0 -i GSE205498 -o data/geo"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/-a               <file>  GEO accession number (e.g. GSE205498)"
+    echo "  -o/--outdir         <dir>   Output dir (will be created if needed)"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --skip_fastq                Don't download raw data (FASTQ files)   [default: $skip_fastq]"
+    echo "  --skip_counts               Don't download processed data (counts)  [default: $skip_counts]"
+    echo "  --skip_meta                 Don't download meta data                [default: $skip_meta]"
+    echo "                                Note: only applies when not downloading counts; metadata is always downloaded with counts"
+    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  --env               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env]"
+    echo "                                (NOTE: If no default '--container_url' is listed below,"
+    echo "                                 you'll have to provide one in order to run the script with a container.)"
+    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
+    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
+    echo "                                A container will only be downloaded if an URL is provided with this option, or '--dl_container' is used"
+    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
+    echo "  --dl_container              Force a redownload of the container     [default: $dl_container]"
+    echo "  --no_strict                 Don't use strict Bash settings ('set -euo pipefail') -- can be useful for troubleshooting"
+    echo "  -h/--help                   Print this help message and exit"
+    echo "  -v                          Print the version of this script and exit"
+    echo "  --version                   Print the version of $TOOL_NAME and exit"
+    echo
+    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+}
+
+# Function to source the script with Bash functions
+source_function_script() {
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$IS_SLURM" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script=$(realpath "$script_dir"/../dev/"$(basename "$FUNCTION_SCRIPT_URL")")
+    # Download the function script if needed, then source it
+    if [[ ! -f "$function_script" ]]; then
+        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
+        function_script=$(basename "$FUNCTION_SCRIPT_URL")
+        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script"
+    fi
+    source "$function_script"
+}
+
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+accession=
+outdir=
+more_opts=
+
+# Parse command-line args
+all_opts="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | -a )           shift && accession=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        --skip_fastq )      skip_fastq=true ;;
+        --skip_counts )     skip_counts=true ;;
+        --skip_meta )     skip_meta=true ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env )             shift && env=$1 ;;
+        --no_strict )       strict_bash=false ;;
+        --dl_container )    dl_container=true ;;
+        --container_dir )   shift && container_dir=$1 ;;
+        --container_url )   shift && container_url=$1 && dl_container=true ;;
+        -h | --help )       script_help; exit 0 ;;
+        -v )                script_version; exit 0 ;;
+        --version )         version_only=true ;;
+        * )                 die "Invalid option $1" "$all_opts" ;;
+    esac
+    shift
+done
+
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Strict Bash settings
+[[ "$strict_bash" == true ]] && set -euo pipefail
+
+# Load software
+load_env "$conda_path" "$container_path" "$dl_container"
+[[ "$version_only" == true ]] && tool_version "$VERSION_COMMAND" && exit 0
+
+# Check options provided to the script
+[[ -z "$accession" ]] && die "No accession number specified, do so with -i/-a" "$all_opts"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
+
+# Define outputs based on script parameters
+LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
+[[ "$skip_fastq" == false ]] && mkdir -p "$outdir"/fastq
+[[ "$skip_counts" == false ]] && mkdir -p "$outdir"/counts
+[[ "$skip_meta" == false ]] && mkdir -p "$outdir"/meta
+
+# ==============================================================================
+#                         REPORT PARSED OPTIONS
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All options passed to this script:        $all_opts"
+echo "GEO accession:                            $accession"
+echo "Output dir:                               $outdir"
+echo "Skip downloading FASTQ data?              $skip_fastq"
+echo "Skip downloading count data?              $skip_counts"
+echo "Skip downloading meta data?               $skip_meta"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
+set_threads "$IS_SLURM"
+[[ "$IS_SLURM" == true ]] && slurm_resources
+
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+log_time "Running $TOOL_NAME..."
+
+if [[ "$skip_meta" == false && "$skip_counts" == true ]]; then
+    log_time "Downloading meta data..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY \
+        -i "$accession" \
+         --just-metadata \
+        --metadata-folder "$outdir"/meta \
+        $more_opts
+fi
+
+if [[ "$skip_counts" == false ]]; then
+    log_time "Downloading count & meta data..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY \
+        -i "$accession" \
+        --processed \
+        --geo-folder "$outdir"/counts \
+        --metadata-folder "$outdir"/meta \
+        $more_opts
+fi
+
+if [[ "$skip_fastq" == false ]]; then
+    #https://geofetch.databio.org/en/latest/install/
+    #echo "/repository/user/main/public/root = \"$DATA\"" > ${HOME}/.ncbi/user-settings.mkfg
+
+    log_time "Downloading FASTQ files..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY \
+        -i "$accession" \
+        --fq-folder "$outdir"/fastq \
+        $more_opts
+fi
+
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+final_reporting "$LOG_DIR"
