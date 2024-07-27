935ea815f9b823bf33ddee6a8c006eeded45bcb9
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2024-07-27 13:54:37 -0400

    Use new script template

--- ont/nanoplot.sh
+++ ont/nanoplot.sh
@@ -1,167 +1,190 @@
 #!/usr/bin/env bash
-
 #SBATCH --account=PAS0471
 #SBATCH --time=1:00:00
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=20G
+#SBATCH --mail-type=FAIL
 #SBATCH --job-name=nanoplot
 #SBATCH --output=slurm-nanoplot-%j.out
 
 # ==============================================================================
-#                                   FUNCTIONS
-# ==============================================================================
-## Help function
-Print_help() {
-    echo
-    echo "======================================================================"
-    echo "      $0: Run Nanoplot to QC ONT/PACBIO READS"
-    echo "======================================================================"
-    echo
-    echo "USAGE:"
-    echo "  sbatch $0 ..."
-    echo "  bash $0 -h"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "    -i/--infile FILE      Input file: sequencing summary from Albacore or Guppy"
-    echo "    -o/--outdir DIR       Output dir (will be created if needed)"
-    echo
-    echo "OTHER KEY OPTIONS:"
-    echo "    -a/--more_args \"QUOTED STRING\"   Other argument(s) to pass to Nanoplot"
-    echo
-    echo "UTILITY OPTIONS"
-    echo "    -x/--debug            Run the script in debug mode"
-    echo "    -h/--help             Print this help message and exit"
-    echo "    -v/--version          Print the version of Nanoplot and exit"
-    echo
-    echo "EXAMPLE COMMANDS:"
-    echo "    sbatch $0 -i results/guppy/sequencing_summary.txt -o results/nanoplot"
-    echo "    sbatch $0 -i results/guppy/sequencing_summary.txt -o results/nanoplot -a \"--minlength N\"" 
-    echo
-    echo "HARDCODED PARAMETERS:"
-    echo "    - This script assumes that the input file is a sequencing summary file,"
-    echo "      whereas Nanoplot can also be run on actual sequence files."
-    echo
-    echo "SOFTWARE DOCUMENTATION:"
-    echo "    - https://github.com/wdecoster/NanoPlot"
-    echo
-}
-
-## Load software
-Load_software() {
-    module load miniconda3/4.12.0-py39
-    source activate /fs/project/PAS0471/jelmer/conda/nanoplot
-}
-
-## Print version
-Print_version() {
-    Load_software
-    NanoPlot --version
-}
-
-## Exit upon error with a message
-Die() {
-    printf "\n$0: ERROR: %s\n" "$1" >&2
-    echo -e "Exiting\n" >&2
-    exit 1
-}
-
-# ==============================================================================
 #                          CONSTANTS AND DEFAULTS
 # ==============================================================================
-## Option defaults
-debug=false
-dryrun=false
+# Constants - generic
+DESCRIPTION="Run NanoPlot to QC ONT/PacBio reads using sequencing summary/FASTQ/BAM input"
+SCRIPT_VERSION="2024-06-29"
+SCRIPT_AUTHOR="Jelmer Poelstra"
+REPO_URL=https://github.com/mcic-osu/mcic-scripts
+FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions2.sh
+TOOL_BINARY=NanoPlot
+TOOL_NAME=NanoPlot
+TOOL_DOCS=https://github.com/wdecoster/NanoPlot
+VERSION_COMMAND="$TOOL_BINARY --version"
 
+# Defaults - generics
+env=container                      # Use a 'conda' env or a Singularity 'container'
+conda_path=/fs/ess/PAS0471/jelmer/conda/nanoplot
+container_path=
+container_url=oras://community.wave.seqera.io/library/nanoplot:1.42.0--72a9f293be813c68
+dl_container=false
+container_dir="$HOME/containers"
+version_only=false                 # When true, just print tool & script version info and exit
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+script_help() {
+    echo -e "\n                          $0"
+    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
+    echo "        =============================================================="
+    echo "DESCRIPTION:"
+    echo "  $DESCRIPTION"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage:"
+    echo "      sbatch $0 -i sequencing_summary.txt -o results/nanoplot"
+    echo "      sbatch $0 -i sampleA.fastq.gz -o results/nanoplot"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--infile         <file>  Input file: sequencing summary text file, FASTQ, or BAM"
+    echo "  -o/--outdir         <dir>   Output dir (will be created if needed)"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  --env               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env]"
+    echo "                                (NOTE: If no default '--container_url' is listed below,"
+    echo "                                 you'll have to provide one in order to run the script with a container.)"
+    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
+    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
+    echo "                                A container will only be downloaded if an URL is provided with this option, or '--dl_container' is used"
+    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
+    echo "  --dl_container              Force a redownload of the container     [default: $dl_container]"
+    echo "  -h/--help                   Print this help message and exit"
+    echo "  -v                          Print the version of this script and exit"
+    echo "  --version                   Print the version of $TOOL_NAME and exit"
+    echo
+    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+}
+
+# Function to source the script with Bash functions
+source_function_script() {
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$IS_SLURM" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script=$(realpath "$script_dir"/../dev/"$(basename "$FUNCTION_SCRIPT_URL")")
+    # Download the function script if needed, then source it
+    if [[ ! -f "$function_script" ]]; then
+        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
+        function_script=$(basename "$FUNCTION_SCRIPT_URL")
+        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script"
+    fi
+    source "$function_script"
+}
+
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
-## Placeholder defaults
-infile=""
-outdir=""
-more_args=""
+# Initiate variables
+infile=
+outdir=
+more_opts=
+threads=
 
-## Parse command-line args
+# Parse command-line args
+all_opts="$*"
 while [ "$1" != "" ]; do
     case "$1" in
-        -i | --infile )         shift && infile=$1 ;;
-        -o | --outdir )         shift && outdir=$1 ;;
-        -a | --more_args )      shift && more_args=$1 ;;
-        -X | --debug )          debug=true ;;
-        -N | --dryrun )         dryrun=false ;;
-        -v | --version )        Print_version && exit ;;
-        -h | --help )           Print_help && exit ;;
-        * )                     Die "Invalid option $1" ;;
+        -i | --infile )     shift && infile=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env )             shift && env=$1 ;;
+        --dl_container )    dl_container=true ;;
+        --container_dir )   shift && container_dir=$1 ;;
+        --container_url )   shift && container_url=$1 && dl_container=true ;;
+        -h | --help )       script_help; exit 0 ;;
+        -v )                script_version; exit 0 ;;
+        --version )         version_only=true ;;
+        * )                 die "Invalid option $1" "$all_opts" ;;
     esac
     shift
 done
 
-
 # ==============================================================================
-#                          OTHER SETUP
+#                          INFRASTRUCTURE SETUP
 # ==============================================================================
-[[ "$debug" = true ]] && set -o xtrace
-
-## Load software
-[[ "$dryrun" = false ]] && Load_software
-
-## Bash script settings
+# Strict Bash settings
 set -euo pipefail
 
-## Check input
-[[ $infile = "" ]] && Die "Please specify an input file with -i"
-[[ $outdir = "" ]] && Die "Please specify an output dir with -o"
-[[ ! -f $infile ]] && Die "Input dir $infile does not exist"
+# Load software
+load_env "$conda_path" "$container_path" "$dl_container"
+[[ "$version_only" == true ]] && tool_version "$VERSION_COMMAND" && exit 0
 
-## Report
-echo
+# Check options provided to the script
+[[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_opts"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
+[[ ! -f "$infile" ]] && die "Input file $infile does not exist"
+
+# Define outputs based on script parameters
+LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
+
+# Output prefix
+prefix=$(basename "${infile%%.*}")_
+
+# Input file type
+if [[ "$infile" =~ txt$ ]]; then
+    filetype=seqsum
+    infile_opt="--summary"
+elif [[ "$infile" =~ bam$ ]]; then
+    filetype=bam
+    infile_opt="--bam"
+elif [[ "$infile" =~ fa?s?t?q ]]; then
+    filetype=fastq
+    infile_opt="--fastq"
+else
+    die "Unknown file type for input file $infile"
+fi
+
+# ==============================================================================
+#                         REPORT PARSED OPTIONS
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
 echo "=========================================================================="
-echo "               STARTING SCRIPT NANOPLOT.SH"
-date
-echo "=========================================================================="
-echo "Input file:                  $infile"
-echo "Output dir:                  $outdir"
-[[ $more_args != "" ]] && echo "Other arguments for Nanoplot:    $more_args"
-echo "Listing input file:"
+echo "All options passed to this script:        $all_opts"
+echo "Input file:                               $infile"
+echo "Inferred input file type:                 $filetype" 
+echo "Output dir:                               $outdir"
+echo "Output prefix:                            $prefix"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
+log_time "Listing the input file(s):"
 ls -lh "$infile"
-echo "=========================================================================="
-echo
+set_threads "$IS_SLURM"
+[[ "$IS_SLURM" == true ]] && slurm_resources
 
 # ==============================================================================
 #                               RUN
 # ==============================================================================
-if [[ "$dryrun" = false ]]; then
-    ## Create the output directory
-    mkdir -p "$outdir"/logs
-fi
-
-echo -e "\n## Now running Nanoplot..."
-[[ "$debug" = false ]] && set -o xtrace
-
-NanoPlot \
-    --summary "$infile" \
+log_time "Running $TOOL_NAME..."
+runstats $CONTAINER_PREFIX $TOOL_BINARY \
+    --threads "$threads" \
+    $infile_opt "$infile" \
     --outdir "$outdir" \
+    --prefix "$prefix" \
     --tsv_stats \
     --info_in_report \
-    --threads "$SLURM_CPUS_PER_TASK" \
-    $more_args
+    $more_opts
 
-[[ "$debug" = false ]] && set +o xtrace
-
-
-# ==============================================================================
-#                               WRAP-UP
-# ==============================================================================
-echo
-echo "========================================================================="
-if [[ "$dryrun" = false ]]; then
-    echo -e "\n## Version used:"
-    Print_version | tee "$outdir"/logs/version.txt
-    echo -e "\n## Listing files in the output dir:"
-    ls -lh "$outdir"
-    echo
-    sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
-    echo
-fi
-echo "## Done with script"
-date
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+final_reporting "$LOG_DIR"
