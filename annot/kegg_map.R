17de1ed39f1f2ce0d1cb7196097104b7edf83f0e
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2024-11-09 16:13:44 -0500

    Assume GhostKOALA format as input

--- annot/kegg_map.R
+++ annot/kegg_map.R
@@ -1,20 +1,36 @@
 #!/usr/bin/env Rscript
-
 #SBATCH --account=PAS0471
 #SBATCH --output=slurm-kegg_map-%j.out
 #SBATCH --time=12:00:00
 
+# kegg_map.R by Jelmer Poelstra, last edited 2024-11-09
 # Script to create a KEGG 'map'/lookup
-# Input is a file that contains KO numbers in the first column
-# The script will look up KEGG modules and pathways that each KO number belongs to
+# - Input is a tab-separated file that contains KO numbers in the second column and has no column header line
+#   This is the format output by the GhostKOALA webserver (https://www.kegg.jp/ghostkoala)
+# - The script will look up KEGG modules and pathways that each KO number belongs to
 
 # Example command to run the script:
-#> ko_list=/fs/ess/PAS0471/jelmer/refdata/kegg/ko_list
-#> ko_map=/fs/ess/PAS0471/jelmer/refdata/kegg/ko_map.tsv
-#> ml R/4.2.1-gnu11.2 && sbatch mcic-scripts/annot/kegg_map.R "$ko_list" "$ko_map"
-
+#> input_ko_list=/fs/ess/PAS0471/jelmer/refdata/kegg/ko_list
+#> output_ko_map=/fs/ess/PAS0471/jelmer/refdata/kegg/ko_map.tsv
+#> module load R/4.4.0-gnu11.2
+#> sbatch mcic-scripts/annot/kegg_map.R "$input_ko_list" "$output_ko_map"
 
 # SET-UP -----------------------------------------------------------------------
+# Process command-line arguments
+args <- commandArgs(trailingOnly = TRUE)
+ko_file <- args[1]             # Input file with KO numbers in the 1st column, and gene IDs in the second
+kegg_map_file <- args[2]       # Output: KO-to-pathway+module mappings
+
+# Report
+message("\n# Starting script kegg_map.R")
+Sys.time()
+message("# Input KO file:               ", ko_file)
+message("# Output KEGG map:             ", kegg_map_file)
+message("==============================================\n")
+
+# Check input
+stopifnot(file.exists(ko_file))
+
 # Install/load packages
 if (!"pacman" %in% installed.packages()) install.packages("pacman")
 packages <- c(
@@ -24,30 +40,6 @@ packages <- c(
     "clusterProfiler"
 )
 pacman::p_load(char = packages)
-
-# Process command-line arguments
-args <- commandArgs(trailingOnly = TRUE)
-ko_file <- args[1]             # Input file with KO numbers
-kegg_map_file <- args[2]       # Output: KO-to-pathway+module mappings
-
-# Example arguments
-# ko_file <- "/fs/ess/PAS0471/jelmer/refdata/kegg/ko_list"
-# kegg_map_file <- "/fs/ess/PAS0471/jelmer/refdata/kegg/ko_map.tsv"
-
-# Create output dir if needed
-outdir <- dirname(kegg_map_file)
-dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
-
-# Report
-message("# Starting script kegg_map.R")
-Sys.time()
-message("# Input KO file:               ", ko_file)
-message("# Output KEGG map:             ", kegg_map_file)
-message("==============================================\n")
-
-# Check input
-stopifnot(file.exists(ko_file))
-
 
 # FUNCTIONS --------------------------------------------------------------------
 # Function to get a KEGG pathway (ko-ID) associated with a KEGG K-term
@@ -97,14 +89,23 @@ ko_info <- function(ko_id, outdir) {
 }
 
 # CREATE KEGG MAP --------------------------------------------------------------
+# Create output dir if needed
+outdir <- dirname(kegg_map_file)
+dir.create(outdir, showWarnings = FALSE, recursive = TRUE)
+
 # Read input files
-ko_terms <- read_tsv(ko_file, show_col_types = FALSE) %>% pull(1)
+message("\n# Now reading the input file...")
+ko_terms <- read_tsv(ko_file, show_col_types = FALSE, col_names = FALSE) |>
+    drop_na() |>
+    pull(2)
+message("\n# Number of ko_terms: ", length(ko_terms))
 
 # Get KEGG pathways associated with the different K-terms
 message("\n# Now creating the KEGG map...")
-kegg_map <- map_dfr(.x = ko_terms, .f = ko_info, outdir_terms)
+kegg_map <- map_dfr(.x = ko_terms, .f = ko_info, outdir)
 
 # Write files
+message("\n# Now writing the output file...")
 write_tsv(kegg_map, kegg_map_file)
 
 
