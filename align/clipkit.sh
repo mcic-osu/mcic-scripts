d6d49547328a21f7127033d670ff2702aa4af259
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2025-01-04 06:28:55 -0500

    Add script to trim MSAs with clipkit

---
+++ align/clipkit.sh
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+#SBATCH --account=PAS0471
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --mail-type=FAIL
+#SBATCH --job-name=clipkit
+#SBATCH --output=slurm-clipkit-%j.out
+
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants - generic
+DESCRIPTION="Trim an MSA (Multiple Sequence Alignment) prior to building a tree with ClipKIT"
+SCRIPT_VERSION="2025-01-04"
+SCRIPT_AUTHOR="Jelmer Poelstra"
+REPO_URL=https://github.com/mcic-osu/mcic-scripts
+FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions2.sh
+TOOL_BINARY=clipkit
+TOOL_NAME=ClipKIT
+TOOL_DOCS=https://jlsteenwyk.com/ClipKIT/
+VERSION_COMMAND="$TOOL_BINARY --version"
+
+# Defaults - generics
+env=container                      # Use a 'conda' env or a Singularity 'container'
+conda_path=/fs/ess/PAS0471/jelmer/conda/clipkit
+container_path=/fs/ess/PAS0471/containers/clipkit_2.3.0--696fa0f33ad2b776.sif
+container_url=oras://community.wave.seqera.io/library/clipkit:2.3.0--696fa0f33ad2b776
+container_dir="$HOME/containers"
+version_only=false                 # When true, just print tool & script version info and exit
+
+# Defaults - tool parameters
+mode=smart-gap
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+script_help() {
+    echo -e "\n                          $0"
+    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
+    echo "        =============================================================="
+    echo "DESCRIPTION:"
+    echo "  $DESCRIPTION"
+    echo
+    echo "USAGE / EXAMPLE COMMANDS:"
+    echo "  - Basic usage example:"
+    echo "      sbatch $0 -i results/my_alignment.fa -o results/clipkit"
+    echo
+    echo "REQUIRED OPTIONS:"
+    echo "  -i/--infile         <file>  Input file with a nucleotide or protein Multiple Sequence Alignment"
+    echo "                                Supported formats: fasta, clustal, maf, mauve, phylip, phylip-sequential,"
+    echo "                                phylip-relaxed, and stockholm"
+    echo "  -o/--outdir         <dir>   Output dir (will be created if needed)"
+    echo "                              The output file will have the same name as the input file"
+    echo
+    echo "OTHER KEY OPTIONS:"
+    echo "  --mode              <str>   ClipKIT running mode / algorithm        [default: $mode]"
+    echo "                                The default is also the ClipKIT default"
+    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
+    echo
+    echo "UTILITY OPTIONS:"
+    echo "  --env               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env]"
+    echo "                                (NOTE: If no default '--container_url' is listed below,"
+    echo "                                 you'll have to provide one in order to run the script with a container.)"
+    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
+    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
+    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
+    echo "  -h/--help                   Print this help message and exit"
+    echo "  -v                          Print the version of this script and exit"
+    echo "  --version                   Print the version of $TOOL_NAME and exit"
+    echo
+    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+}
+
+# Function to source the script with Bash functions
+source_function_script() {
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$IS_SLURM" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script_name="$(basename "$FUNCTION_SCRIPT_URL")"
+    function_script="$script_dir"/../dev/"$function_script_name"
+
+    # Download the function script if needed, then source it
+    if [[ -f "$function_script" ]]; then
+        source "$function_script"
+    elif [[ ! -f "$function_script_name" ]]; then
+        echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
+        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
+        source "$function_script_name"
+    else
+        source "$function_script_name"
+    fi
+}
+
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+infile=
+outdir=
+more_opts=
+threads=
+
+# Parse command-line args
+all_opts="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --infile )     shift && infile=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env )             shift && env=$1 ;;
+        --container_dir )   shift && container_dir=$1 ;;
+        --container_url )   shift && container_url=$1 ;;
+        -h | --help )       script_help; exit 0 ;;
+        -v )                script_version; exit 0 ;;
+        --version )         version_only=true ;;
+        * )                 die "Invalid option $1" "$all_opts" ;;
+    esac
+    shift
+done
+
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Strict Bash settings
+set -euo pipefail
+
+# Load software
+load_env "$conda_path" "$container_path"
+[[ "$version_only" == true ]] && tool_version "$VERSION_COMMAND" && exit 0
+
+# Check options provided to the script
+[[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_opts"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
+[[ ! -f "$infile" ]] && die "Input file $infile does not exist"
+[[ $(dirname "$infile") == "$outdir" ]] && die "Input dir and output dir cannot be the same ($outdir)"
+
+# Define outputs based on script parameters
+LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
+outfile="$outdir"/$(basename "$infile")
+
+# ==============================================================================
+#                         REPORT PARSED OPTIONS
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All options passed to this script:        $all_opts"
+echo "Input file:                               $infile"
+echo "Output file:                              $outfile"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
+log_time "Listing the input file(s):"
+ls -lh "$infile"
+set_threads "$IS_SLURM"
+[[ "$IS_SLURM" == true ]] && slurm_resources
+
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+log_time "Running $TOOL_NAME..."
+runstats $CONTAINER_PREFIX $TOOL_BINARY \
+    "$infile" \
+    --output "$outfile" \
+    --mode "$mode" \
+    $more_opts
+
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+final_reporting "$LOG_DIR"
