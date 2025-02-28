ee2f956b9ba3b6ba6e5054521b894c97a1034b5b
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2025-02-28 15:56:03 -0500

    Add option to input trained classifier & export results as TSV

--- metabar/qiime2_classify.sh
+++ metabar/qiime2_classify.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #SBATCH --account=PAS0471
 #SBATCH --time=12:00:00
-#SBATCH --cpus-per-task=25
-#SBATCH --mem=100G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=170G
 #SBATCH --mail-type=FAIL
 #SBATCH --job-name=qiime-classify
 #SBATCH --output=slurm-qiime-classify-%j.out
@@ -12,7 +12,7 @@
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Classify ASV sequences with Qiime2's feature-classifier classify-sklearn"
-SCRIPT_VERSION="2025-02-26"
+SCRIPT_VERSION="2025-02-27"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions2.sh
@@ -39,15 +39,20 @@ script_help() {
     echo "  $DESCRIPTION"
     echo
     echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  - Basic usage example:"
+    echo "  - Usage example -- include the training of the classifier:"
     echo "      sbatch $0 --query_fasta ASVf.fa --ref_fasta REF.fa --ref_tax TAX.tsv -o results/qiime --prefix eukaryome"
+    echo "  - Usage example -- with a pre-trained classifier:"
+    echo "      sbatch $0 --query_fasta ASVf.fa --classifier classifier.qza -o results/qiime --prefix eukaryome"
     echo
     echo "REQUIRED OPTIONS:"
     echo "  --query_fasta       <file>  Input query (ASV) FASTA file"
-    echo "  --ref_fasta         <file>  Input reference FASTA file"
-    echo "  --ref_tax           <file>  Input reference TSV taxonomy file"
     echo "  -o/--outdir         <dir>   Output dir (will be created if needed)"
     echo "  --prefix            <file>  Output file prefix"
+    echo
+    echo "REQUIRED MUTUALLY EXCLUSIVE OPTIONS:"
+    echo "  --classifier        <file>  Pre-trained classifier to use (don't specify --ref_fasta or --ref_tax)"
+    echo "  --ref_fasta         <file>  Input reference FASTA file (use with --ref_tax, don't specify --classifier)"
+    echo "  --ref_tax           <file>  Input reference TSV taxonomy file (use with --ref_fasta, don't specify --classifier)"
     echo
     echo "OTHER KEY OPTIONS:"
     echo "  --more_opts         <str>   Quoted string with additional options for"
@@ -103,6 +108,7 @@ source_function_script
 query_fasta=
 ref_fasta=
 ref_tax=
+classifier=
 prefix=
 outdir=
 more_opts=
@@ -116,6 +122,7 @@ while [ "$1" != "" ]; do
         --query_fasta )     shift && query_fasta=$1 ;;
         --ref_fasta )       shift && ref_fasta=$1 ;;
         --ref_tax )         shift && ref_tax=$1 ;;
+        --classifier )      shift && classifier=$1 ;;
         --prefix )          shift && prefix=$1 ;;
         -o | --outdir )     shift && outdir=$1 ;;
         --more_opts )       shift && more_opts=$1 ;;
@@ -142,13 +149,21 @@ load_env "$conda_path" "$container_path"
 
 # Check options provided to the script
 [[ -z "$query_fasta" ]] && die "No input query FASTA file specified, do so with --query_fasta" "$all_opts"
-[[ -z "$ref_fasta" ]] && die "No input reference FASTA file specified, do so with --ref_fasta" "$all_opts"
-[[ -z "$ref_tax" ]] && die "No input reference taxonomy file specified, do so with --ref_tax" "$all_opts"
 [[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
 [[ -z "$prefix" ]] && die "No output file prefix specified, do so with --prefix" "$all_opts"
 [[ ! -f "$query_fasta" ]] && die "Input query FASTA file $query_fasta does not exist"
-[[ ! -f "$ref_fasta" ]] && die "Input reference FASTA file $ref_fasta does not exist"
-[[ ! -f "$ref_tax" ]] && die "Input reference taxonomy file $ref_tax does not exist"
+
+if [[ -n "$classifier" ]]; then
+    [[ -n "$ref_fasta" || -n "$ref_tax" ]] && die "If classifier is provided, don't specify --ref_fasta or --ref_tax" "$all_opts"
+else
+    [[ -z "$ref_fasta" && -z "$ref_tax" ]] && die "No classifier, input reference FASTA, or taxonomy file specified" "$all_opts"
+fi
+[[ -z "$ref_fasta" && -n "$ref_tax" ]] && die "Reference taxonomy but no reference FASTA specified, do so with --ref_fasta" "$all_opts"
+[[ -z "$ref_tax" && -n "$ref_fasta" ]] && die "Reference FASTA but no reference taxonomy specified, do so with --ref_tax" "$all_opts"
+
+[[ -n "$ref_fasta" && ! -f "$ref_fasta" ]] && die "Input reference FASTA file $ref_fasta does not exist"
+[[ -n "$ref_tax" && ! -f "$ref_tax" ]] && die "Input reference taxonomy file $ref_tax does not exist"
+[[ -n "$classifier" && ! -f "$classifier" ]] && die "Input classifier file $classifier does not exist"
 
 # Define outputs based on script parameters
 LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
@@ -168,7 +183,10 @@ echo "Output dir:                               $outdir"
 echo "Output prefix:                            $prefix"
 [[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
 log_time "Listing the input file(s):"
-ls -lh "$query_fasta" "$ref_fasta" "$ref_tax"
+ls -lh "$query_fasta"
+[[ -n "$ref_fasta" ]] && ls -lh "$ref_fasta"
+[[ -n "$ref_tax" ]] && ls -lh "$ref_tax"
+[[ -n "$classifier" ]] && ls -lh "$classifier"
 set_threads "$IS_SLURM"
 [[ "$IS_SLURM" == true ]] && slurm_resources
 
@@ -182,26 +200,31 @@ runstats $CONTAINER_PREFIX $TOOL_BINARY tools import \
     --input-path "$query_fasta" \
     --output-path "$outdir"/"$prefix"_query-seq.qza
 
-# Importing the reference taxonomy
-log_time "Importing the reference taxonomy..."
-runstats $CONTAINER_PREFIX $TOOL_BINARY tools import \
-    --type 'FeatureData[Taxonomy]' \
-    --input-path "$ref_tax" \
-    --output-path "$outdir"/"$prefix"_ref-tax.qza
-
-# Importing the reference sequence
-log_time "Importing the reference sequence..."
-runstats $CONTAINER_PREFIX $TOOL_BINARY tools import \
-    --type 'FeatureData[Sequence]' \
-    --input-path "$ref_fasta" \
-    --output-path "$outdir"/"$prefix"_ref-seq.qza
-
 # Train the classifier
-log_time "Training the classifier..."
-runstats $CONTAINER_PREFIX $TOOL_BINARY feature-classifier fit-classifier-naive-bayes \
-    --i-reference-reads "$outdir"/"$prefix"_ref-seq.qza \
-    --i-reference-taxonomy "$outdir"/"$prefix"_ref-tax.qza \
-    --o-classifier "$outdir"/"$prefix"_classifier.qza
+if [[ -z "$classifier" ]]; then
+    # Import the reference taxonomy
+    log_time "Importing the reference taxonomy..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY tools import \
+        --type 'FeatureData[Taxonomy]' \
+        --input-path "$ref_tax" \
+        --output-path "$outdir"/"$prefix"_ref-tax.qza
+
+    # Import the reference sequences
+    log_time "Importing the reference sequence..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY tools import \
+        --type 'FeatureData[Sequence]' \
+        --input-path "$ref_fasta" \
+        --output-path "$outdir"/"$prefix"_ref-seq.qza
+
+    # Train the classifier
+    log_time "Training the classifier..."
+    runstats $CONTAINER_PREFIX $TOOL_BINARY feature-classifier fit-classifier-naive-bayes \
+        --i-reference-reads "$outdir"/"$prefix"_ref-seq.qza \
+        --i-reference-taxonomy "$outdir"/"$prefix"_ref-tax.qza \
+        --o-classifier "$outdir"/"$prefix"_classifier.qza
+else
+    log_time "Classifier provided, skipping training..."
+fi
 
 # Run the classifier
 log_time "Running the classifier..."
@@ -211,6 +234,12 @@ runstats $CONTAINER_PREFIX $TOOL_BINARY feature-classifier classify-sklearn \
     --o-classification "$outdir"/"$prefix"_taxonomy.qza \
     --p-n-jobs "$threads" \
     $more_opts
+
+
+log_time "Exporting the classification results to a TSV file..."
+runstats $CONTAINER_PREFIX $TOOL_BINARY tools export \
+    --input-path "$outdir"/"$prefix"_taxonomy.qza \
+    --output-path "$outdir"
 
 # ==============================================================================
 #                               WRAP-UP
