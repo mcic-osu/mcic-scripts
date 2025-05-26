b173cbfd28651fdb6fbd37bc08206447091efef7
Jelmer Poelstra <jelmerpoelstra@gmail.com>
2025-05-26 12:59:41 -0400

    Finally use a good routine for SnpEff usage w custom genomes

--- popgenom/snpeff-build.sh
+++ popgenom/snpeff-build.sh
@@ -1,38 +1,210 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#SBATCH --account=PAS0471
+#SBATCH --time=1:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=FAIL
+#SBATCH --job-name=snpeff-build
+#SBATCH --output=slurm-snpeff-build-%j.out
 
-# Building a SNPeff database
-# See https://pcingola.github.io/SnpEff/snpeff/build_db
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants - generic
+DESCRIPTION="Build a SnpEff database for a specific genome"
+SCRIPT_VERSION="2025-05-26"
+SCRIPT_AUTHOR="Jelmer Poelstra"
+REPO_URL=https://github.com/mcic-osu/mcic-scripts
+FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
+TOOL_BINARY=snpEff
+TOOL_NAME=snpEff
+TOOL_DOCS=https://pcingola.github.io/SnpEff/snpeff
+VERSION_COMMAND="$TOOL_BINARY -version"
 
-# Load the software
-conda_env_type=/fs/ess/PAS0471/jelmer/conda/snpeff
-module load miniconda3
-conda activate "$conda_env"
+# Defaults - generics
+env_type=conda
+conda_path=/fs/ess/PAS0471/jelmer/conda/snpeff
+container_dir="$HOME/containers"
+container_url=
+container_path=
 
-# Genome characteristics
-genome_id=GCF_002878395.1
-species_name="Capsicum annuum"
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+script_help() {
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+            =================================================
 
-# Derived variables
-data_dir="$conda_env"/share/snpeff-5.2-1/data
-config="$conda_env"/share/snpeff-5.2-1/snpEff.config
-species_name2=${species_name/ /_}                       # E.g. "Capsicum_annuum"
+DESCRIPTION:
+$DESCRIPTION
+    
+USAGE / EXAMPLE COMMANDS:
+  - Basic usage example:
+      sbatch $0 -i results/gatk/my.vcf -o results/snpeff \
+        --genome_id GCA_047496595.1 --species my_species \
+        --config results/snpeff/snpEff.config
+    
+REQUIRED OPTIONS:
+  --fna               <file>  Input genome assembly file in FASTA format
+  --gtf               <file>  Input genome annotation file in GTF (v. 2.2) format
+                                Don't use GFF3 files: if needed, first convert with
+                                mcic-scripts/convert/gff2gtf_agat.sh
+  --genome_id         <str>   Genome ID to use (use this ID again when running SnpEff)
+  --species           <str>   Species name with underscore, e.g. 'Triticum_aestivum'
+  --config            <file>  Path to a NEW SnpEff config file to create ('.config' extension)
+                                Use a dedicated dir for this, as the database
+                                files will be created in the same dir.
+                                (Use this again when running SnpEff.)
+    
+OTHER KEY OPTIONS:
+  --more_opts         <str>   Quoted string with one or more additional options
+                              for $TOOL_NAME
+    
+UTILITY OPTIONS:
+  --env_type          <str>   Use a Singularity container ('container')         [default: $env_type]
+                              or a Conda environment ('conda') 
+  --conda_path        <dir>   Full path to a Conda environment to use           [default: $conda_path]
+  --container_dir     <str>   Dir to download a container to                    [default: $container_dir]
+  --container_url     <str>   URL to download a container from                  [default (if any): $container_url]
+  --container_path    <file>  Local singularity image file (.sif) to use        [default (if any): $container_path]
+  -h/--help                   Print this help message
+  -v/--version                Print script and $TOOL_NAME versions
+    
+TOOL DOCUMENTATION:
+  $TOOL_DOCS
+"
+}
 
-# Test
-[[ ! -d "$data_dir" ]] && echo "ERROR: SnpEff data dir does not exist"
-[[ ! -f "$config" ]] && echo "ERROR: SnpEff config file does not exist"
+# Function to source the script with Bash functions
+source_function_script() {
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$IS_SLURM" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script=$(realpath "$script_dir"/../dev/"$(basename "$FUNCTION_SCRIPT_URL")")
+    # Download the function script if needed, then source it
+    if [[ ! -f "$function_script" ]]; then
+        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
+        function_script=$(basename "$FUNCTION_SCRIPT_URL")
+        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script"
+    fi
+    source "$function_script"
+}
 
-# Download the reference genome files
-sbatch mcic-scripts/download/dl-genomes.sh -A "$genome_id" -o data/ref
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script
 
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+version_only=false          # When true, just print tool & script version info and exit
+fna=
+gtf=
+genome_id=
+species=
+config=
+more_opts=
+
+# Parse command-line args
+all_opts="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        --fna )             shift && fna=$1 ;;
+        --gtf )             shift && gtf=$1 ;;
+        --genome_id )       shift && genome_id=$1 ;;
+        --species )         shift && species=$1 ;;
+        --config )          shift && config=$1 ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env_type )        shift && env_type=$1 ;;
+        --container_dir )   shift && container_dir=$1 ;;
+        --container_url )   shift && container_url=$1 ;;
+        -h | --help )       script_help; exit 0 ;;
+        -v | --version )    version_only=true ;;
+        * )                 die "Invalid option $1" "$all_opts" ;;
+    esac
+    shift
+done
+
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Strict Bash settings
+set -euo pipefail
+
+# Load software
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
+[[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
+
+# Check options provided to the script
+[[ -z "$fna" ]] && die "No input assembly file specified, do so with --fna" "$all_opts"
+[[ -z "$gtf" ]] && die "No input GTF file specified, do so with --gtf" "$all_opts"
+[[ -z "$species" ]] && die "No species name specified, do so with --species" "$all_opts"
+[[ -z "$config" ]] && die "No config file specified, do so with --config" "$all_opts"
+[[ -z "$genome_id" ]] && die "No genome ID specified, do so with --genome_id" "$all_opts"
+[[ ! -f "$fna" ]] && die "Input assembly file $fna does not exist"
+[[ ! -f "$gtf" ]] && die "Input GTF file $gtf does not exist"
+
+# Define outputs based on script parameters
+outdir=$(dirname "$config")
+data_dir="$outdir"/data/"$genome_id"
+LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
+
+# ==============================================================================
+#                         REPORT PARSED OPTIONS
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All options passed to this script:        $all_opts"
+echo "Input genome FASTA file:                  $fna"
+echo "Input genome GTF file:                    $gtf"
+echo "Genome ID:                                $genome_id"
+echo "Species:                                  $species"
+echo "Output config file:                       $config"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
+log_time "Listing the input file(s):"
+ls -lh "$fna" "$gtf"
+[[ "$IS_SLURM" == true ]] && slurm_resources
+
+# ==============================================================================
+#                               RUN
+# ==============================================================================
 # Copy the genome assembly and annotation into the SnpEff data dir
-mkdir -p "$data_dir"/"$genome_id"
-cp -v data/ref/"$genome_id".fna "$data_dir"/"$genome_id"/sequences.fa
-cp -v data/ref/"$genome_id".gtf "$data_dir"/"$genome_id"/genes.gtf
-cp -v data/ref/"$genome_id".faa "$data_dir"/"$genome_id"/protein.fa
+log_time "Copying genome assembly and annotation to the SnpEff data dir..."
+mkdir -p "$data_dir"
+cp -v "$fna" "$data_dir"/sequences.fa
+cp -v "$gtf" "$data_dir"/genes.gtf
 
 # Edit the snpEff.config file
-echo -e "\n# $species_name genome, version $genome_id" >> "$config"
-echo "${genome_id}.genome : $species_name2" >> "$config"
+log_time "Creating SnpEff config file..."
+echo -e "\ndata.dir = ./data/" > "$config"
+echo -e "\n# $species genome, version $genome_id" >> "$config"
+echo -e "${genome_id}.genome : $species\n" >> "$config"
+log_time "Printing the contents of their SnpEff config file:"
+cat -n "$config"
 
-# Build the SnpEff database
-sbatch -A PAS0471 -c 4 --wrap="snpEff build -gtf22 -verbose $genome_id"
+# Run SnpEff build
+log_time "Running $TOOL_NAME..."
+runstats $TOOL_BINARY build \
+    -gtf22 \
+    -verbose \
+    -noCheckCds \
+    -noCheckProtein \
+    -nodownload \
+    -config $config \
+    $genome_id
+
+#? Protein checks seem to usually fail, so skip them
+#? See also https://pcingola.github.io/SnpEff/snpeff/build_db
+
+# Report
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+final_reporting "$LOG_DIR"
--- popgenom/snpeff.sh
+++ popgenom/snpeff.sh
@@ -12,7 +12,7 @@
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Run SnpEff to annotate variants with predictions of their functional effects"
-SCRIPT_VERSION="2024-09-21"
+SCRIPT_VERSION="2025-05-25"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
@@ -24,47 +24,56 @@ VERSION_COMMAND="$TOOL_BINARY -version"
 # Defaults - generics
 env_type=conda                           # Use a 'conda' env or a Singularity 'container'
 conda_path=/fs/ess/PAS0471/jelmer/conda/snpeff
-container_path=
-container_url=
-dl_container=false
 container_dir="$HOME/containers"
-version_only=false                 # When true, just print tool & script version info and exit
+container_url=
+container_path=
 
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
 script_help() {
-    echo -e "\n                          $0"
-    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
-    echo "        =============================================================="
-    echo "DESCRIPTION:"
-    echo "  $DESCRIPTION"
-    echo
-    echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  - Basic usage:"
-    echo "      sbatch $0 -i results/gatk/my.vcf -o results/snpeff"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "  -i/--infile         <file>  Input VCF file"
-    echo "  -o/--outdir         <dir>   Output dir (will be created if needed)"
-    echo
-    echo "OTHER KEY OPTIONS:"
-    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
-    echo
-    echo "UTILITY OPTIONS:"
-    echo "  --env_type               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env_type]"
-    echo "                                (NOTE: If no default '--container_url' is listed below,"
-    echo "                                 you'll have to provide one in order to run the script with a container.)"
-    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
-    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
-    echo "                                A container will only be downloaded if an URL is provided with this option, or '--dl_container' is used"
-    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
-    echo "  --dl_container              Force a redownload of the container     [default: $dl_container]"
-    echo "  -h/--help                   Print this help message and exit"
-    echo "  -v                          Print the version of this script and exit"
-    echo "  --version                   Print the version of $TOOL_NAME and exit"
-    echo
-    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+            =================================================
+
+DESCRIPTION:
+$DESCRIPTION
+    
+USAGE / EXAMPLE COMMANDS:
+  - Basic usage example:
+      sbatch $0 -i results/gatk/my.vcf -o results/snpeff \
+        --genome_id GCA_047496595.1 --config results/snpeff/snpEff.config
+    
+REQUIRED OPTIONS:
+  -i/--infile         <file>  Input VCF file
+  --genome_id         <str>   Genome ID: this must be present in the SnpEff database
+                                - Either first add a new genome to the database with:
+                                  mcic-scripts/popgenom/snpeff-build.sh
+                                - Or use a genome ID already present in the database
+                                  Check with command: 'snpEff databases'
+  -o/--outdir         <dir>   Output dir (will be created if needed)
+    
+OTHER KEY OPTIONS:
+  --config            <file>  Path to a SnpEff config file to use.
+                              Use this with a custom/self-built SnpEff database
+                              for the genome in question.
+  --more_opts         <str>   Quoted string with one or more additional options
+                              for $TOOL_NAME
+    
+UTILITY OPTIONS:
+  --env_type          <str>   Use a Singularity container ('container')         [default: $env_type]
+                              or a Conda environment ('conda') 
+  --conda_path        <dir>   Full path to a Conda environment to use           [default: $conda_path]
+  --container_dir     <str>   Dir to download a container to                    [default: $container_dir]
+  --container_url     <str>   URL to download a container from                  [default (if any): $container_url]
+  --container_path    <file>  Local singularity image file (.sif) to use        [default (if any): $container_path]
+  -h/--help                   Print this help message
+  -v/--version                Print script and $TOOL_NAME versions
+    
+TOOL DOCUMENTATION:
+  $TOOL_DOCS
+"
 }
 
 # Function to source the script with Bash functions
@@ -96,8 +105,11 @@ source_function_script
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
 # Initiate variables
+version_only=false          # When true, just print tool & script version info and exit
 infile=
 outdir=
+genome_id=
+config= && config_opt=
 more_opts=
 
 # Parse command-line args
@@ -106,13 +118,14 @@ while [ "$1" != "" ]; do
     case "$1" in
         -i | --infile )     shift && infile=$1 ;;
         -o | --outdir )     shift && outdir=$1 ;;
+        --genome_id )       shift && genome_id=$1 ;;
+        --config )          shift && config=$1 ;;
         --more_opts )       shift && more_opts=$1 ;;
-        --env_type )             shift && env_type=$1 ;;
-        --dl_container )    dl_container=true ;;
+        --env_type )        shift && env_type=$1 ;;
         --container_dir )   shift && container_dir=$1 ;;
-        --container_url )   shift && container_url=$1 && dl_container=true ;;
+        --container_url )   shift && container_url=$1 ;;
         -h | --help )       script_help; exit 0 ;;
-        -v | --version )         version_only=true ;;
+        -v | --version )    version_only=true ;;
         * )                 die "Invalid option $1" "$all_opts" ;;
     esac
     shift
@@ -125,18 +138,19 @@ done
 set -euo pipefail
 
 # Load software
-load_env "$conda_path" "$container_path" "$dl_container"
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
 [[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
 
 # Check options provided to the script
 [[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_opts"
 [[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
+[[ -z "$genome_id" ]] && die "No genome ID specified, do so with --genome_id" "$all_opts"
 [[ ! -f "$infile" ]] && die "Input file $infile does not exist"
 
 # Define outputs based on script parameters
 LOG_DIR="$outdir"/logs && mkdir -p "$LOG_DIR"
 outfile="$outdir"/$(basename "$infile")
-genome_id=$(basename "$infile" .vcf)
+[[ -n "$config" ]] && config_opt="-config $config"
 
 # ==============================================================================
 #                         REPORT PARSED OPTIONS
@@ -144,8 +158,10 @@ genome_id=$(basename "$infile" .vcf)
 log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
 echo "=========================================================================="
 echo "All options passed to this script:        $all_opts"
-echo "Input file:                               $infile"
+echo "Input VCF file:                           $infile"
+echo "Genome ID:                                $genome_id"
 echo "Output dir:                               $outdir"
+[[ -n $config ]] && echo "SnpEff config file:                       $config"
 [[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
 log_time "Listing the input file(s):"
 ls -lh "$infile" 
@@ -156,16 +172,19 @@ ls -lh "$infile"
 # ==============================================================================
 log_time "Running $TOOL_NAME..."
 runstats $TOOL_BINARY ann \
+    -verbose \
     -o vcf \
+    -htmlStats "$outdir"/snpEff_summary.html \
+    -csvStats "$outdir"/snpEff_stats.csv \
+    $config_opt \
     $more_opts \
     "$genome_id" \
     "$infile" \
-    >"$outfile"
+    > "$outfile"
 
 log_time "Listing files in the output dir:"
 ls -lhd "$(realpath "$outdir")"/*
 final_reporting "$LOG_DIR"
-
 
 # ==============================================================================
 #                               INFO
