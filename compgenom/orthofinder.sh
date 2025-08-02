4e64b6f0b6b014d7fb3206c193a7f48474d6c916
Jelmer Poelstra <poelstra.1@osu.edu>
2025-08-02 18:33:11 -0400

    Update to most recent template

--- compgenom/orthofinder.sh
+++ compgenom/orthofinder.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #SBATCH --account=PAS0471
-#SBATCH --time=6:00:00
+#SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=12
 #SBATCH --mem=48G
 #SBATCH --mail-type=END,FAIL
@@ -12,7 +12,7 @@
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Run OrthoFinder to find orthologs between genomes or proteomes"
-SCRIPT_VERSION="2023-10-19"
+SCRIPT_VERSION="2025-08-02"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
@@ -22,59 +22,61 @@ TOOL_DOCS=https://github.com/davidemms/OrthoFinder
 VERSION_COMMAND="$TOOL_BINARY --help | head -n2"
 
 # Defaults - generics
-env_type=conda                           # Use a 'conda' env or a Singularity 'container'
-conda_path=/fs/project/PAS0471/jelmer/conda/orthofinder
-container_path=
-container_url=
-dl_container=false
+env_type=container                  # Use a 'conda' env or a Singularity 'container'
+conda_path=
+container_url=oras://community.wave.seqera.io/library/orthofinder:3.1.0--888d04d0c725fcc8
 container_dir="$HOME/containers"
-strict_bash=true
-version_only=false                 # When true, just print tool & script version info and exit
+container_path=
 
 # Defaults - tool parameters
 tree_method=msa                    # This is NOT the Orthofinder default, which is 'dendroblast'
-fa_type=nuc                         
+fa_type=prot
 
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
 script_help() {
-    echo -e "\n                          $0"
-    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
-    echo "        =============================================================="
-    echo "DESCRIPTION:"
-    echo "  $DESCRIPTION"
-    echo
-    echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  - Basic usage:"
-    echo "      sbatch $0 -i data/proteomes/ -o results/orthofinder"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "  -i/--indir          <dir>   Input dir with genomes (nucleotide) or proteomes (protein FASTA files), one per genome"
-    echo "                                Accepted file extensions: .fa, .faa, .fasta, .fas, .pep"
-    echo "                                (Files with different extensions will be ignored.)"
-    echo "  -o/--outdir         <dir>   Output dir (NOTE: If this dir exists, it will be removed!)"
-    echo
-    echo "OTHER KEY OPTIONS:"
-    echo "  --tree_method       <str>   Gene tree inference method, 'dendroblast' or 'msa'              [default: $tree_method]"
-    echo "  --nuc                       Input files are nucleotide FASTA files (genomes), not proteomes [default: proteomes]"
-    echo "  --opts              <str>   Quoted string with additional options for $TOOL_NAME"
-    echo
-    echo "UTILITY OPTIONS:"
-    echo "  --env_type               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env_type]"
-    echo "                                (NOTE: If no default '--container_url' is listed below,"
-    echo "                                 you'll have to provide one in order to run the script with a container.)"
-    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
-    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
-    echo "                                A container will only be downloaded if an URL is provided with this option, or '--dl_container' is used"
-    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
-    echo "  --dl_container              Force a redownload of the container     [default: $dl_container]"
-    echo "  --no_strict                 Don't use strict Bash settings ('set -euo pipefail') -- can be useful for troubleshooting"
-    echo "  -h/--help                   Print this help message and exit"
-    echo "  -v                          Print the version of this script and exit"
-    echo "  --version                   Print the version of $TOOL_NAME and exit"
-    echo
-    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+            =================================================
+
+DESCRIPTION:
+$DESCRIPTION
+    
+USAGE / EXAMPLE COMMANDS:
+  - Basic usage example:
+      sbatch $0 -i TODO -o results/TODO
+    
+REQUIRED OPTIONS:
+  -i/--indir          <dir>   Input dir with genomes (nucleotide) or proteomes
+                              (protein FASTA files), one file per genome
+                              Accepted file extensions:
+                              .fa, .faa, .fasta, .fas, .pep
+                              (Files with different extensions will be ignored.)
+  -o/--outdir         <dir>   Output dir
+                              (NOTE: If this dir exists, it will be removed!)
+    
+OTHER KEY OPTIONS:
+  --tree_method       <str>   Gene tree inference method:'dendroblast' or 'msa' [default: $tree_method]
+  --nuc                       Use this option to indicate that input files are  [default: proteomes]
+                              nucleotide FASTA files (genomes), not proteomes 
+  --more_opts         <str>   Quoted string with one or more additional options
+                              for $TOOL_NAME
+    
+UTILITY OPTIONS:
+  --env_type          <str>   Whether to use a Singularity/Apptainer container  [default: $env_type]
+                              ('container') or a Conda environment ('conda') 
+  --container_url     <str>   URL to download a container from                  [default (if any): $container_url]
+  --container_dir     <str>   Dir to download a container to                    [default: $container_dir]
+  --container_path    <file>  Local container image file ('.sif') to use        [default (if any): $container_path]
+  --conda_path        <dir>   Full path to a Conda environment to use           [default (if any): $conda_path]
+  -h/--help                   Print this help message
+  -v/--version                Print script and $TOOL_NAME versions
+    
+TOOL DOCUMENTATION:
+  $TOOL_DOCS
+"
 }
 
 # Function to source the script with Bash functions
@@ -88,46 +90,52 @@ source_function_script() {
         script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
         SCRIPT_NAME=$(basename "$0")
     fi
-    function_script=$(realpath "$script_dir"/../dev/"$(basename "$FUNCTION_SCRIPT_URL")")
+    function_script_name="$(basename "$FUNCTION_SCRIPT_URL")"
+    function_script_path="$script_dir"/../dev/"$function_script_name"
+
     # Download the function script if needed, then source it
-    if [[ ! -f "$function_script" ]]; then
-        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
-        function_script=$(basename "$FUNCTION_SCRIPT_URL")
-        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script"
+    if [[ -f "$function_script_path" ]]; then
+        source "$function_script_path"
+    else
+        if [[ ! -f "$function_script_name" ]]; then
+            echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
+            wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
+        fi
+        source "$function_script_name"
     fi
-    source "$function_script"
 }
 
 # Check if this is a SLURM job, then load the Bash functions
 if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
-source_function_script
+source_function_script $IS_SLURM
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
 # Initiate variables
+version_only=false  # When true, just print tool & script version info and exit
 indir=
 outdir=
 fa_type_opt=
-opts=
+more_opts=
 threads=
 
-# Parse command-line args
+# Parse command-line options
 all_opts="$*"
 while [ "$1" != "" ]; do
     case "$1" in
         -i | --indir )      shift && indir=$1 ;;
         -o | --outdir )     shift && outdir=$1 ;;
         --tree_method )     shift && tree_method=$1 ;;
-        --nuc )             fa_type=prot && fa_type_opt="-d" ;;
-        --opts )            shift && opts=$1 ;;
-        --env_type )             shift && env_type=$1 ;;
-        --no_strict )       strict_bash=false ;;
-        --dl_container )    dl_container=true ;;
+        --nuc )             fa_type=nuc && fa_type_opt="-d" ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env_type )        shift && env_type=$1 ;;
+        --conda_path )      shift && conda_path=$1 ;;
         --container_dir )   shift && container_dir=$1 ;;
-        --container_url )   shift && container_url=$1 && dl_container=true ;;
+        --container_url )   shift && container_url=$1 ;;
+        --container_path )  shift && container_path=$1 ;;
         -h | --help )       script_help; exit 0 ;;
-        -v | --version )         version_only=true ;;
+        -v | --version)     version_only=true ;;
         * )                 die "Invalid option $1" "$all_opts" ;;
     esac
     shift
@@ -137,15 +145,15 @@ done
 #                          INFRASTRUCTURE SETUP
 # ==============================================================================
 # Strict Bash settings
-[[ "$strict_bash" == true ]] && set -euo pipefail
+set -euo pipefail
 
 # Load software
-load_env "$conda_path" "$container_path" "$dl_container"
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
 [[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
 
 # Check options provided to the script
-[[ -z "$indir" ]] && die "No input file specified, do so with -i/--indir" "$opts"
-[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$opts"
+[[ -z "$indir" ]] && die "No input file specified, do so with -i/--indir" "$more_opts"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$more_opts"
 [[ ! -d "$indir" ]] && die "Input dir $indir does not exist"
 
 # Define outputs based on script parameters
@@ -158,11 +166,13 @@ mkdir -p "$outdir" && rm -r "$outdir"
 log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
 echo "=========================================================================="
 echo "All options passed to this script:        $all_opts"
+echo "Working directory:                        $PWD"
+echo
 echo "Input dir:                                $indir"
 echo "Input FASTA type:                         $fa_type"
 echo "Output dir:                               $outdir"
 echo "Gene tree inference method:               $tree_method"
-[[ -n $opts ]] && echo "Additional options for $TOOL_NAME:        $opts"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
 set_threads "$IS_SLURM"
 [[ "$IS_SLURM" == true ]] && slurm_resources
 
@@ -176,7 +186,8 @@ runstats $TOOL_BINARY \
     -M "$tree_method" \
     -t "$threads" \
     -a "$threads" \
-    $fa_type_opt $opts
+    $fa_type_opt \
+    $more_opts
 
 # (Orthofinder puts the results within a dir called 'Results_<date>')
 log_time "Moving the output files..."
@@ -186,6 +197,9 @@ rmdir "$outdir"/Results_*
 # Create the log directory
 mkdir -p "$LOG_DIR"
 
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
 log_time "Listing files in the output dir:"
 ls -lhd "$(realpath "$outdir")"/*
 final_reporting "$LOG_DIR"
