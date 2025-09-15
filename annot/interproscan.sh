8aa826782fa30e4b932e8b8e0eb8e8e7aa278465
Jelmer Poelstra <poelstra.1@osu.edu>
2025-09-15 12:23:52 -0400

    Update software versions, databases, and script to latest template

--- annot/interproscan.sh
+++ annot/interproscan.sh
@@ -1,102 +1,206 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
 #SBATCH --account=PAS0471
 #SBATCH --time=12:00:00
 #SBATCH --cpus-per-task=15
 #SBATCH --mem=100G
+#SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=interproscan
 #SBATCH --output=slurm-interproscan-%j.out
 
+# ==============================================================================
+#                          CONSTANTS AND DEFAULTS
+# ==============================================================================
+# Constants - generic
+DESCRIPTION="Run InterProScan to functionally annotate a genome (proteome)"
+SCRIPT_VERSION="2025-09-14"
+SCRIPT_AUTHOR="Jelmer Poelstra"
+REPO_URL=https://github.com/mcic-osu/mcic-scripts
+FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
+TOOL_BINARY=interproscan.sh
+TOOL_NAME=InterProScan
+TOOL_DOCS=https://interproscan-docs.readthedocs.io
+VERSION_COMMAND="$TOOL_BINARY --version"
 
-# PARSE ARGUMENTS --------------------------------------------------------------
-## Help function
-Help() {
-  echo
-  echo "$0: Run InterProScan to functionally annotate a genome."
-  echo
-  echo "Syntax: $0 -i <protein-FASTA> -o <output-dir> ..."
-  echo
-  echo "Required options:"
-  echo "    -i STRING         Protein FASTA file"
-  echo "    -o STRING         Output dir"
-  echo
-  echo "Other options:"
-  echo "    -a STRING         Other argument(s) to pass to InterProScan"
-  echo "    -h                Print this help message and exit"
-  echo
-  echo "Example:              $0 -i my_genome.fa -o results/braker -d odb_prots.fa"
-  echo "To submit the OSC queue, preface with 'sbatch': sbatch $0 ..."
-  echo
-  echo "InterProScan documentation: https://interproscan-docs.readthedocs.io/"
-  echo
+# Defaults - generics
+env_type=conda                  # Use a 'conda' env or a Singularity 'container'
+conda_path=/fs/ess/PAS0471/conda/interproscan_5.75
+container_url=
+container_dir="$HOME/containers"
+container_path=
+
+# ==============================================================================
+#                                   FUNCTIONS
+# ==============================================================================
+script_help() {
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+            =================================================
+
+DESCRIPTION:
+$DESCRIPTION
+    
+USAGE / EXAMPLE COMMANDS:
+  - Basic usage example:
+      sbatch $0 -i proteins.faa -o results/interproscan
+    
+REQUIRED OPTIONS:
+  -i/--infile         <file>  A FASTA file with protein sequences (proteome)
+  -o/--outdir         <dir>   Output dir (will be created if needed)
+    
+OTHER KEY OPTIONS:
+  --more_opts         <str>   Quoted string with one or more additional options
+                              for $TOOL_NAME
+    
+UTILITY OPTIONS:
+  --env_type          <str>   Whether to use a Singularity/Apptainer container  [default: $env_type]
+                              ('container') or a Conda environment ('conda') 
+  --container_url     <str>   URL to download a container from                  [default (if any): $container_url]
+  --container_dir     <str>   Dir to download a container to                    [default: $container_dir]
+  --container_path    <file>  Local container image file ('.sif') to use        [default (if any): $container_path]
+  --conda_path        <dir>   Full path to a Conda environment to use           [default (if any): $conda_path]
+  -h/--help                   Print this help message
+  -v/--version                Print script and $TOOL_NAME versions
+
+HARDCODED INTERPROSCAN OPTIONS THAT THE SCRIPT WILL ALWAYS RUN:
+  --goterms                  Retrieve Gene Ontology (GO) terms
+  --pathways                 Retrieve pathway annotations (e.g. KEGG, MetaCyc)
+  --iprlookup                Include InterPro annotations
+  --disable-precalc          Run everything offline
+
+TOOL DOCUMENTATION:
+  $TOOL_DOCS
+"
 }
 
-## Option defaults
-protein_fa=""
-outdir=""
-more_args=""
+# Function to source the script with Bash functions
+source_function_script() {
+    # Determine the location of this script, and based on that, the function script
+    if [[ "$IS_SLURM" == true ]]; then
+        script_path=$(scontrol show job "$SLURM_JOB_ID" | awk '/Command=/ {print $1}' | sed 's/Command=//')
+        script_dir=$(dirname "$script_path")
+        SCRIPT_NAME=$(basename "$script_path")
+    else
+        script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+        SCRIPT_NAME=$(basename "$0")
+    fi
+    function_script_name="$(basename "$FUNCTION_SCRIPT_URL")"
+    function_script_path="$script_dir"/../dev/"$function_script_name"
 
-## Parse command-line options
-while getopts ':i:o:a:h' flag; do
-  case "${flag}" in
-    i) protein_fa="$OPTARG" ;;
-    o) outdir="$OPTARG" ;;
-    a) more_args="$OPTARG" ;;
-    h) Help && exit 0 ;;
-    \?) echo -e "\n## $0: ERROR: Invalid option -$OPTARG\n\n" >&2 && exit 1 ;;
-    :) echo -e "\n## $0: ERROR: Option -$OPTARG requires an argument\n\n" >&2 && exit 1 ;;
-  esac
+    # Download the function script if needed, then source it
+    if [[ -f "$function_script_path" ]]; then
+        source "$function_script_path"
+    else
+        if [[ ! -f "$function_script_name" ]]; then
+            echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
+            wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
+        fi
+        source "$function_script_name"
+    fi
+}
+
+# Check if this is a SLURM job, then load the Bash functions
+if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
+source_function_script $IS_SLURM
+
+# ==============================================================================
+#                          PARSE COMMAND-LINE ARGS
+# ==============================================================================
+# Initiate variables
+version_only=false  # When true, just print tool & script version info and exit
+infile=
+outdir=
+more_opts=
+threads=
+
+# Parse command-line options
+all_opts="$*"
+while [ "$1" != "" ]; do
+    case "$1" in
+        -i | --infile )     shift && infile=$1 ;;
+        -o | --outdir )     shift && outdir=$1 ;;
+        --more_opts )       shift && more_opts=$1 ;;
+        --env_type )        shift && env_type=$1 ;;
+        --conda_path )      shift && conda_path=$1 ;;
+        --container_dir )   shift && container_dir=$1 ;;
+        --container_url )   shift && container_url=$1 ;;
+        --container_path )  shift && container_path=$1 ;;
+        -h | --help )       script_help; exit 0 ;;
+        -v | --version)     version_only=true ;;
+        * )                 die "Invalid option $1" "$all_opts" ;;
+    esac
+    shift
 done
 
-
-# SETUP ------------------------------------------------------------------------
-## Check input
-[[ ! -f "$protein_fa" ]] && echo "## ERROR: Protein file (-d) $protein_fa does not exist" >&2 && exit 1
-
-## Braker2 conda env which contains everything except GeneMark-EX and ProtHint 
-module load python/3.6-conda5.2
-source activate /fs/project/PAS0471/jelmer/conda/interproscan-5.55
-
-## Bash strict mode
+# ==============================================================================
+#                          INFRASTRUCTURE SETUP
+# ==============================================================================
+# Strict Bash settings
 set -euo pipefail
 
-## Make output dir
-mkdir -p "$outdir"
+# Load software
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
+[[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
 
-## Report
-echo "## Starting script interproscan.sh"
-date
+# Check options provided to the script
+[[ -z "$infile" ]] && die "No input file specified, do so with -i/--infile" "$all_opts"
+[[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
+[[ ! -f "$infile" ]] && die "Input file $infile does not exist"
+
+# Define outputs based on script parameters
+LOG_DIR="$outdir"/logs
+mkdir -p "$LOG_DIR"
+
+# ==============================================================================
+#                         REPORT PARSED OPTIONS
+# ==============================================================================
+log_time "Starting script $SCRIPT_NAME, version $SCRIPT_VERSION"
+echo "=========================================================================="
+echo "All options passed to this script:        $all_opts"
+echo "Working directory:                        $PWD"
 echo
-echo "## Protein FASTA file:                   $protein_fa"
-echo "## Output dir:                           $outdir"
-[[ $more_args != "" ]] && echo "## Other arguments to pass to InterProScan:    $more_args"
-echo -e "--------------------\n"
+echo "Input file:                               $infile"
+echo "Output dir:                               $outdir"
+[[ -n $more_opts ]] && echo "Additional options for $TOOL_NAME:        $more_opts"
+log_time "Listing the input file(s):"
+ls -lh "$infile"
+set_threads "$IS_SLURM"
+[[ "$IS_SLURM" == true ]] && slurm_resources
 
-
-# MAIN -------------------------------------------------------------------------
-echo "## Now running InterProScan..."
-interproscan.sh \
-    -i "$protein_fa" \
-    --cpu "$SLURM_CPUS_PER_TASK" \
+# ==============================================================================
+#                               RUN
+# ==============================================================================
+log_time "Running $TOOL_NAME..."
+runstats $TOOL_BINARY \
+    -i "$infile" \
     --output-dir "$outdir" \
     --goterms \
+    --pathways \
+    --iprlookup \
+    --disable-precalc \
     --tempdir "$TMPDIR" \
-    --disable-precalc
+    --cpu "$threads" \
+    $more_opts
 
-# --pathways -- add this option?
+#? https://interproscan-docs.readthedocs.io/en/v5/HowToRun.html
 
-# WRAP-UP ----------------------------------------------------------------------
-echo -e "\n-------------------------------"
-echo "## Listing files in the output dir:"
-ls -lh "$outdir"
-echo -e "\n## Done with script interproscan.sh"
-date
-echo
-sacct -j "$SLURM_JOB_ID" -o JobID,AllocTRES%50,Elapsed,CPUTime,TresUsageInTot,MaxRSS
-echo
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
+log_time "Listing files in the output dir:"
+ls -lhd "$(realpath "$outdir")"/*
+final_reporting "$LOG_DIR"
 
-
-# DEV NOTES --------------------------------------------------------------------
-## Setup
-# python3 /fs/project/PAS0471/jelmer/conda/interproscan-5.55/share/InterProScan/initial_setup.py
-# Note: above didn't work, but running interproscan.sh without args seemed to do the setup!
+# ==============================================================================
+#                      INSTALLATION NOTES (2025-09-15)
+# ==============================================================================
+## - Installation - https://interproscan-docs.readthedocs.io/en/v5/HowToDownload.html
+# wget -P software/interproscan https://ftp.ebi.ac.uk/pub/software/unix/iprscan/5/5.75-106.0/interproscan-5.75-106.0-64-bit.tar.gz
+# cd software/interproscan
+# tar -pxvzf interproscan-5.75-106.0-*-bit.tar.gz
+## - Create a Conda env for it:
+# micromamba env create -p /fs/ess/PAS0471/conda/interproscan_5.75
+# mkdir -p /fs/ess/PAS0471/conda/interproscan_5.75/bin
+# cp -r software/interproscan/interproscan-5.75-106.0/* /fs/ess/PAS0471/conda/interproscan_5.75/bin/
+## - Download the database:
+# setup.py -f interproscan.properties
