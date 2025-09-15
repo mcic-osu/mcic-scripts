6ac63c3964868c834b7b47a6cae9846415d83c5c
Jelmer Poelstra <poelstra.1@osu.edu>
2025-09-15 12:23:15 -0400

    Update to latest template, improve help

--- meta/kraken_build.sh
+++ meta/kraken_build.sh
@@ -2,6 +2,7 @@
 #SBATCH --account=PAS0471
 #SBATCH --time=30:00:00
 #SBATCH --mem=100G
+#SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=kraken_build
 #SBATCH --output=slurm-kraken_build-%j.out
@@ -11,8 +12,8 @@
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Build a custom Kraken database
-  Note: Standard Kraken databases can be downloaded from https://benlangmead.github.io/aws-indexes/"
-SCRIPT_VERSION="2024-09-19"
+  Note: Standard Kraken databases can be downloaded from https://benlangmead.github.io/aws-indexes"
+SCRIPT_VERSION="2025-09-15"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 FUNCTION_SCRIPT_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/dev/bash_functions.sh
@@ -22,14 +23,11 @@ TOOL_DOCS=https://github.com/DerrickWood/kraken2/blob/master/docs/MANUAL.markdow
 VERSION_COMMAND="$TOOL_BINARY --version"
 
 # Defaults - generics
-env_type=conda                           # Use a 'conda' env or a Singularity 'container'
+env_type=conda                  # Use a 'conda' env or a Singularity 'container'
 conda_path=/fs/ess/PAS0471/jelmer/conda/kraken2
-container_path=
 container_url=
-dl_container=false
 container_dir="$HOME/containers"
-strict_bash=true
-version_only=false                 # When true, just print tool & script version info and exit
+container_path=
 
 # Defaults - tool parameters
 libs=false
@@ -38,43 +36,49 @@ libs=false
 #                                   FUNCTIONS
 # ==============================================================================
 script_help() {
-    echo -e "\n                          $0"
-    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
-    echo "        =============================================================="
-    echo "DESCRIPTION:"
-    echo "  $DESCRIPTION"
-    echo
-    echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  - Basic usage:"
-    echo "      sbatch $0 -o refdata/kraken/my_db --genome_dir refdata/kraken/my_db/genomes"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "  -o/--db_dir         <dir>   Dir for the Kraken DB"
-    echo
-    echo "OTHER KEY OPTIONS:"
-    echo "  --libs              <str>   Comma-separated list of reference libraries to include [default:none]"
-    echo "                                Options: 'archaea', 'bacteria', 'viral', 'plasmid', 'human', 'fungi', 'plant', 'protozoa',"
-    echo "                                         'UniVec', 'UniVec_Core', 'nr', 'nt'"
-    echo "                                See https://github.com/DerrickWood/kraken2/blob/master/docs/MANUAL.markdown#custom-databases"
-    echo "  --genome_fa         <file>  Custom genome FASTA file to be added to the db (use if wanting to add a single genome)"
-    echo "  --genome_dir        <dir>   Dir with custom genome FASTA files (use if wanting to add multiple genomes)"
-    echo "  --more_opts         <str>   Quoted string with additional options for $TOOL_NAME"
-    echo
-    echo "UTILITY OPTIONS:"
-    echo "  --env_type               <str>   Use a Singularity container ('container') or a Conda env ('conda') [default: $env_type]"
-    echo "                                (NOTE: If no default '--container_url' is listed below,"
-    echo "                                you'll have to provide one in order to run the script with a container.)"
-    echo "  --conda_env         <dir>   Full path to a Conda environment to use [default: $conda_path]"
-    echo "  --container_url     <str>   URL to download the container from      [default: $container_url]"
-    echo "                                A container will only be downloaded if an URL is provided with this option, or '--dl_container' is used"
-    echo "  --container_dir     <str>   Dir to download the container to        [default: $container_dir]"
-    echo "  --dl_container              Force a redownload of the container     [default: $dl_container]"
-    echo "  --no_strict                 Don't use strict Bash settings ('set -euo pipefail') -- can be useful for troubleshooting"
-    echo "  -h/--help                   Print this help message and exit"
-    echo "  -v                          Print the version of this script and exit"
-    echo "  --version                   Print the version of $TOOL_NAME and exit"
-    echo
-    echo "TOOL DOCUMENTATION: $TOOL_DOCS"
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+            =================================================
+
+DESCRIPTION:
+$DESCRIPTION
+    
+USAGE / EXAMPLE COMMANDS:
+  - Basic usage example:
+      sbatch $0 -o refdata/kraken/my_db --genome_dir refdata/kraken/my_db/genomes
+      sbatch $0 -o refdata/kraken/my_db --libs 'archaea,bacteria,viral'
+    
+REQUIRED OPTIONS:
+    -o/--db_dir       <dir>   Dir for the Kraken DB
+    
+OTHER KEY OPTIONS:
+    --libs            <str>   Comma-separated list of reference libraries       [default:none]
+                              to include in the DB (see below for options)
+                              Options: 'archaea', 'bacteria', 'viral', 'plasmid',
+                              'human', 'fungi', 'plant', 'protozoa',
+                              'UniVec', 'UniVec_Core', 'nr', 'nt'
+                              See https://github.com/DerrickWood/kraken2/blob/master/docs/MANUAL.markdown#custom-databases
+  --genome_fa         <file>  Custom genome FASTA file to be added to the db
+                              (use this to add a single genome/file)
+  --genome_dir        <dir>   Dir with custom genome FASTA files
+                              (use this to add multiple genomes/files)
+  --more_opts         <str>   Quoted string with one or more additional options
+                              for $TOOL_NAME
+    
+UTILITY OPTIONS:
+  --env_type          <str>   Whether to use a Singularity/Apptainer container  [default: $env_type]
+                              ('container') or a Conda environment ('conda') 
+  --container_url     <str>   URL to download a container from                  [default (if any): $container_url]
+  --container_dir     <str>   Dir to download a container to                    [default: $container_dir]
+  --container_path    <file>  Local container image file ('.sif') to use        [default (if any): $container_path]
+  --conda_path        <dir>   Full path to a Conda environment to use           [default (if any): $conda_path]
+  -h/--help                   Print this help message
+  -v/--version                Print script and $TOOL_NAME versions
+    
+TOOL DOCUMENTATION:
+  $TOOL_DOCS
+"
 }
 
 # Function to source the script with Bash functions
@@ -88,31 +92,37 @@ source_function_script() {
         script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
         SCRIPT_NAME=$(basename "$0")
     fi
-    function_script=$(realpath "$script_dir"/../dev/"$(basename "$FUNCTION_SCRIPT_URL")")
+    function_script_name="$(basename "$FUNCTION_SCRIPT_URL")"
+    function_script_path="$script_dir"/../dev/"$function_script_name"
+
     # Download the function script if needed, then source it
-    if [[ ! -f "$function_script" ]]; then
-        echo "Can't find script with Bash functions ($function_script), downloading from GitHub..."
-        function_script=$(basename "$FUNCTION_SCRIPT_URL")
-        wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script"
+    if [[ -f "$function_script_path" ]]; then
+        source "$function_script_path"
+    else
+        if [[ ! -f "$function_script_name" ]]; then
+            echo "Can't find script with Bash functions ($function_script_name), downloading from GitHub..."
+            wget -q "$FUNCTION_SCRIPT_URL" -O "$function_script_name"
+        fi
+        source "$function_script_name"
     fi
-    source "$function_script"
 }
 
 # Check if this is a SLURM job, then load the Bash functions
 if [[ -z "$SLURM_JOB_ID" ]]; then IS_SLURM=false; else IS_SLURM=true; fi
-source_function_script
+source_function_script $IS_SLURM
 
 # ==============================================================================
 #                          PARSE COMMAND-LINE ARGS
 # ==============================================================================
 # Initiate variables
+version_only=false  # When true, just print tool & script version info and exit
 db_dir=
 genome_fa=
 genome_dir=
 more_opts=
 threads=
 
-# Parse command-line args
+# Parse command-line options
 all_opts="$*"
 while [ "$1" != "" ]; do
     case "$1" in
@@ -121,13 +131,13 @@ while [ "$1" != "" ]; do
         --genome_dir )      shift && genome_dir=$1 ;;
         --libs )            shift && libs=$1 ;;
         --more_opts )       shift && more_opts=$1 ;;
-        --env_type )             shift && env_type=$1 ;;
-        --no_strict )       strict_bash=false ;;
-        --dl_container )    dl_container=true ;;
+        --env_type )        shift && env_type=$1 ;;
+        --conda_path )      shift && conda_path=$1 ;;
         --container_dir )   shift && container_dir=$1 ;;
-        --container_url )   shift && container_url=$1 && dl_container=true ;;
+        --container_url )   shift && container_url=$1 ;;
+        --container_path )  shift && container_path=$1 ;;
         -h | --help )       script_help; exit 0 ;;
-        -v | --version )         version_only=true ;;
+        -v | --version)     version_only=true ;;
         * )                 die "Invalid option $1" "$all_opts" ;;
     esac
     shift
@@ -137,18 +147,18 @@ done
 #                          INFRASTRUCTURE SETUP
 # ==============================================================================
 # Strict Bash settings
-[[ "$strict_bash" == true ]] && set -euo pipefail
-shopt -s nullglob # Needed for flexible globbing
+set -euo pipefail
 
 # Load software
-load_env "$conda_path" "$container_path" "$dl_container"
+load_env "$env_type" "$conda_path" "$container_dir" "$container_path" "$container_url"
 [[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
 
 # Check options provided to the script
 [[ -z "$db_dir" ]] && die "No db dir specified, do so with -o/--db" "$all_opts"
 
 # Define outputs based on script parameters
-LOG_DIR="$db_dir"/logs && mkdir -p "$LOG_DIR"
+LOG_DIR="$db_dir"/logs
+mkdir -p "$LOG_DIR" "$db_dir"
 
 # ==============================================================================
 #                         REPORT PARSED OPTIONS
@@ -170,22 +180,23 @@ set_threads "$IS_SLURM"
 # ==============================================================================
 #                               RUN
 # ==============================================================================
-# Create the output directories
-mkdir -p "$LOG_DIR" "$db_dir"
-
-# Download taxonomy
+# Download NCBI taxonomy
 if [[ ! -d "$db_dir"/taxonomy ]]; then
     log_time "Downloading taxonomy..."
-    runstats $TOOL_BINARY --download-taxonomy --db "$db_dir"
+    runstats $TOOL_BINARY --download-taxonomy --db "$db_dir" --threads "$threads"
+else
+    log_time "Taxonomy dir already exists, not downloading again"
 fi
 
-# Download libraries
+# Download taxon-specific 'libraries'
 if [[ "$libs" != "false" ]]; then
+    
     IFS="," read -ra lib_array <<< "$libs"
+    
     for lib in "${lib_array[@]}"; do
         if [[ ! -d "$db_dir"/library/"$lib" ]]; then
             log_time "Downloading library: $lib..."
-            $TOOL_BINARY --download-library "$lib" --db "$db_dir"
+            $TOOL_BINARY --download-library "$lib" --db "$db_dir" --threads "$threads"
         else
             log_time "Library dir for $lib already exists, not downloading again"
         fi
@@ -196,25 +207,28 @@ fi
 if [[ -n "$genome_fa" ]]; then
     # If there is a single genome to be added
     log_time "Adding custom genome $genome_fa to Kraken library..."
-    $TOOL_BINARY --add-to-library "$genome_fa" --db "$db_dir"
+    $TOOL_BINARY --add-to-library "$genome_fa" --db "$db_dir" --threads "$threads"
 elif [[ -n "$genome_dir" ]]; then
     # If all genomes in a dir should be added
     for genome_fa in "$genome_dir"/*.{fa,fasta,fna}; do
         log_time "Adding custom genome $genome_fa to Kraken library..."
-        $TOOL_BINARY --add-to-library "$genome_fa" --db "$db_dir"
+        $TOOL_BINARY --add-to-library "$genome_fa" --db "$db_dir" --threads "$threads"
     done
 else
     log_time "Not adding any custom genomes"
 fi
 
-# Run the tool
+# Build the database
 log_time "Building the database..."
 runstats $TOOL_BINARY \
     --build \
     --db "$db_dir" \
-    -t "$threads" \
+    --threads "$threads" \
     $more_opts
 
+# ==============================================================================
+#                               WRAP-UP
+# ==============================================================================
 log_time "Listing files in the output dir:"
-ls -lhd "$(realpath "$db_dir")"/*
+ls -lhd "$(realpath "$outdir")"/*
 final_reporting "$LOG_DIR"
