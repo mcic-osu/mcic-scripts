e72e1645210ccb2793f3ce8aed4abf9105630f76
Jelmer Poelstra <poelstra.1@osu.edu>
2026-04-03 13:53:37 -0400

    Improve documentation

--- smrna/nfc-smrnaseq.sh
+++ smrna/nfc-smrnaseq.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=PAS0471
 #SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=1
 #SBATCH --mail-type=END,FAIL
 #SBATCH --job-name=nfc_smrnaseq
 #SBATCH --output=slurm-nfc_smrnaseq-%j.out
@@ -10,7 +11,7 @@
 # ==============================================================================
 # Constants - generic
 DESCRIPTION="Run the Nextflow/nf-core small RNA-Seq pipeline 'smrnaseq' (https://nf-co.re/smrnaseq)"
-SCRIPT_VERSION="2025-02-02"
+SCRIPT_VERSION="2026-03-27"
 SCRIPT_AUTHOR="Jelmer Poelstra"
 REPO_URL=https://github.com/mcic-osu/mcic-scripts
 TOOL_BINARY="nextflow run"
@@ -19,7 +20,7 @@ VERSION_COMMAND="nextflow -v"
 
 # Constants - parameters
 WORKFLOW_NAME=nf-core/smrnaseq                              # The name of the nf-core workflow
-PROTOCOL=illumina                                           # See https://nf-co.re/smrnaseq/2.4.0/docs/usage/#protocol
+PROTOCOL=illumina                                           # See https://nf-co.re/smrnaseq/2.4.1/docs/usage/#protocol
 PROFILE="singularity,$PROTOCOL"                             # Run workflow with Singularity containers
 OSC_CONFIG_URL=https://raw.githubusercontent.com/mcic-osu/mcic-scripts/main/nextflow/osc.config
 
@@ -30,49 +31,63 @@ osc_account=PAS0471                                         # If the scripts is 
 [[ -n $SLURM_JOB_ACCOUNT ]] && osc_account=$(echo "$SLURM_JOB_ACCOUNT" | tr "[:lower:]" "[:upper:]")
 
 # Parameter defaults - workflow
-workflow_version=2.4.0                                      # The version of the nf-core workflow #! Check if up-to-date
-work_dir=/fs/scratch/"$osc_account"/$USER/nfc_smrnaseq      # 'work dir' for initial outputs (note: final outputs go to --outdir instead)
+workflow_version=2.4.1                                      # The version of the nf-core workflow
+workflow_dir=                                               # Path to local workflow copy (optional)
 resume=true && resume_opt="-resume"
+work_dir=/fs/scratch/"$osc_account"/$USER/nfc_smrnaseq      # 'work dir' for initial outputs (note: final outputs go to --outdir instead)
+container_dir="$work_dir"/containers
 
 # ==============================================================================
 #                                   FUNCTIONS
 # ==============================================================================
 # Help function
 script_help() {
-    echo -e "\n                          $0"
-    echo "      (v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL)"
-    echo "        =============================================="
-    echo "DESCRIPTION:"
-    echo "  $DESCRIPTION"
-    echo
-    echo "USAGE / EXAMPLE COMMANDS:"
-    echo "  sbatch $0 -o results/nfc_smrnaseq --fasta data/genome.fna --fq_dir data/fastq -p config/nfc_smrnaseq_params.yml"
-    echo
-    echo "REQUIRED OPTIONS:"
-    echo "  --fasta             <file>  FASTA file with genome sequence for the focal species"
-    echo "  -o/--outdir         <dir>   Dir for workflow output files (will be created if needed)"
-    echo "  -p/--params         <file>  YAML file with workflow parameters"
-    echo "                              Use 'mcic-scripts/metabar/nfcore_smrnaseq_params.yml' as a template/starting point"
-    echo 
-    echo "TO POINT TO THE INPUT FASTQ FILES, USE ONE OF THE BELOW TWO OPTIONS:"
-    echo "  --fq_dir            <dir>   Dir with FASTQ files -- assumes single-end Illumina files, all from a single run"
-    echo "  --samplesheet       <file>  Pre-made CSV samplesheet, see https://nf-co.re/smrnaseq/2.4.0/docs/usage/#samplesheet-input"
-    echo
-    echo "OTHER OPTIONS:"
-    echo "  --workflow_version  <str>   Workflow version to use                                     [default: $workflow_version]"
-    echo "  --restart                   Don't attempt to resume workflow run, but start over        [default: resume workflow]"
-    echo "  --container_dir     <dir>   Directory with container images                             [default: $container_dir]"
-    echo "                                - Required images will be downloaded here when not already present here" 
-    echo "  --config            <file>  Additional config file                                      [default: none]"
-    echo "                                - Settings in this file will override default settings"
-    echo "                                - Note that the mcic-scripts OSC config file will always be included, too"
-    echo "                                  (https://github.com/mcic-osu/mcic-scripts/blob/main/nextflow/osc.config)"
-    echo "  --work_dir           <dir>  Scratch (work) dir for the workflow                         [default: $work_dir]"
-    echo "                                - This is where the workflow results will be stored before final results are copied to the output dir."
-    echo "  -h/--help                   Print this help message and exit"
-    echo "  -v                          Print the version of this script and exit"
-    echo "  --version                   Print the version of Nextflow and exit"
-    echo
+    echo -e "
+                        $0
+    v. $SCRIPT_VERSION by $SCRIPT_AUTHOR, $REPO_URL
+        ==============================================
+
+DESCRIPTION:
+$DESCRIPTION
+
+USAGE / EXAMPLE COMMANDS:
+  sbatch $0 -o results/nfc_smrnaseq --fq_dir data/fastq -p config/nfc-smrnaseq.yml
+
+REQUIRED OPTIONS:
+  -o/--outdir         <dir>   Dir for final workflow output files
+                              (will be created if needed)
+  -p/--params         <file>  YAML file with workflow parameters.
+                              Use 'mcic-scripts/smrna/nfc-smrnaseq.yml' as
+                              a template/starting point
+
+TO POINT TO THE INPUT FASTQ FILES, USE ONE OF THE BELOW TWO OPTIONS:
+  -i/--fq_dir         <dir>   Dir with FASTQ files -- assumes
+                              single-end Illumina files from a single run
+  --samplesheet       <file>  Pre-made CSV samplesheet, see
+                              https://nf-co.re/smrnaseq/2.4.1/docs/usage/#samplesheet-input
+
+OTHER OPTIONS:
+  --workflow_dir      <dir>   Path to local workflow copy (e.g., software/nf-core-smrnaseq_2.4.1/2_4_1)  [default: use remote version]
+                                - When specified, this local copy will be used instead of pulling from nf-core
+                                - Useful for testing patched/edited workflows
+  --workflow_version  <str>   nf-core smrnaseq workflow version to use          [default: $workflow_version]
+  --restart                   Don't attempt to resume workflow: start over      [default: resume workflow]
+  --container_dir     <dir>   Directory with container images                   [default: $container_dir]
+                                - Required images will be downloaded here when
+                                  not already present here
+  --config            <file>  Additional config file                            [default: none]
+                                - Settings in this file will override default
+                                  settings
+                                - Note that the mcic-scripts OSC config file
+                                  will always be included, too
+                                  (https://github.com/mcic-osu/mcic-scripts/blob/main/nextflow/osc.config)
+  --work_dir           <dir>  Scratch (work) dir for the workflow               [default: $work_dir]
+                                - This is where the workflow results will be
+                                  stored before final results are copied to
+                                  the output dir.
+  -h/--help                   Print this help message and exit
+  -v/--version                Print versions and exit
+"
 }
 
 # Function to source the script with Bash functions
@@ -121,28 +136,28 @@ source_function_script $IS_SLURM
 # Initiate variables
 fq_dir=
 samplesheet=
-fasta=
 outdir=
 params_file=
 config_file=
-container_dir=
+workflow_dir=
+revision_opt=
 
 # Parse command-line args
 all_opts="$*"
 while [ "$1" != "" ]; do
     case "$1" in
-        --fasta )                       shift && fasta=$1 ;;
-        --fq_dir )                      shift && fq_dir=$1 ;;
+        -i | --fq_dir )                 shift && fq_dir=$1 ;;
         --samplesheet )                 shift && samplesheet=$1 ;;
         -p | --params )                 shift && params_file=$1 ;;
         -o | --outdir )                 shift && outdir=$1 ;;
+        --workflow_dir )                shift && workflow_dir=$1 ;;
         --workflow_version )            shift && workflow_version=$1 ;;
         --config | -config )            shift && config_file=$1 ;;
-        --work_dir | -work-dir )        shift && work_dir=$1 ;;
+        --work_dir | -work )            shift && work_dir=$1 ;;
         --container_dir )               shift && container_dir=$1 ;;
         --restart | -restart )          resume=false && resume_opt= ;;
         -h | --help )                   script_help; exit 0 ;;
-        -v | --version )                     version_only=true ;;
+        -v | --version )                version_only=true ;;
         * )                             die "Invalid option $1" "$all_opts" ;;
     esac
     shift
@@ -151,13 +166,12 @@ done
 # Check arguments
 [[ -z "$outdir" ]] && die "No output dir specified, do so with -o/--outdir" "$all_opts"
 [[ -z "$params_file" ]] && die "No parameter YAML file specified, do so with -p/--params" "$all_opts"
-[[ -z "$fasta" ]] && die "No input FASTA genome file specified, do so with --fasta" "$all_opts"
 [[ ! -f "$params_file"  ]] && die "Input parameter YAML file $params_file does not exist"
-[[ ! -f "$fasta"  ]] && die "FASTA genome file $fasta does not exist"
 [[ -z "$fq_dir" && -z "$samplesheet" ]] && die "No input FASTQ dir nor samplesheet specified, specify one of these either with --samplesheet or --fq_dir" "$all_opts"
 [[ -n "$fq_dir" && ! -d "$fq_dir"  ]] && die "Input FASTQ file dir $fq_dir does not exist"
 [[ -n "$samplesheet" && ! -f "$samplesheet"  ]] && die "Input FASTQ samplesheet file $samplesheet does not exist"
 [[ -n "$config_file" && ! -f "$config_file"  ]] && die "Input config file $config_file does not exist"
+[[ -n "$workflow_dir" && ! -d "$workflow_dir"  ]] && die "Workflow dir $workflow_dir does not exist"
 
 # ==============================================================================
 #                          INFRASTRUCTURE SETUP II
@@ -165,13 +179,25 @@ done
 # Strict Bash settings
 set -euo pipefail
 
+# Load software
+load_env "$conda_path"
+nextflow_setup
+[[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
+set_threads "$IS_SLURM"
+
+# Set workflow to local copy if provided, otherwise use remote
+if [[ -n "$workflow_dir" ]]; then
+    WORKFLOW_NAME="$(cd "$workflow_dir" && pwd)"  # Use absolute path
+else
+     revision_opt="-r $workflow_version"
+fi
+
 # Build the config argument
 OSC_CONFIG="$outdir"/$(basename "$OSC_CONFIG_URL")
 config_opt="-c $OSC_CONFIG"
 [[ -n "$config_file" ]] && config_opt="$config_opt -c ${config_file/,/ -c }"
 
 # Other dirs
-[[ -z "$container_dir" ]] && container_dir="$work_dir"/containers
 LOG_DIR="$outdir"/logs
 
 # Report
@@ -182,23 +208,25 @@ echo
 echo "INPUT AND OUTPUT:"
 [[ -n "$samplesheet" ]] && echo "Samplesheet:                                  $samplesheet"
 [[ -n "$fq_dir" ]] && echo "Input FASTQ dir:                              $fq_dir"
-echo "Genome FASTA file:                            $fasta"
 echo "Parameter YAML file:                          $params_file"
 echo "Output dir:                                   $outdir"
 echo
 echo "NEXTFLOW-RELATED SETTINGS:"
+[[ -n "$workflow_dir" ]] && echo "Local workflow dir:                           $WORKFLOW_NAME"
+[[ -z "$workflow_dir" ]] && echo "Workflow (remote):                            $WORKFLOW_NAME"
 echo "Resume previous run (if any):                 $resume"
 echo "Container dir:                                $container_dir"
 echo "Scratch (work) dir:                           $work_dir"
 echo "Config file argument:                         $config_opt"
 [[ -n "$config_file" ]] && echo "Additional config file:                       $config_file"
 echo "=========================================================================="
-
-# Load software and set nr of threads
-load_env "$conda_path"
-nextflow_setup
-[[ "$version_only" == true ]] && print_version "$VERSION_COMMAND" && exit 0
-set_threads "$IS_SLURM"
+log_time "Printing the contents of the parameter file:"
+cat -n "$params_file"
+if [[ -n "$config_file" ]]; then
+    log_time "Printing the contents of the additional config file:"
+    cat -n "$config_file"
+fi
+echo "=========================================================================="
 [[ "$IS_SLURM" = true ]] && slurm_resources
 
 # ==============================================================================
@@ -231,12 +259,15 @@ if [[ -z "$samplesheet" ]]; then
     cat "$samplesheet"
 fi
 
+# Copy the samplesheet and the parameter YAML file to the output dir for record-keeping
+log_time "Copying the samplesheet and parameter YAML file to the output dir..."
+cp -v "$samplesheet" "$params_file" "$outdir"/
+
 # Run the tool
 log_time "Starting the workflow.."
 runstats $TOOL_BINARY $WORKFLOW_NAME \
-    -r "$workflow_version" \
+    $revision_opt \
     -params-file "$params_file" \
-    --fasta "$fasta" \
     --input "$samplesheet" \
     --outdir "$outdir" \
     -work-dir "$work_dir" \
